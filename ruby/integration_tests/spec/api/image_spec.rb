e0f4a433eb6a53f543422cb38beb5c4ef9189a28
Vikash Rai <vrai@vmware.com>
2015-11-09 14:31:19 -0800

    hk :  Updating replicated datastore count for image in cloudstore when copy is finished.
    Change-Id: I000099afad51786531052053c3eb2366df33b72d

--- ruby/integration_tests/spec/api/image_spec.rb
+++ ruby/integration_tests/spec/api/image_spec.rb
@@ -226,7 +226,7 @@ describe "image", management: true, image: true do
       image = EsxCloud::Image.find_all.items.find { |i| i.id == image_id }
       expect(image).to_not be_nil
       expect(image.state).to eq "PENDING_DELETE"
-      expect(image.replication_progress).to eq "0%"
+      expect(image.replication_progress =~ /^(100|\d{1,2})[.]\d{1,2}%$/).to_not be nil
 
       tasks = client.get_image_tasks(image.id).items
       expect(tasks.size).to eq(2)
