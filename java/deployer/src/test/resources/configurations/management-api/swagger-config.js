4e81dcd859475bf5af1885fe415417dbe459967c
Ashok Chandrasekar <ashokc@vmware.com>
2015-11-10 13:20:01 -0800

    config: make unit tests use real config files
    This change creates symbolic links to the config files which are
    referenced in the deployer unit tests.
    
    Change-Id: I671bdceaff75370b2ee505cff60dc9e6a22db4a1

--- java/deployer/src/test/resources/configurations/management-api/swagger-config.js
+++ java/deployer/src/test/resources/configurations/management-api/swagger-config.js
@@ -1,5 +1,1 @@
-window.swaggerConfig = {
-  enableAuth: {{{ENABLE_AUTH}}},
-  swaggerLoginUrl: '{{{SWAGGER_LOGIN_URL}}}',
-  swaggerLogoutUrl: '{{{SWAGGER_LOGOUT_URL}}}',
-};
+../../../../../../api-frontend/management/src/dist/configuration/swagger-config.js
