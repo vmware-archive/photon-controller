89936c48fd072b096820a6babe7b1e8358e7dbe0
Matt Olenik <molenik@vmware.com>
2015-11-11 16:37:10 -0800

    Initial sketch of new container build
    Only has base container images for now. Can be built with "./gradlew
    :containers:all" and removed with "./gradlew clean"
    
    Requires Docker Toolbox at the moment
    
    Change-Id: I98be12ea7ca1ef389d83a14489517c4bfe5d2877

---
+++ java/containers/zookeeper/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Copyright 2015 VMware, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy of
+# the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, without warranties or
+# conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+# specific language governing permissions and limitations under the License.
+
+CONFIG="/etc/zookeeper/zoo.cfg"
+MYIDFILE="{{{ZOOKEEPER_DATA_DIRECTORY}}}/myid"
+
+echo "zookeeper config file:"
+cat $CONFIG
+
+echo "zookeeper id file:"
+if [ -f myid ]
+then
+  cp -f myid $MYIDFILE
+  cat $MYIDFILE
+else
+  echo "myid file does not exit"
+fi
+
+exec /usr/bin/zkServer.sh start-foreground $CONFIG
