0d843206fba762cac12675f59d4ff7ae5246d91a
Matt Olenik <molenik@vmware.com>
2015-11-11 16:37:10 -0800

    Gradle based container build
    Squashed changes:
    
    Initial sketch of new container build
    
    Only has base container images for now. Can be built with "./gradlew
    :containers:all" and removed with "./gradlew clean"
    
    Requires Docker Toolbox at the moment
    
    Change-Id: I98be12ea7ca1ef389d83a14489517c4bfe5d2877
    
    Add deployer container build
    
    Change-Id: I7e6da8404475aa1a0f711f375cf2ca7ea3069b28
    
    Break up vagrant provisioning into scripts
    
    Invoke scripts from Gradle into devbox, allows for fine-grained build
    workflow instead of having a large one-shot sequence in the vagrantfile
    
    Change-Id: Ibf604c84c4c0598992c076d298b2a0c262bd2d73
    
    Revise start scripts, make chairman work
    
    Change-Id: Icdf3d09bc6989332dc5710072547b7a8bc4194a9
    
    Working containers for all but mgmt and deployer
    
    Change-Id: I7b786966f641579533f1146682fe9206662a44a3
    
    Make all containers work
    
    Change-Id: I1c74de1285d83fa5d350963ccaf5eebb7d66a4b0
    
    All containers now working together, tests passing
    
    Change-Id: I74e8d9823926aeb40bf750d5c55638ef7e472a7d

--- ruby/integration_tests/ci/start_devbox.sh
+++ ruby/integration_tests/ci/start_devbox.sh
@@ -1,39 +1,25 @@
-#!/bin/bash -e
-set -x
+#!/bin/bash -ex
 
-if [ -z "$DEVBOX" ]; then
-  export DEVBOX=$WORKSPACE/devbox-photon
-  echo Assume DEVBOX at $DEVBOX
-fi
+export WORKSPACE=${WORKSPACE:=$(git rev-parse --show-toplevel)}
+export DEVBOX=${DEVBOX:="$WORKSPACE/devbox-photon"}
+export TESTS=${TESTS:="$WORKSPACE/ruby/integration_tests"}
 if [ ! -d "$DEVBOX" ]; then fail "$DEVBOX is not accessible"; fi
 
-checklist="PUBLIC_NETWORK_IP\
-  PUBLIC_NETWORK_NETMASK\
-  PUBLIC_NETWORK_GATEWAY\
-  BRIDGE_NETWORK"
+checklist=(PUBLIC_NETWORK_IP PUBLIC_NETWORK_NETMASK PUBLIC_NETWORK_GATEWAY BRIDGE_NETWORK)
 
-
-for var in $checklist
-do
-  if [ -z "$(printenv $var)" ]
-    then
-      echo Cannot start devbox. $var is not defined.
-      echo This list of properties must be defined. $checklist
-      exit 1
+for var in "${checklist[@]}"; do
+  if [ -z "$(printenv "$var")" ]; then
+    echo Cannot start devbox. "$var" is not defined.
+    echo This list of env vars must be defined: "${checklist[@]}"
+    exit 1
   fi
 done
 
-# Now installing devbox
-cd $DEVBOX
-(
-  # Using custom HOME so 'puppet install' processes don't step on each other
-  export HOME=/tmp/devbox$WORKSPACE_INDEX
-  mkdir -p $HOME
-  ./update_dependencies.sh
-)
-
+cd "$DEVBOX"
 vagrant destroy -f
-rm -rf $DEVBOX/log/*
+rm -rf "$DEVBOX/log/"
+
+./update_dependencies.sh
 
 # Exporting deployment id generated randomly used to create deployment document in cloudstore:seed
 if [ "$(uname)" == "Darwin" ]; then
@@ -42,35 +28,20 @@ else
   export RANDOM_GENERATED_DEPLOYMENT_ID=$(shuf -i 1000000000-10000000000 -n 1)
 fi
 
-if [ "$DEPLOYER_TEST" ]
-then
+if [ -n "$DEPLOYER_TEST" ]; then
   ./prepare-devbox-deployment.sh
 else
-  vagrant up
+  (cd ../java && ./gradlew :devbox:buildAll :devbox:startAll)
   # seed the database
-  (
-    cd $TESTS
-    bundle exec rake cloudstore:seed
-  )
+  (cd "$TESTS" && bundle exec rake cloudstore:seed)
 fi
 
 # Setup auth-token tool
-if [ -n "$ENABLE_AUTH" ]
-then
+if [ -n "$ENABLE_AUTH" ]; then
     echo "Copy auth-tool from within vagrant box"
-    vagrant ssh -c "mkdir -p /devbox_data/ruby/cli/assets/ && cd /devbox_data/ruby/cli/assets/ && sudo rm -f ./auth*.jar && sudo cp /esxcloud/java/auth-tool/build/libs/auth*.jar ." -- -T
+    vagrant ssh -c "mkdir -p /devbox_data/ruby/cli/assets/ && cd /devbox_data/ruby/cli/assets/ && sudo rm -f ./auth*.jar && sudo cp /devbox_data/java/auth-tool/build/libs/auth*.jar ." -- -T
 fi
 
-# Register real agent to devbox
-if [ -n "$REAL_AGENT" ]; then
-  echo vagrant ssh -c "sudo WORKSPACE=/esxcloud DEVBOX_PHOTON=1 /esxcloud/python/misc/register_agent $ESX_IP $PUBLIC_NETWORK_IP:13000 $ESX_DATASTORE" -- -T
-
-  if [ -z "$DEPLOYER_TEST" ]; then
-    (
-      cd $TESTS
-      bundle exec rake api:seed:host
-    )
-  fi
+if [ -n "$REAL_AGENT" ] && [ -z "$DEPLOYER_TEST" ]; then
+  (cd "$TESTS" && bundle exec rake api:seed:host)
 fi
-# sleep 30 seconds to wait for root-scheduler to get live child
-sleep 30
