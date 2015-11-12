f21d239da0d5bd7324d03a908a8e1025b911d452
Matt Olenik <molenik@vmware.com>
2015-11-11 16:37:10 -0800

    Gradle based container build
    This change reworks the way devbox is used to build containers for
    Photon Controller services. Rather than keeping container image build
    logic deep within the Vagrantfile for devbox, containers can now be
    built using Gradle tasks for each service. E.g. "./gradlew
    :deployer:container" will build the image for deployer _into_ a running
    devbox using the Docker remote API. The command "./gradlew
    :deployer:start" can be used to start the container.
    
    Convenience targets are provided to build and bring up all services for
    you. Rather than "vagrant up", you should now run:
    
    ./gradlew :devbox:buildAll :devbox:startAll
    
    from the Java directory. Containers can be re-built and re-started into
    a running devbox for quicker turnaround when testing.
    
    In short, reduce the Vagrantfile so that "vagrant up" creates a skeleton
    management VM with no running services. Then, build/deploy containers
    from host machine _into_ devbox using Docker remote API. Lastly,
    start/stop services using Gradle commands. This model decouples the
    container build logic from the devbox logic, making the devbox is no
    different than any other Docker endpoint. This allows us to build
    containers directly into a running management VM by setting DOCKER_URL
    before invoking Gradle. DOCKER_URL can also be set to a Docker Machine
    VM.
    
    Change-Id: I74e8d9823926aeb40bf750d5c55638ef7e472a7d

--- devbox-photon/prepare-devbox-deployment.sh
+++ devbox-photon/prepare-devbox-deployment.sh
@@ -2,58 +2,28 @@
 
 MANAGEMENT_VM_DIR="./management-vm"
 EXPORTED_OVA_FILE="exported-photon-management-vm.ova"
-OUTPUT_OVA_FILE="photon-management-vm.ova"
-OUTPUT_MF_FILE="photon-management-vm.mf"
 OUTPUT_OVF_FILE="photon-management-vm.ovf"
-OUTPUT_VMDK_FILE="photon-management-vm-disk1.vmdk"
 
-function remove_if_exists() {
-  if [ -f "$*" ]
-  then
-    rm -f "$*"
-  fi
-}
+rm -rf "$MANAGEMENT_VM_DIR"
+mkdir -p "$MANAGEMENT_VM_DIR"
 
-if [ ! -d $MANAGEMENT_VM_DIR ]
-then
-  mkdir $MANAGEMENT_VM_DIR
-else
-  remove_if_exists "$MANAGEMENT_VM_DIR/$EXPORTED_OVA_FILE"
-  remove_if_exists "$MANAGEMENT_VM_DIR/$OUTPUT_OVF_FILE"
-  remove_if_exists "$MANAGEMENT_VM_DIR/$OUTPUT_VMDK_FILE"
-  remove_if_exists "$MANAGEMENT_VM_DIR/$OUTPUT_MF_FILE"
-  remove_if_exists "$MANAGEMENT_VM_DIR/$OUTPUT_OVA_FILE"
-fi
-
-# Use ovftool to convert to vmware friendly format
-# This will generate vmdk, mf and ovf
-# Find `ovftool` in PATH if needed
-if [ -z "$RUN_ON_MAC" ]
-then
-  image_vsphere_ovf_ovftool_path=$(which ovftool)
-else
-  image_vsphere_ovf_ovftool_path="$OVFTOOL_FULL_PATH"
-fi
-
-if [ -z "$image_vsphere_ovf_ovftool_path" ]
-then
+ovftool=${OVFTOOL_FULL_PATH:="$(which ovftool)"}
+if [ ! -f "$ovftool" ]; then
   echo "ERROR: ovftool not found"
   exit -1
 fi
 
 export NO_RESTART_ALWAYS=1
 
-vagrant destroy -f
+./gradlew :devbox:renew
 
-vagrant up
-
-vagrant ssh -c "docker tag devbox/zookeeper esxcloud/zookeeper"
-vagrant ssh -c "docker tag devbox/haproxy esxcloud/haproxy"
-vagrant ssh -c "docker tag devbox/deployer esxcloud/deployer"
-vagrant ssh -c "docker tag devbox/cloud_store esxcloud/cloud-store"
-vagrant ssh -c "docker tag devbox/management_api esxcloud/management-api"
-vagrant ssh -c "docker tag devbox/root_scheduler esxcloud/root-scheduler"
-vagrant ssh -c "docker tag devbox/housekeeper esxcloud/housekeeper"
+vagrant ssh -c "docker tag photon/zookeeper esxcloud/zookeeper"
+vagrant ssh -c "docker tag photon/haproxy esxcloud/haproxy"
+vagrant ssh -c "docker tag photon/deployer esxcloud/deployer"
+vagrant ssh -c "docker tag photon/cloud-store esxcloud/cloud-store"
+vagrant ssh -c "docker tag photon/management-api esxcloud/management-api"
+vagrant ssh -c "docker tag photon/root-scheduler esxcloud/root-scheduler"
+vagrant ssh -c "docker tag photon/housekeeper esxcloud/housekeeper"
 
 mgmt_ui_container_url="https://ci.ec.eng.vmware.com/view/UI/job/ec-ui-mgmt-publish-docker-image-develop/lastSuccessfulBuild/artifact/ci/docker-image/esxcloud-management-ui.tar"
 container_tar=$(basename "$mgmt_ui_container_url")
@@ -64,10 +34,9 @@ vagrant ssh -c "rm -f \"$container_tar\""
 #
 # Copy the config files and scripts
 #
-vagrant ssh -c "sudo docker cp devbox_deployer_container:/etc/esxcloud-deployer/ /etc/"
+vagrant ssh -c "sudo docker cp deployer:/etc/esxcloud-deployer/ /etc/"
 vagrant ssh -c "sudo mkdir -p /usr/lib/esxcloud/deployer/"
-vagrant ssh -c "sudo docker cp devbox_deployer_container:/usr/lib/esxcloud/deployer/scripts/ /usr/lib/esxcloud/deployer/"
-vagrant ssh -c "sudo chmod +x /usr/lib/esxcloud/deployer/scripts/"
+vagrant ssh -c "sudo docker cp deployer:/usr/lib/esxcloud/deployer/scripts/ /usr/lib/esxcloud/deployer/"
 
 photon_vm=$(VBoxManage list runningvms | grep devbox-photon_photon | sed 's/"\(.*\)".*/\1/')
 #
@@ -78,62 +47,19 @@ sleep 120
 
 vagrant suspend
 
-VBoxManage export ${photon_vm} -o ../$EXPORTED_OVA_FILE
+VBoxManage export "$photon_vm" -o "../$EXPORTED_OVA_FILE"
 
-mv ../$EXPORTED_OVA_FILE $MANAGEMENT_VM_DIR/
+mv "../$EXPORTED_OVA_FILE" "$MANAGEMENT_VM_DIR/"
 
-# Use ovftool to convert to vmware friendly format
-# This will generate vmdk, mf and ovf
-# Find `ovftool` in PATH if needed
-if [ -z "$RUN_ON_MAC" ]
-then
-  image_vsphere_ovf_ovftool_path=$(which ovftool)
-else
-  image_vsphere_ovf_ovftool_path="$OVFTOOL_FULL_PATH"
-fi
-
-# Abort when $image_vsphere_ovf_ovftool_path is empty
-if [ -z "${image_vsphere_ovf_ovftool_path:-}" ]
-then
-  echo "image_vsphere_ovf_ovftool_path is empty"
-  exit -1
-fi
 
 cd $MANAGEMENT_VM_DIR
 
-"$image_vsphere_ovf_ovftool_path" --lax $EXPORTED_OVA_FILE $OUTPUT_OVF_FILE
-if [ -z "$RUN_ON_MAC" ]
-then
-  # get the old sha1 of the ovf file
-  OLD_OVF_SHA=$(sha1sum $OUTPUT_OVF_FILE | cut -d ' ' -f 1)
-else
-  # get the old sha1 of the ovf file
-  OLD_OVF_SHA=$(openssl sha1 $OUTPUT_OVF_FILE | cut -d ' ' -f 2)
-fi
-
-# Overwrite VirtualBox related stuff with vmware related values
-sed -i.bak 's/virtualbox-2.2/vmx-04 vmx-06 vmx-07 vmx-09/' $OUTPUT_OVF_FILE
-sed -i.bak 's/<rasd:Caption>sataController0/<rasd:Caption>SCSIController/' $OUTPUT_OVF_FILE
-sed -i.bak 's/<rasd:Description>SATA Controller/<rasd:Description>SCSI Controller/' $OUTPUT_OVF_FILE
-sed -i.bak 's/<rasd:ElementName>sataController0/<rasd:ElementName>SCSIController/' $OUTPUT_OVF_FILE
-sed -i.bak 's/<rasd:ResourceSubType>AHCI/<rasd:ResourceSubType>lsilogic/' $OUTPUT_OVF_FILE
-sed -i.bak 's/<rasd:ResourceType>20</<rasd:ResourceType>6</' $OUTPUT_OVF_FILE
-
-if [ -z "$RUN_ON_MAC" ]
-then
-  # update the sha1 to match the new ovf file
-  NEW_OVF_SHA=$(sha1sum $OUTPUT_OVF_FILE | cut -d ' ' -f 1)
-else
-  # update the sha1 to match the new ovf file
-  NEW_OVF_SHA=$(openssl sha1 $OUTPUT_OVF_FILE | cut -d ' ' -f 2)
-fi
-sed -i.bak "s/$OLD_OVF_SHA/$NEW_OVF_SHA/" $OUTPUT_MF_FILE
-
-tar cvf $OUTPUT_OVA_FILE $OUTPUT_OVF_FILE $OUTPUT_VMDK_FILE $OUTPUT_MF_FILE
-rm *.ova
-rm *.ovf
-rm *.mf
-rm *.bak
+# Extract vmdk
+"$ovftool" --lax $EXPORTED_OVA_FILE $OUTPUT_OVF_FILE
+# Remove unused files
+rm -f -- *.ova
+rm -f -- *.ovf
+rm -f -- *.mf
 cd ..
 
 vagrant up --no-provision
@@ -141,4 +67,4 @@ vagrant ssh -c "sudo mkdir -p /var/photon/images"
 vagrant ssh -c "sudo chown vagrant /var/photon/images"
 vagrant ssh -c "sudo cp /vagrant/$MANAGEMENT_VM_DIR/photon-management-vm-disk1.vmdk /var/photon/images/"
 
-echo "Dev Box is ready for deployment"
+echo "Devbox is ready for deployment"
