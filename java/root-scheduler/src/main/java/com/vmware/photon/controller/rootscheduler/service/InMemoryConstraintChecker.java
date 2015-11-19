2225301c35b0460c1037e58e56d8a49ead0cef5b
Michi Mutsuzaki <mmutsuzaki@vmware.com>
2015-11-19 15:30:16 -0800

    Add a prototype flat scheduler.
    Change-Id: I383d8fdeb5679c9af109d018743cbe18109fadc3

--- java/root-scheduler/src/main/java/com/vmware/photon/controller/rootscheduler/service/InMemoryConstraintChecker.java
+++ java/root-scheduler/src/main/java/com/vmware/photon/controller/rootscheduler/service/InMemoryConstraintChecker.java
@@ -25,7 +25,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
-import java.util.Random;
 
 /**
  * An immutable, in-memory implementation of {@link ConstraintChecker}.
