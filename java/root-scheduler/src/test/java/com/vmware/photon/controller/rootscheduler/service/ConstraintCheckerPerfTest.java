61f06c074453ee9bbc6f2a1a34f4771dcdc87546
Michi Mutsuzaki <mmutsuzaki@vmware.com>
2015-11-19 15:30:16 -0800

    Add a prototype flat scheduler.
    Change-Id: I2ef4cdc4504a8defb3fc61b5b6ca3b04bcd0acb7

--- java/root-scheduler/src/test/java/com/vmware/photon/controller/rootscheduler/service/ConstraintCheckerPerfTest.java
+++ java/root-scheduler/src/test/java/com/vmware/photon/controller/rootscheduler/service/ConstraintCheckerPerfTest.java
@@ -26,6 +26,7 @@ import com.vmware.photon.controller.resource.gen.ResourceConstraintType;
 import com.vmware.xenon.common.Operation;
 
 import com.google.common.base.Stopwatch;
+import com.vmware.xenon.common.Operation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
