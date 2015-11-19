0af048043145ddf5fec0b329c2661ddfc7d0e956
sufiand <dars@vmware.com>
2015-11-18 19:34:37 -0800

    Photon-Model: Move tests to use VerificationHost
    Change-Id: I3ac212577479027ac7e46608a907b226bb3bab19

--- java/photon-model/src/test/java/com/vmware/photon/controller/model/helpers/BaseModelTest.java
+++ java/photon-model/src/test/java/com/vmware/photon/controller/model/helpers/BaseModelTest.java
@@ -13,8 +13,18 @@
 
 package com.vmware.photon.controller.model.helpers;
 
+import com.vmware.photon.controller.model.resources.ComputeDescriptionFactoryService;
+
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * Abstract base class that creates a DCP ServiceHost
@@ -22,15 +32,21 @@ import org.testng.annotations.BeforeClass;
  */
 public abstract class BaseModelTest {
 
-  public static final int HOST_COUNT = 1;
-  public static final String TAG = "REUSE_HOST";
+  private static final int HOST_PORT = 0;
+  private static final Logger logger = LoggerFactory.getLogger(BaseModelTest.class);
 
-  protected TestEnvironment machine;
+  protected TestHost machine;
+  private Path sandboxDirectory;
+
+  public static final Class[] FACTORY_SERVICES = {
+      ComputeDescriptionFactoryService.class,
+  };
 
   @BeforeClass
   public void setUpClass() throws Throwable {
     if (machine == null) {
-      machine = new TestEnvironment(HOST_COUNT);
+      sandboxDirectory = Files.createTempDirectory(null);
+      machine = new TestHost(HOST_PORT, sandboxDirectory, FACTORY_SERVICES);
       machine.start();
     }
   }
@@ -38,8 +54,16 @@ public abstract class BaseModelTest {
   @AfterClass
   public void tearDownClass() throws Throwable {
     if (machine != null) {
-      machine.stop();
+      machine.tearDown();
       machine = null;
+    }
+    File sandbox = new File(sandboxDirectory.toUri());
+    if (sandbox.exists()) {
+      try {
+        FileUtils.forceDelete(sandbox);
+      } catch (FileNotFoundException | IllegalArgumentException ex) {
+        logger.debug("Sandbox file was not found");
+      }
     }
   }
 }
