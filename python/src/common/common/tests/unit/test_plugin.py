169a18241667bb5c3053cf5fcb050c2cec70b1c6
Anfernee Gui <agui@vmware.com>
2015-11-24 15:31:42 -0800

    agent: Refactor agent plugin
    - Add 2 phases loading: init, start. Init to be the phase when a plugin
      reads configuration and register the services it provides. Start to
      be the phase when the agent starts working.
    - Support plugin to have no thrift service exposed, only one or more
      backend threads running.
    
    Change-Id: Ib0eed07e72553869b25a2601c5854ddb87545ecd

---
+++ python/src/common/common/tests/unit/test_plugin.py
@@ -0,0 +1,86 @@
+# Copyright 2015 VMware, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy
+# of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, without
+# warranties or conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the
+# License for then specific language governing permissions and limitations
+# under the License.
+import threading
+import unittest
+
+from hamcrest import *  # noqa
+from matchers import *  # noqa
+
+
+from gen.resource.ttypes import Host
+from common.plugin import Plugin, ThriftService
+
+from mock import MagicMock
+
+
+service = ThriftService(
+    name="Host",
+    service=Host,
+    handler=MagicMock(),
+    num_threads=1,
+)
+
+
+class SamplePlugin(Plugin):
+    """ Sample plugin for testing
+    """
+
+    def __init__(self):
+        super(SamplePlugin, self).__init__("sample")
+
+
+class TestPlugin(unittest.TestCase):
+
+    def test_add_remove_service(self):
+        plugin = SamplePlugin()
+
+        # Add the first service
+        plugin.add_thrift_service(service)
+        assert_that(plugin.thrift_services, has_length(1))
+
+        # Add the same service
+        plugin.add_thrift_service(service)
+        assert_that(plugin.thrift_services, has_length(1))
+
+        # Remove the service
+        plugin.remove_thrift_service(service)
+        assert_that(plugin.thrift_services, has_length(0))
+
+    def test_add_bad_service(self):
+        plugin = SamplePlugin()
+
+        # Not allow to add a string as service
+        self.assertRaises(AssertionError,
+                          plugin.add_thrift_service, "Sample Service")
+
+    def test_add_remove_worker(self):
+        plugin = SamplePlugin()
+        worker = threading.Thread()
+
+        # Add the first worker
+        plugin.add_backend_worker(worker)
+        assert_that(plugin.backend_workers, has_length(1))
+
+        # Add the same worker
+        plugin.add_backend_worker(worker)
+        assert_that(plugin.backend_workers, has_length(1))
+
+        # Remove the worker
+        plugin.remove_backend_worker(worker)
+        assert_that(plugin.backend_workers, has_length(0))
+
+    def test_add_bad_worker(self):
+        plugin = SamplePlugin()
+
+        # Not allow to add a string as worker
+        self.assertRaises(AssertionError,
+                          plugin.add_backend_worker, "Sample Service")
