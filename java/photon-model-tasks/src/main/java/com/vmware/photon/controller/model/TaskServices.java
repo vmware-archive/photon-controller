abb45cb72e1d3f52c064622753841068a6cf30f5
Long Zhou <longz@vmware.com>
2015-11-25 10:05:55 -0800

    Adding ResourceAllocationTaskService
    Change-Id: Ife1efdc27077f4663f37a31aceb3fdbf0c7dc100

--- java/photon-model-tasks/src/main/java/com/vmware/photon/controller/model/TaskServices.java
+++ java/photon-model-tasks/src/main/java/com/vmware/photon/controller/model/TaskServices.java
@@ -14,6 +14,7 @@
 package com.vmware.photon.controller.model;
 
 import com.vmware.photon.controller.model.tasks.ProvisionComputeTaskFactoryService;
+import com.vmware.photon.controller.model.tasks.ResourceAllocationTaskFactoryService;
 import com.vmware.photon.controller.model.tasks.ResourceRemovalTaskFactoryService;
 
 /**
@@ -22,6 +23,7 @@ import com.vmware.photon.controller.model.tasks.ResourceRemovalTaskFactoryServic
 public class TaskServices {
   public static final Class[] FACTORIES = {
       ProvisionComputeTaskFactoryService.class,
+      ResourceAllocationTaskFactoryService.class,
       ResourceRemovalTaskFactoryService.class
   };
 }
