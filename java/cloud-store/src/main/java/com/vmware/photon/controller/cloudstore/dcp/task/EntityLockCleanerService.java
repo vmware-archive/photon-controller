2576ab3f07f2f842123f5a3a9eb25713b3e8fba7
Touseef Liaqat <tliaqat@vmware.com>
2015-11-24 16:24:57 -0800

    Add default delete watermark time in EntityLockCleaner
    Change-Id: I63aefb4bb57623f05981a4e6c2c9220222363b65

--- java/cloud-store/src/main/java/com/vmware/photon/controller/cloudstore/dcp/task/EntityLockCleanerService.java
+++ java/cloud-store/src/main/java/com/vmware/photon/controller/cloudstore/dcp/task/EntityLockCleanerService.java
@@ -52,6 +52,7 @@ import java.util.Map;
 public class EntityLockCleanerService extends StatefulService {
 
   public static final Integer ENTITY_LOCK_DEFAULT_PAGE_LIMIT = 1000;
+  public static final Long ENTITY_LOCK_DEFAULT_DELETE_WATERMARK_TIME_MILLIS = (long) (5 * 60 * 1000);
   private static final String DOCUMENT_UPDATE_TIME_MICROS = "documentUpdateTimeMicros";
 
   public EntityLockCleanerService() {
@@ -94,6 +95,10 @@ public class EntityLockCleanerService extends StatefulService {
    */
   private void initializeState(State current) {
     InitializationUtils.initialize(current);
+    if (current.entityLockDeleteWatermarkTimeInMicros == null) {
+      current.entityLockDeleteWatermarkTimeInMicros =
+          EntityLockCleanerService.ENTITY_LOCK_DEFAULT_DELETE_WATERMARK_TIME_MILLIS;
+    }
 
     if (current.documentExpirationTimeMicros <= 0) {
       current.documentExpirationTimeMicros =
@@ -457,7 +462,6 @@ public class EntityLockCleanerService extends StatefulService {
     /**
      * Duration that controls how old the entity locks should be for cleaning.
      */
-    @DefaultLong(value = 0)
     public Long entityLockDeleteWatermarkTimeInMicros;
 
   }
