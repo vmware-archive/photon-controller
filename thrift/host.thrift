b2a0f3a545e7a909b9e9f1aca1c72911d4e6450d
Anfernee Gui <agui@vmware.com>
2015-11-30 17:48:29 -0800

    agent: Support host-to-host image metadata copy
    As part of receive_image request, add raw metadata and manifest
    information so they are correctly saved together with the image as
    source image does.
    
    copy_image will also copy image manifest file together with image.
    
    Change-Id: I03b11831d41b9612e9435d7b28ca9d3923a380c0

--- thrift/host.thrift
+++ thrift/host.thrift
@@ -632,6 +632,10 @@ struct ReceiveImageRequest {
   // The id to lookup transferred image data at the receiving host
   3: required string transferred_image_id
 
+  // Raw image metadata
+  4: required string metadata
+  5: required string manifest
+
   99: optional tracing.TracingInfo tracing_info
 }
 enum ReceiveImageResultCode {
