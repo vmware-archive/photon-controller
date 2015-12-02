a458dfa824de42bf07a597a65d88968a3299e805
Xuli Liu <xulil@vmware.com>
2015-12-01 17:52:13 -0800

    Prefix the page links with proper route path
    In this case, the returned page links can be directly used.
    
    Change-Id: I387fa6c5e5b3e317184a049435a2084119435c59

--- java/api-frontend/management/src/test/java/com/vmware/photon/controller/apife/utils/DataTypeConversionUtilsTest.java
+++ java/api-frontend/management/src/test/java/com/vmware/photon/controller/apife/utils/DataTypeConversionUtilsTest.java
@@ -22,6 +22,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -29,55 +30,113 @@ import java.util.UUID;
  * Tests for {@link DataTypeConversionUtils}.
  */
 public class DataTypeConversionUtilsTest {
-  private class ExampleData {
-    public String name;
+
+  @Test(enabled = false)
+  private void dummy() {
   }
 
-  @Test
-  public void testXenonQueryResultToResourceListEmptyDataSet() {
-    ResourceList<ExampleData> resourceList = DataTypeConversionUtils.xenonQueryResultToResourceList(ExampleData.class,
-        new ServiceDocumentQueryResult());
+  /**
+   * Tests for xenonQueryResultToResourceList.
+   */
+  public static class TestXenonQueryResultToResourceList {
 
-    assertThat(resourceList.getItems().size(), is(0));
-    assertThat(resourceList.getNextPageLink(), nullValue());
-    assertThat(resourceList.getPreviousPageLink(), nullValue());
-  }
+    private class ExampleData {
+      public String name;
+    }
 
-  @Test
-  public void testXenonQueryResultToResourceList() {
-    ServiceDocumentQueryResult queryResult = generateQueryResultData();
-    ResourceList<ExampleData> resourceList = DataTypeConversionUtils.xenonQueryResultToResourceList(
-        ExampleData.class, queryResult);
+    @Test
+    public void testXenonQueryResultToResourceListEmptyDataSet() {
+      ResourceList<ExampleData> resourceList = DataTypeConversionUtils.xenonQueryResultToResourceList(ExampleData.class,
+          new ServiceDocumentQueryResult());
 
-    assertThat(resourceList.getItems().size(), is(queryResult.documentLinks.size()));
-    for (int i = 0; i < queryResult.documentCount; i++) {
-      ExampleData document = Utils.fromJson(queryResult.documents.get(queryResult.documentLinks.get(i)),
-          ExampleData.class);
+      assertThat(resourceList.getItems().size(), is(0));
+      assertThat(resourceList.getNextPageLink(), nullValue());
+      assertThat(resourceList.getPreviousPageLink(), nullValue());
+    }
 
-      assertThat(resourceList.getItems().get(i).name, is(document.name));
+    @Test
+    public void testXenonQueryResultToResourceList() {
+      ServiceDocumentQueryResult queryResult = generateQueryResultData();
+      ResourceList<ExampleData> resourceList = DataTypeConversionUtils.xenonQueryResultToResourceList(
+          ExampleData.class, queryResult);
+
+      assertThat(resourceList.getItems().size(), is(queryResult.documentLinks.size()));
+      for (int i = 0; i < queryResult.documentCount; i++) {
+        ExampleData document = Utils.fromJson(queryResult.documents.get(queryResult.documentLinks.get(i)),
+            ExampleData.class);
+
+        assertThat(resourceList.getItems().get(i).name, is(document.name));
+      }
+    }
+
+    private ServiceDocumentQueryResult generateQueryResultData() {
+      ServiceDocumentQueryResult queryResult = new ServiceDocumentQueryResult();
+
+      final long documentCount = 100;
+      queryResult.documentCount = documentCount;
+      queryResult.documents = new HashMap<>();
+
+      for (int i = 0; i < documentCount; i++) {
+        String documentLink = UUID.randomUUID().toString();
+
+        ExampleData exampleData = new ExampleData();
+        exampleData.name = "document" + i;
+
+        queryResult.documentLinks.add(documentLink);
+        queryResult.documents.put(documentLink, Utils.toJson(exampleData));
+      }
+
+      queryResult.nextPageLink = UUID.randomUUID().toString();
+      queryResult.prevPageLink = UUID.randomUUID().toString();
+
+      return queryResult;
     }
   }
 
-  private ServiceDocumentQueryResult generateQueryResultData() {
-    ServiceDocumentQueryResult queryResult = new ServiceDocumentQueryResult();
+  /**
+   * Tests for formalizePageLinks method.
+   */
+  public static class TestFormalizePageLinks {
 
-    final long documentCount = 100;
-    queryResult.documentCount = documentCount;
-    queryResult.documents = new HashMap<>();
+    @Test
+    public void testPageLinksNull() {
+      String route = "/tasks";
+      ResourceList<Object> resourceList = DataTypeConversionUtils.formalizePageLinks(
+          new ResourceList<>(Collections.emptyList(), null, null), route);
 
-    for (int i = 0; i < documentCount; i++) {
-      String documentLink = UUID.randomUUID().toString();
-
-      ExampleData exampleData = new ExampleData();
-      exampleData.name = "document" + i;
-
-      queryResult.documentLinks.add(documentLink);
-      queryResult.documents.put(documentLink, Utils.toJson(exampleData));
+      assertThat(resourceList.getNextPageLink(), nullValue());
+      assertThat(resourceList.getPreviousPageLink(), nullValue());
     }
+    @Test
+    public void testNextPageLinkWithValue() {
+      String route = "/tasks";
+      String pageLink = UUID.randomUUID().toString();
+      ResourceList<Object> resourceList = DataTypeConversionUtils.formalizePageLinks(
+          new ResourceList<>(Collections.emptyList(), pageLink, null), route);
 
-    queryResult.nextPageLink = UUID.randomUUID().toString();
-    queryResult.prevPageLink = UUID.randomUUID().toString();
+      assertThat(resourceList.getNextPageLink().startsWith(route + "?pageLink="), is(true));
+      assertThat(resourceList.getPreviousPageLink(), nullValue());
+    }
+    @Test
+    public void testPrevPageLinkWithValue() {
+      String route = "/tasks";
+      String pageLink = UUID.randomUUID().toString();
+      ResourceList<Object> resourceList = DataTypeConversionUtils.formalizePageLinks(
+          new ResourceList<>(Collections.emptyList(), null, pageLink), route);
 
-    return queryResult;
+      assertThat(resourceList.getNextPageLink(), nullValue());
+      assertThat(resourceList.getPreviousPageLink().startsWith(route + "?pageLink="), is(true));
+    }
+    @Test
+    public void testBothPageLinksWithValues() {
+      String route = "/tasks";
+      String pageLink1 = UUID.randomUUID().toString();
+      String pageLink2 = UUID.randomUUID().toString();
+      ResourceList<Object> resourceList = DataTypeConversionUtils.formalizePageLinks(
+          new ResourceList<>(Collections.emptyList(), pageLink1, pageLink2), route);
+
+      assertThat(resourceList.getNextPageLink().startsWith(route + "?pageLink="), is(true));
+      assertThat(resourceList.getPreviousPageLink().startsWith(route + "?pageLink="), is(true));
+    }
   }
 }
