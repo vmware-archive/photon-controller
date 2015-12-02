6593e3f93e13a8f5a692adff23e4048c9a2466e4
sufiand <dars@vmware.com>
2015-12-01 18:02:41 -0800

    photon-model-adapters
    Change-Id: Ie370d7271b764a2e672210346bfd29f41946a778

--- java/photon-model-adapters/aws/src/main/java/com/vmware/photon/controller/model/adapters/aws/AWSStages.java
+++ java/photon-model-adapters/aws/src/main/java/com/vmware/photon/controller/model/adapters/aws/AWSStages.java
@@ -11,15 +11,20 @@
  * specific language governing permissions and limitations under the License.
  */
 
-  package com.vmware.photon.controller.model;
+package com.vmware.photon.controller.model.adapters.aws;
 
 /**
- * ServiceUriPaths
- *
- * Service paths used by the photon model.
+ * Defines AWS stages.
  */
-public class UriPaths {
-  public static final String PROVISIONING = "/provisioning";
-  public static final String RESOURCES = "/resources";
-  public static final String RESOURCES_NETWORKS = RESOURCES + "/networks";
+public enum AWSStages {
+  VMDESC,
+  PARENTDESC,
+  PARENTAUTH,
+  CLIENT,
+  DELETE,
+  CREATE,
+  VMDISKS,
+  FIREWALL,
+  DONE,
+  ERROR
 }
