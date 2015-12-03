4da01f84f4c0e707252e35ede35053184ba0d2a1
Jake Dsouza <jaked@vmware.com>
2015-12-03 15:12:17 -0800

    photon-model-tasks Adding ProvisionNetworkTaskService
    - Added ProvisionNetworkTaskService and ProvisionNetworkTaskFactoryService
    - Added Unit tests for ProvisionNetworkTaskService
      and ProvisionNetworkTaskFactoryService
    
    Change-Id: I65375795db5595bf7aa06493d800a681f97ceb60

---
+++ java/photon-model-tasks/src/test/java/com/vmware/photon/controller/model/tasks/ProvisionNetworkTaskServiceTest.java
@@ -0,0 +1,231 @@
+/*
+ * Copyright 2015 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.photon.controller.model.tasks;
+
+import com.vmware.photon.controller.model.ModelServices;
+import com.vmware.photon.controller.model.TaskServices;
+import com.vmware.photon.controller.model.adapterapi.NetworkInstanceRequest;
+import com.vmware.photon.controller.model.helpers.BaseModelTest;
+import com.vmware.photon.controller.model.helpers.TestHost;
+import com.vmware.xenon.common.Service;
+import com.vmware.xenon.common.TaskState;
+import com.vmware.xenon.common.UriUtils;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+
+/**
+ * This class implements tests for the {@link ProvisionNetworkTaskService} class.
+ */
+public class ProvisionNetworkTaskServiceTest {
+
+  private static ProvisionNetworkTaskService.ProvisionNetworkTaskState buildValidStartState(
+      TestHost host,
+      NetworkInstanceRequest.InstanceRequestType requestType) {
+    ProvisionNetworkTaskService.ProvisionNetworkTaskState startState =
+        new ProvisionNetworkTaskService.ProvisionNetworkTaskState();
+
+    startState.requestType = requestType;
+    startState.authCredentialsLink = "http://authCredentialsLink";
+    startState.resourcePoolLink = "http://resourcePoolLink";
+    startState.networkDescriptionLink = "http://networkDescriptionLink";
+    startState.isMockRequest = true;
+    startState.networkServiceReference = UriUtils.buildUri(
+        host,
+        MockAdapter.MockNetworkInstanceSuccessAdapter.SELF_LINK);
+    startState.firewallServiceReference = UriUtils.buildUri(
+        host,
+        MockAdapter.MockNetworkInstanceSuccessAdapter.SELF_LINK);
+    return startState;
+  }
+
+  private static ProvisionNetworkTaskService.ProvisionNetworkTaskState postAndWaitForService(
+      TestHost host,
+      ProvisionNetworkTaskService.ProvisionNetworkTaskState startState) throws Throwable {
+    ProvisionNetworkTaskService.ProvisionNetworkTaskState returnState = host.postServiceSynchronously(
+        ProvisionNetworkTaskFactoryService.SELF_LINK,
+        startState,
+        ProvisionNetworkTaskService.ProvisionNetworkTaskState.class);
+
+    ProvisionNetworkTaskService.ProvisionNetworkTaskState completeState = host.waitForServiceState(
+        ProvisionNetworkTaskService.ProvisionNetworkTaskState.class,
+        returnState.documentSelfLink,
+        state -> TaskState.TaskStage.FINISHED.ordinal() <= state.taskInfo.stage.ordinal());
+
+    return completeState;
+  }
+
+  private static Class[] getFactoryServices() {
+    List<Class> services = new ArrayList<>();
+    Collections.addAll(services, ModelServices.FACTORIES);
+    Collections.addAll(services, TaskServices.FACTORIES);
+    Collections.addAll(services, MockAdapter.FACTORIES);
+    return services.toArray(new Class[services.size()]);
+  }
+
+  @Test
+  private void dummy() {
+  }
+
+  /**
+   * This class implements tests for the constructor.
+   */
+  public class ConstructorTest {
+
+    private ProvisionNetworkTaskService provisionNetworkTaskService;
+
+    @BeforeMethod
+    public void setUpTest() {
+      provisionNetworkTaskService = new ProvisionNetworkTaskService();
+    }
+
+    @Test
+    public void testServiceOptions() {
+
+      EnumSet<Service.ServiceOption> expected = EnumSet.of(
+          Service.ServiceOption.PERSISTENCE,
+          Service.ServiceOption.REPLICATION,
+          Service.ServiceOption.OWNER_SELECTION);
+
+      assertThat(provisionNetworkTaskService.getOptions(), is(expected));
+      assertThat(provisionNetworkTaskService.getProcessingStage(), is(Service.ProcessingStage.CREATED));
+    }
+  }
+
+  /**
+   * This class implements tests for the {@link ProvisionNetworkTaskService#handleStart} method.
+   */
+  public class HandleStartTest extends BaseModelTest {
+    @Override
+    protected Class[] getFactoryServices() {
+      return ProvisionNetworkTaskServiceTest.getFactoryServices();
+    }
+
+    @Test
+    public void testValidateNetworkService() throws Throwable {
+      ProvisionNetworkTaskService.ProvisionNetworkTaskState startState = buildValidStartState(host,
+          NetworkInstanceRequest.InstanceRequestType.CREATE);
+      ProvisionNetworkTaskService.ProvisionNetworkTaskState completeState = postAndWaitForService(host, startState);
+      assertThat(completeState.taskInfo.stage, is(TaskState.TaskStage.FINISHED));
+    }
+
+    @DataProvider(name = "createMissingValues")
+    public Object[][] createMissingValues() {
+      ProvisionNetworkTaskService.ProvisionNetworkTaskState
+          invalidRequestType =
+          buildValidStartState(host, NetworkInstanceRequest.InstanceRequestType.CREATE),
+          invalidAuthCredentialsLink =
+              buildValidStartState(host, NetworkInstanceRequest.InstanceRequestType.CREATE),
+          invalidResourcePoolLink =
+              buildValidStartState(host, NetworkInstanceRequest.InstanceRequestType.CREATE),
+          invalidNetworkDescriptionLink =
+              buildValidStartState(host, NetworkInstanceRequest.InstanceRequestType.CREATE),
+          invalidNetworkServiceReference =
+              buildValidStartState(host, NetworkInstanceRequest.InstanceRequestType.CREATE),
+          invalidFirewallServiceReference =
+              buildValidStartState(host, NetworkInstanceRequest.InstanceRequestType.CREATE);
+
+      invalidRequestType.requestType = null;
+      invalidAuthCredentialsLink.authCredentialsLink = null;
+      invalidResourcePoolLink.resourcePoolLink = null;
+      invalidNetworkDescriptionLink.networkDescriptionLink = null;
+      invalidNetworkServiceReference.networkServiceReference = null;
+      invalidFirewallServiceReference.firewallServiceReference = null;
+
+      return new Object[][]{
+          {invalidRequestType},
+          {invalidAuthCredentialsLink},
+          {invalidResourcePoolLink},
+          {invalidNetworkDescriptionLink},
+          {invalidNetworkServiceReference},
+          {invalidFirewallServiceReference}
+      };
+    }
+
+    @Test(dataProvider = "createMissingValues")
+    public void testMissingValue(ProvisionNetworkTaskService.ProvisionNetworkTaskState startState) throws Throwable {
+      ProvisionNetworkTaskService.ProvisionNetworkTaskState returnState = host.postServiceSynchronously(
+          ProvisionNetworkTaskFactoryService.SELF_LINK,
+          startState,
+          ProvisionNetworkTaskService.ProvisionNetworkTaskState.class, IllegalArgumentException.class);
+    }
+  }
+
+  /**
+   * This class implements tests for the {@link ProvisionNetworkTaskService#handlePatch} method.
+   */
+  public class HandlePatchTest extends BaseModelTest {
+    @Override
+    protected Class[] getFactoryServices() {
+      return ProvisionNetworkTaskServiceTest.getFactoryServices();
+    }
+
+    @Test
+    public void testCreateNetworkSuccess() throws Throwable {
+
+      ProvisionNetworkTaskService.ProvisionNetworkTaskState startState = buildValidStartState(host,
+          NetworkInstanceRequest.InstanceRequestType.CREATE);
+
+      ProvisionNetworkTaskService.ProvisionNetworkTaskState completeState = postAndWaitForService(host, startState);
+
+      assertThat(completeState.taskInfo.stage, is(TaskState.TaskStage.FINISHED));
+    }
+
+    @Test
+    public void testDeleteNetworkSuccess() throws Throwable {
+      ProvisionNetworkTaskService.ProvisionNetworkTaskState startState = buildValidStartState(host,
+          NetworkInstanceRequest.InstanceRequestType.DELETE);
+
+      ProvisionNetworkTaskService.ProvisionNetworkTaskState completeState = postAndWaitForService(host, startState);
+
+      assertThat(completeState.taskInfo.stage, is(TaskState.TaskStage.FINISHED));
+    }
+
+    @Test
+    public void testCreateNetworkServiceAdapterFailure() throws Throwable {
+      ProvisionNetworkTaskService.ProvisionNetworkTaskState startState = buildValidStartState(host,
+          NetworkInstanceRequest.InstanceRequestType.CREATE);
+
+      startState.networkServiceReference = UriUtils.buildUri(
+          host,
+          MockAdapter.MockNetworkInstanceFailureAdapter.SELF_LINK);
+
+      ProvisionNetworkTaskService.ProvisionNetworkTaskState completeState = postAndWaitForService(host, startState);
+
+      assertThat(completeState.taskInfo.stage, is(TaskState.TaskStage.FAILED));
+    }
+
+    @Test
+    public void testCreateFirewallServiceAdapterFailure() throws Throwable {
+      ProvisionNetworkTaskService.ProvisionNetworkTaskState startState = buildValidStartState(host,
+          NetworkInstanceRequest.InstanceRequestType.CREATE);
+
+      startState.firewallServiceReference = UriUtils.buildUri(
+          host,
+          MockAdapter.MockNetworkInstanceFailureAdapter.SELF_LINK);
+
+      ProvisionNetworkTaskService.ProvisionNetworkTaskState completeState = postAndWaitForService(host, startState);
+
+      assertThat(completeState.taskInfo.stage, is(TaskState.TaskStage.FAILED));
+    }
+  }
+}
