f5b47ed7b06e30be4123af0f6445eee86b343787
Mihnea Olteanu <molteanu@vmware.com>
2015-12-09 13:44:31 -0800

    ruby: int: add test for VM with 2 nics on same network
    Change-Id: Ia2c37aba550b3ed070dae7aba63f3abd33cbf48d

--- ruby/integration_tests/spec/api/vm/vm_spec.rb
+++ ruby/integration_tests/spec/api/vm/vm_spec.rb
@@ -84,6 +84,25 @@ describe "vm", management: true, image: true do
     end
   end
 
+  it "should create a vm with multiple nics on same network", disable_for_cli_test: true do
+    begin
+      network1 = EsxCloud::SystemSeeder.instance.network!
+
+      vm_name = random_name("vm-")
+      create_vm(@project, name: vm_name, networks: [network1.id, network1.id])
+
+      vms = client.find_vms_by_name(@project.id, vm_name).items
+      expect(vms.size).to eq 1
+
+      networks = client.get_vm_networks(vms[0].id).network_connections
+      network_ids = networks.map { |n| n.network }
+      expect(network_ids).to contain_exactly(network1.id, network1.id)
+    ensure
+      ignoring_all_errors { vm.delete if vm }
+      ignoring_all_errors { network2.delete if network2 }
+    end
+  end
+
   it "should fail to delete vm with persistent disk attached" do
     persistent_disk = @project.create_disk(
         name: random_name("disk-"),
