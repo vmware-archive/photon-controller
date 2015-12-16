5230250495e2f90c011e3793036f6915c3ba350b
Jake Dsouza <jaked@vmware.com>
2015-12-15 17:25:20 -0800

    photon-model Adding FirewallService and NetworkService
    Moved over NetworkService and FirewallService along with unit tests
    
    Change-Id: If9101b02b473a5fa12935593d16ef604a7c43496

--- java/photon-model-tasks/src/main/java/com/vmware/photon/controller/model/TaskServices.java
+++ java/photon-model-tasks/src/main/java/com/vmware/photon/controller/model/TaskServices.java
@@ -14,6 +14,7 @@
 package com.vmware.photon.controller.model;
 
 import com.vmware.photon.controller.model.tasks.ProvisionComputeTaskFactoryService;
+import com.vmware.photon.controller.model.tasks.ProvisionFirewallTaskFactoryService;
 import com.vmware.photon.controller.model.tasks.ProvisionNetworkTaskFactoryService;
 import com.vmware.photon.controller.model.tasks.ResourceAllocationTaskFactoryService;
 import com.vmware.photon.controller.model.tasks.ResourceEnumerationTaskFactoryService;
@@ -27,6 +28,7 @@ import com.vmware.photon.controller.model.tasks.SshCommandTaskFactoryService;
 public class TaskServices {
   public static final Class[] FACTORIES = {
       ProvisionComputeTaskFactoryService.class,
+      ProvisionFirewallTaskFactoryService.class,
       ProvisionNetworkTaskFactoryService.class,
       ResourceAllocationTaskFactoryService.class,
       ResourceEnumerationTaskFactoryService.class,
