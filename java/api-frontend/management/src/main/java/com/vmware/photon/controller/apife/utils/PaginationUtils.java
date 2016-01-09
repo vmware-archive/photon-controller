3761cbd0ea98c95a052ffbd40138ac5b37022a32
Touseef Liaqat <tliaqat@vmware.com>
2016-01-09 00:43:29 -0800

    apife: Add page by page list for Hosts
    Change-Id: I59872e1228b7a667b6f3fbca8349c899555c3780

--- java/api-frontend/management/src/main/java/com/vmware/photon/controller/apife/utils/PaginationUtils.java
+++ java/api-frontend/management/src/main/java/com/vmware/photon/controller/apife/utils/PaginationUtils.java
@@ -23,6 +23,7 @@ import com.google.common.base.Optional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * A collection of tools that can be used to convert data from one type to another.
@@ -60,6 +61,39 @@ public class PaginationUtils {
   }
 
   /**
+   * Convert the data returned as ServiceDocumentQueryResult from Xenon to
+   * ResourceList, which is being used by api-fe.
+   * <p>
+   * The order of the data will be honored.
+   *
+   * @param documentType
+   * @param queryResult
+   * @param convert
+   * @param <T>
+   * @return
+   */
+  public static <T, S> ResourceList<T> xenonQueryResultToResourceList(Class<S> documentType,
+                                                                   ServiceDocumentQueryResult queryResult,
+                                                                   Function<S, T> convert) {
+
+    // The documents links stored in documentLinks are sorted while documents are not, and
+    // the following loop iterates on the documentLinks to preserve this order.
+    List<T> documents = new ArrayList<>();
+    if (queryResult.documentLinks != null) {
+      for (String link : queryResult.documentLinks) {
+        documents.add(convert.apply(Utils.fromJson(queryResult.documents.get(link), documentType)));
+      }
+    }
+
+    ResourceList<T> resourceList = new ResourceList<>();
+    resourceList.setItems(documents);
+    resourceList.setNextPageLink(queryResult.nextPageLink);
+    resourceList.setPreviousPageLink(queryResult.prevPageLink);
+
+    return resourceList;
+  }
+
+  /**
    * The page links returned from xenon cannot be directly used. This util
    * function is to formalize it to a format like "/tasks?pageLink=xxxxxx"
    * @param resourceList
@@ -68,12 +102,14 @@ public class PaginationUtils {
    * @return
    */
   public static <T> ResourceList<T> formalizePageLinks(ResourceList<T> resourceList, String apiRoute) {
-    if (resourceList.getNextPageLink() != null) {
-      resourceList.setNextPageLink(apiRoute + "?pageLink=" + resourceList.getNextPageLink());
-    }
+    if (resourceList != null) {
+      if (resourceList.getNextPageLink() != null) {
+        resourceList.setNextPageLink(apiRoute + "?pageLink=" + resourceList.getNextPageLink());
+      }
 
-    if (resourceList.getPreviousPageLink() != null) {
-      resourceList.setPreviousPageLink(apiRoute + "?pageLink=" + resourceList.getPreviousPageLink());
+      if (resourceList.getPreviousPageLink() != null) {
+        resourceList.setPreviousPageLink(apiRoute + "?pageLink=" + resourceList.getPreviousPageLink());
+      }
     }
 
     return resourceList;
