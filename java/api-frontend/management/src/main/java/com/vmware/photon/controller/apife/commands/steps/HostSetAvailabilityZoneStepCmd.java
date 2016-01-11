6e7326229315566523d77582212635af82d8ed8d
Umer Khan <ukhan@vmware.com>
2016-01-10 21:20:36 -0800

    api-fe: make a provision call after setting availability zone
    - Post-deployment provision call due to setting availability zone from none to specific
    
    Change-Id: I8fdde9ec1046d13623d50df36d7f5e928af886db

---
+++ java/api-frontend/management/src/main/java/com/vmware/photon/controller/apife/commands/steps/HostSetAvailabilityZoneStepCmd.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2015 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.photon.controller.apife.commands.steps;
+
+import com.vmware.photon.controller.api.Host;
+import com.vmware.photon.controller.apife.backends.HostBackend;
+import com.vmware.photon.controller.apife.backends.StepBackend;
+import com.vmware.photon.controller.apife.commands.tasks.TaskCommand;
+import com.vmware.photon.controller.apife.entities.HostEntity;
+import com.vmware.photon.controller.apife.entities.StepEntity;
+import com.vmware.photon.controller.apife.exceptions.external.HostNotFoundException;
+import com.vmware.photon.controller.apife.exceptions.external.HostSetAvailabilityZoneFailedException;
+import com.vmware.photon.controller.common.clients.exceptions.RpcException;
+
+import com.google.common.base.Preconditions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * StepCommand for setting host availability zone.
+ */
+public class HostSetAvailabilityZoneStepCmd extends StepCommand {
+
+  private static final Logger logger = LoggerFactory.getLogger(HostSetAvailabilityZoneStepCmd.class);
+
+  private final HostBackend hostBackend;
+
+  public HostSetAvailabilityZoneStepCmd(TaskCommand taskCommand,
+                                        StepBackend stepBackend,
+                                        StepEntity step,
+                                        HostBackend hostBackend) {
+    super(taskCommand, stepBackend, step);
+    this.hostBackend = hostBackend;
+  }
+
+  @Override
+  protected void execute() throws HostSetAvailabilityZoneFailedException, HostNotFoundException {
+
+    List<HostEntity> hostList = step.getTransientResourceEntities(Host.KIND);
+    Preconditions.checkArgument(hostList.size() == 1);
+    HostEntity hostEntity = hostList.get(0);
+
+    try {
+      taskCommand.getHostClient().setHostIp(hostEntity.getAddress());
+      taskCommand.getHostClient().provision(
+          taskCommand.getHostClient().getHostIp(),
+          taskCommand.getHostClient().getPort(),
+          hostEntity.getAvailabilityZone());
+
+      hostBackend.updateAvailabilityZone(hostEntity);
+
+    } catch (InterruptedException | RpcException e) {
+      HostSetAvailabilityZoneFailedException exception = new HostSetAvailabilityZoneFailedException(
+          hostEntity.getId(),
+          hostEntity.getAvailabilityZone(),
+          e);
+      logger.error(exception.getMessage());
+      throw exception;
+    }
+  }
+
+  @Override
+  protected void cleanup() {
+
+  }
+}
