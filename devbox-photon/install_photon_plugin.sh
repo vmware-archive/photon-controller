4179e106d8e52f3875ea1142ac7ca9bfa766129d
Benjamin Wang <greencacti@gmail.com>
2016-01-11 16:56:00 +0800

    remove --no-proxy to make http proxy work

--- devbox-photon/install_photon_plugin.sh
+++ devbox-photon/install_photon_plugin.sh
@@ -20,14 +20,14 @@ fi
 if [ -z "$(vagrant plugin list | grep vagrant-guests-photon)" ]
 then
   echo "Installing Photon plugin for Vagrant."
-  wget --no-proxy $photon_vagrant_plugin_url
+  wget $photon_vagrant_plugin_url
   vagrant plugin install vagrant-guests-photon-0.0.1.gem
   rm vagrant-guests-photon-0.0.1.gem
 elif [ ! -z "$DEVBOX_FORCE_PHOTON_PLUGIN_REINSTALL" ]
 then
   echo "Forcing photon plugin reinstall"
   vagrant plugin uninstall vagrant-guests-photon
-  wget --no-proxy $photon_vagrant_plugin_url
+  wget $photon_vagrant_plugin_url
   vagrant plugin install vagrant-guests-photon-0.0.1.gem
   rm vagrant-guests-photon-0.0.1.gem
 else
--- devbox-photon/update_dependencies.sh
+++ devbox-photon/update_dependencies.sh
@@ -3,14 +3,14 @@
 if [ -z "$(vagrant plugin list | grep vagrant-guests-photon)" ]
 then
   echo "Installing Photon plugin for Vagrant."
-  wget --no-proxy https://bintray.com/artifact/download/photon-controller/public/27/resource/vagrant-guests-photon-0.0.1.gem
+  wget https://bintray.com/artifact/download/photon-controller/public/27/resource/vagrant-guests-photon-0.0.1.gem
   vagrant plugin install vagrant-guests-photon-0.0.1.gem
   rm vagrant-guests-photon-0.0.1.gem
 elif [ ! -z "$DEVBOX_FORCE_PHOTON_PLUGIN_REINSTALL" ]
 then
   echo "Forcing photon plugin reinstall"
   vagrant plugin uninstall vagrant-guests-photon
-  wget --no-proxy https://bintray.com/artifact/download/photon-controller/public/27/resource/vagrant-guests-photon-0.0.1.gem
+  wget https://bintray.com/artifact/download/photon-controller/public/27/resource/vagrant-guests-photon-0.0.1.gem
   vagrant plugin install vagrant-guests-photon-0.0.1.gem
   rm vagrant-guests-photon-0.0.1.gem
 else
