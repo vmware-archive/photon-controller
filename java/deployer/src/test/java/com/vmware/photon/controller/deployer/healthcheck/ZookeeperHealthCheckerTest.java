7fe80c6ca4b89e1b40d3d7bc22d0c029fdf7c68d
Adrian Schroeter <aschroeter@vmware.com>
2016-02-03 10:39:44 -0800

    deployer: bind zookeeper test instance to random port
    since we are running java tests in parallel we can end up trying to bind to a taken port.
    instead of using a fixed port we try n random ports before giving up.
    
    Change-Id: Ied6b307b12cf3b0731106fe1540bf17b36b48050

--- java/deployer/src/test/java/com/vmware/photon/controller/deployer/healthcheck/ZookeeperHealthCheckerTest.java
+++ java/deployer/src/test/java/com/vmware/photon/controller/deployer/healthcheck/ZookeeperHealthCheckerTest.java
@@ -15,22 +15,34 @@ package com.vmware.photon.controller.deployer.healthcheck;
 
 import com.vmware.photon.controller.deployer.dcp.constant.ServicePortConstants;
 
-import org.apache.curator.test.TestingServer;
-import org.testng.annotations.Test;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
+
+import org.apache.curator.test.TestingServer;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Random;
 
 /**
  * Implements tests for {@link ZookeeperHealthChecker}.
  */
 public class ZookeeperHealthCheckerTest {
 
+  public static final int startPort = 10000;
+  public static final int retries = 10;
+  public int port;
+
+  @BeforeMethod
+  public void beforeMethod() {
+    port = startPort;
+  }
+
   @Test
   public void testZookeeperRunning() throws Throwable {
-    try (TestingServer zookeeper = new TestingServer(ServicePortConstants.ZOOKEEPER_PORT)) {
-
+    try (TestingServer zookeeper = startZookeeper()) {
       HealthChecker zookeeperHealthChecker = new ZookeeperHealthChecker(
-          "127.0.0.1", ServicePortConstants.ZOOKEEPER_PORT);
+          "127.0.0.1", port);
       boolean response = zookeeperHealthChecker.isReady();
 
       assertTrue(response);
@@ -44,5 +56,22 @@ public class ZookeeperHealthCheckerTest {
     boolean response = zookeeperHealthChecker.isReady();
 
     assertFalse(response);
+  }
+
+  private TestingServer startZookeeper() {
+    int retriesLeft = retries;
+    Random random = new Random();
+    while (retriesLeft > 0) {
+      try {
+        return new TestingServer(port);
+      } catch(Throwable ignore) {
+        retriesLeft--;
+        port = startPort + random.nextInt(10000);
+        if (retries > 0) {
+          throw new RuntimeException(ignore);
+        }
+      }
+    }
+    return null;
   }
 }
