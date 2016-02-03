3657c23021582f788b562b8a6b96a93a296cc4b0
Varnika Goyal <goyalv@vmware.com>
2016-02-03 10:32:00 -0800

    run_cli cmd for creating, deleting, finding disk
    Change-Id: I7daa19b615837439d3ebb2f749ce0db4ac258fed

--- ruby/common/lib/common/go_cli_client/disk_api.rb
+++ ruby/common/lib/common/go_cli_client/disk_api.rb
@@ -17,13 +17,38 @@ module EsxCloud
       # @param [Hash] payload
       # @return [Disk]
       def create_disk(project_id, payload)
-        @api_client.create_disk(project_id, payload)
+        project = find_project_by_id(project_id)
+        tenant = @project_to_tenant[project.id]
+
+        cmd = "disk create -t '#{tenant.name}' -p '#{project.name}' -n '#{payload[:name]}' -f '#{payload[:flavor]}' -c '#{payload[:capacityGb]}'"
+
+        if payload[:affinities] && !payload[:affinities].empty?
+          affinities = payload[:affinities].map do |affinities|
+            "#{affinities[:kind]} #{affinities[:id]}"
+          end.join(", ")
+          cmd += " -a '#{affinities}'"
+        end
+
+        if payload[:tags] && !payload[:tags].empty?
+          tags = payload[:tags].map do |tag|
+            "#{tag}"
+          end.join(", ")
+          cmd += " -s '#{tags}'"
+        end
+
+        disk_id = run_cli(cmd)
+
+        disk = find_disk_by_id(disk_id)
+        @disk_to_project[disk.id] = project
+
+        disk
       end
 
       # @param [String] id
       # @return [Boolean]
       def delete_disk(id)
-        @api_client.delete_disk(id)
+        run_cli("disk delete '#{id}'")
+        true
       end
 
       # @param [String] project_id
@@ -35,7 +60,9 @@ module EsxCloud
       # @param [String] id
       # @return [Disk]
       def find_disk_by_id(id)
-        @api_client.find_disk_by_id(id)
+        cmd = "disk show #{id}"
+        result = run_cli(cmd)
+        get_disk_from_response(result)
       end
 
       # @param [String] project_id
@@ -51,7 +78,31 @@ module EsxCloud
       def get_disk_tasks(id, state = nil)
         @api_client.get_disk_tasks(id, state)
       end
+      private
 
+      # @param [String] result
+      # @return [Host]
+      def get_disk_from_response(result)
+        values = result.split
+        disk_hash = { "id" => values[0],
+                      "name" => values[1],
+                      "state" => values[2],
+                      "kind" => values[3],
+                      "flavor" => values[4],
+                      "capacityGb" => values[5].to_i,
+                      "datastore" => values[6],
+                      "tags" => stringToArray(values[7]),
+                      "vms" => stringToArray(values[8]) }
+        Disk.create_from_hash(disk_hash)
+      end
+
+      def stringToArray(result)
+        values = result
+        if !(result.to_s == '')
+          values = result.split(',')
+        end
+        values
+      end
     end
   end
 end
