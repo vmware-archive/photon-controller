7fb7fa58ece3734a8260868f78b7a6579e05a3a4
Long Zhou <longz@vmware.com>
2016-02-04 16:23:01 -0800

    Agent: delete tserver module
    After deleting custom thrift server and multiplex processor, tserver module is empty.
    
    Change-Id: I93ae6f1f152ea3663b47c3db3934b7910070a694

--- python/src/common/setup.py
+++ python/src/common/setup.py
@@ -26,7 +26,6 @@ setup(name='photon.controller.common',
       install_requires=[
           'enum34==0.9.19',
           'photon.controller.gen.thrift',
-          'photon.controller.tserver',
           'setuptools==0.6c11',
           'thrift==0.9.3',
       ],
