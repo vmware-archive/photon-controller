vmware-archive/photon-controller

commit 3fcc2cddc6630eb7736073ca2be0bef99afce482

Replace custom thrift server and multiplex processor with Thrift's
Since we have upgraded thrift to 0.9.3 that has built-in multiplexing support, we no longer need the custom copy.

Change-Id: I3817263ca455e1f6e90319443a7c28eb1abde37c

python/src/agent/agent/agent.py
@@ -33,7 +33,9 @@ import time
 import traceback
 
 from concurrent.futures import ThreadPoolExecutor
+from thrift import TMultiplexedProcessor
 from thrift.protocol import TCompactProtocol
+from thrift.server import TNonblockingServer
 from thrift.transport import TSocket
 
 from .agent_config import AgentConfig
@@ -46,8 +48,6 @@ from common.plugin import load_plugins, thrift_services
 from common.request_id import RequestIdExecutor
 from common.service_name import ServiceName
 from common.state import State
-from tserver.multiplex import TMultiplexedProcessor
-from tserver.thrift_server import TNonblockingServer
 
 
 class Agent:
@@ -132,22 +132,20 @@ class Agent:
 
     def _initialize_thrift_service(self):
         """ Initialize the thrift server. """
-        mux_processor = TMultiplexedProcessor()
+        mux_processor = TMultiplexedProcessor.TMultiplexedProcessor()
 
         for plugin in thrift_services():
             self._logger.info("Load thrift services %s (num_threads: %d)",
                               plugin.name, plugin.num_threads)
             handler = plugin.handler
             processor = plugin.service.Processor(handler)
-            mux_processor.registerProcessor(plugin.name, processor,
-                                            plugin.num_threads,
-                                            plugin.max_entries)
+            mux_processor.registerProcessor(plugin.name, processor)
 
         transport = TSocket.TServerSocket(port=self._config.host_port)
         protocol_factory = TCompactProtocol.TCompactProtocolFactory()
 
-        server = TNonblockingServer(
-            mux_processor, transport, protocol_factory, protocol_factory)
+        server = TNonblockingServer.TNonblockingServer(
+            mux_processor, transport, protocol_factory, protocol_factory, 32)
         self._server = server
 
     def _start_thrift_service(self):

python/src/agent/agent/tests/common_helper_functions.py
@@ -24,8 +24,8 @@ import time
 import uuid
 import yaml
 
-from tserver.multiplex import TMultiplexedProtocol
 from thrift.protocol import TCompactProtocol
+from thrift.protocol import TMultiplexedProtocol
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 
@@ -98,7 +98,7 @@ def create_chairman_client(host, port):
     socket = TSocket.TSocket(host, port)
     transport = TTransport.TFramedTransport(socket)
     protocol = TCompactProtocol.TCompactProtocol(transport)
-    mp = TMultiplexedProtocol(protocol, "Chairman")
+    mp = TMultiplexedProtocol.TMultiplexedProtocol(protocol, "Chairman")
     client = Chairman.Client(mp)
     _wait_for_transport(transport)
     return (transport, client)
@@ -108,7 +108,7 @@ def create_root_client(port, host):
     socket = TSocket.TSocket(host, port)
     transport = TTransport.TFramedTransport(socket)
     protocol = TCompactProtocol.TCompactProtocol(transport)
-    mp = TMultiplexedProtocol(protocol, "RootScheduler")
+    mp = TMultiplexedProtocol.TMultiplexedProtocol(protocol, "RootScheduler")
     client = RootScheduler.Client(mp)
     _wait_for_transport(transport)
     return (transport, client)

python/src/chairman/integration_tests/servers/thrift_server.py
@@ -19,9 +19,9 @@ from gen.host import Host
 from gen.scheduler.root import RootScheduler
 from gen.scheduler import Scheduler
 from thrift.protocol import TCompactProtocol
+from thrift import TMultiplexedProcessor
+from thrift.server import TNonblockingServer
 from thrift.transport import TSocket
-from tserver.multiplex import TMultiplexedProcessor
-from tserver.thrift_server import TNonblockingServer
 
 
 class AgentControlHandler(AgentControl.Iface):
@@ -80,18 +80,14 @@ class ThriftServer(object):
         if service_name == "Scheduler":
             _processor = Scheduler.Processor(self._leafSchedulerHandler)
 
-        mux_processor.registerProcessor(
-            service_name,
-            _processor,
-            1,
-            0)  # unbounded queue
+        mux_processor.registerProcessor(service_name, _processor)
 
     def start_server(self):
         """
         Registers the host and root scheduler processors and
         starts the thrift server.
         """
-        mux_processor = TMultiplexedProcessor()
+        mux_processor = TMultiplexedProcessor.TMultiplexedProcessor()
 
         if self._hostHandler:
             self._register_processer("Host", mux_processor)
@@ -111,7 +107,7 @@ class ThriftServer(object):
 
         protocol_factory = TCompactProtocol.TCompactProtocolFactory()
 
-        self._server = TNonblockingServer(
+        self._server = TNonblockingServer.TNonblockingServer(
             mux_processor, transport, protocol_factory, protocol_factory)
 
         self._thread = threading.Thread(target=self._server.serve)

python/src/chairman/integration_tests/tests/unit/servers/test_thrift_server.py
@@ -27,10 +27,10 @@ from gen.scheduler.root import RootScheduler
 from gen.scheduler.ttypes import ConfigureRequest
 from gen.scheduler.ttypes import ConfigureResponse
 from thrift.protocol import TCompactProtocol
+from thrift.protocol import TMultiplexedProtocol
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.transport.TTransport import TTransportException
-from tserver.multiplex import TMultiplexedProtocol
 
 from integration_tests.servers.thrift_server \
     import ThriftServer
@@ -86,7 +86,7 @@ class TestThriftServer(unittest.TestCase):
         transport = TTransport.TFramedTransport(sock)
         self.transports.append(transport)
         protocol = TCompactProtocol.TCompactProtocol(transport)
-        mp = TMultiplexedProtocol(protocol, service_name)
+        mp = TMultiplexedProtocol.TMultiplexedProtocol(protocol, service_name)
 
         client = \
             {"Host": Host.Client(mp),

python/src/common/common/photon_thrift/client.py
@@ -17,8 +17,8 @@ from threading import Condition
 from time import time
 
 from common.photon_thrift import ServerSetListener
-from tserver.multiplex import TMultiplexedProtocol
 from thrift.protocol import TCompactProtocol
+from thrift.protocol import TMultiplexedProtocol
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 
@@ -242,7 +242,8 @@ class Client(ServerSetListener):
         transport = TTransport.TFramedTransport(sock)
 
         protocol = TCompactProtocol.TCompactProtocol(transport)
-        mp = TMultiplexedProtocol(protocol, self._service_name)
+        mp = TMultiplexedProtocol.TMultiplexedProtocol(
+                protocol, self._service_name)
 
         client = self._client_class(mp)
         self._transports[client] = transport

python/src/common/common/photon_thrift/decorators.py
@@ -13,7 +13,6 @@
 import common
 import logging
 import sys
-import threading
 import time
 import uuid
 
@@ -22,7 +21,6 @@ from functools import wraps
 from common.lock_vm import ConcurrentVmOperation
 from common.service_name import ServiceName
 from common.photon_thrift.validation import deep_validate
-from tserver.multiplex import Worker
 
 
 def log_request(func=None, log_level=logging.INFO):
@@ -59,21 +57,11 @@ def log_request(func=None, log_level=logging.INFO):
 
             request_id.value.append(request_id_value)
 
-            # if the request was made directly to the handler it will not
-            # have a value
-            current = threading.current_thread()
-            if isinstance(current, Worker) and current.queued_time():
-                queued = "%f" % (start - current.queued_time())
-            else:
-                queued = "N/A"
-
             try:
                 if no_request_id:
-                    self._logger.log(log_level, "[Queued:%s] %s no tracing",
-                                     queued, str(request))
+                    self._logger.log(log_level, "%s no tracing", str(request))
                 else:
-                    self._logger.log(log_level, "[Queued:%s] %s",
-                                     queued, str(request))
+                    self._logger.log(log_level, "%s", str(request))
                 response = func(self, request)
                 end = time.time()
                 self._logger.log(log_level, "result:%d [Duration:%f] %s",

python/src/common/common/photon_thrift/direct_client.py
@@ -12,8 +12,8 @@
 
 import logging
 
-from tserver.multiplex import TMultiplexedProtocol
 from thrift.protocol import TCompactProtocol
+from thrift.protocol import TMultiplexedProtocol
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 
@@ -48,7 +48,8 @@ class DirectClient(object):
             sock.setTimeout(self._client_timeout * 1000)
         self._transport = TTransport.TFramedTransport(sock)
         protocol = TCompactProtocol.TCompactProtocol(self._transport)
-        mux_protocol = TMultiplexedProtocol(protocol, self._service_name)
+        mux_protocol = TMultiplexedProtocol.TMultiplexedProtocol(
+                protocol, self._service_name)
         self._client = self._client_cls(mux_protocol)
         self._transport.open()
         self._logger.info("Connected to %s:%s. for service %s"

python/src/common/common/tests/unit/thrift/test_client.py
@@ -23,12 +23,12 @@ from common.photon_thrift.client import ClosedError
 from common.photon_thrift.client import TimeoutError
 from common.photon_thrift.serverset import ServerSet
 from gen.test.echoer import Echoer
+from thrift import TMultiplexedProcessor
 from thrift.protocol import TCompactProtocol
 from thrift.server import TServer
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.transport.TTransport import TTransportException
-from tserver.multiplex import TMultiplexedProcessor
 
 
 class TestServerSet(ServerSet):
@@ -62,7 +62,7 @@ PORT = 23456
 
 
 def echo_server():
-    mp = TMultiplexedProcessor()
+    mp = TMultiplexedProcessor.TMultiplexedProcessor()
     mp.registerProcessor("echo", Echoer.Processor(EchoHandler()))
     transport = TSocket.TServerSocket(port=PORT)
     tf = TTransport.TFramedTransportFactory()

commit 68bea9c461c73ecaca0e877eb7700e9cda57739e

Agent: lower thrift worker thread pool size
vm life cycle test to create 85 vms simultaneously is failing. Log
says "vim_client: wait_for_task Number of current tasks: 27".

This could be a regression caused by change 3fcc2cd "Replace custom
thrift server and multiplex processor with Thrift's". The number of
thrift worker threads are changed from 20 (host) + 32 (scheduler)
to combined 32 threads shared by these services. Having more
threads available to host thrift service may allow more
simultaneous operations hitting hostd and overload it.

This change lowers the shared thrift worker thread pool size from
32 to 20. Thrift can throttle the incoming requests and improve
hostd throughput.

Change-Id: I0cc5f14263d175f225bcdd8a0152a07d99647576

python/src/agent/agent/agent.py
@@ -145,7 +145,8 @@ class Agent:
         protocol_factory = TCompactProtocol.TCompactProtocolFactory()
 
         server = TNonblockingServer.TNonblockingServer(
-            mux_processor, transport, protocol_factory, protocol_factory, 32)
+            mux_processor, transport, protocol_factory, protocol_factory,
+            self._config.host_service_threads)
         self._server = server
 
     def _start_thrift_service(self):