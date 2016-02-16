f62d3eb057f49bfec36ceba1f1fc0da0374d978c
Alain Roy <alainr@vmware.com>
2016-02-16 14:43:16 -0800

    Fix build break
    Commit I6cd1b used imports with "dcp" when they should have been
    "xenon". Presumably Jenkins gave it a +1 before the rename was done,
    but it was checked in after the rename.
    
    Change-Id: Ib1abe9a2d8f38c3e3dc0b376077fc2101a387250

--- java/housekeeper/src/test/java/com/vmware/photon/controller/housekeeper/dcp/ImageSeederSyncTriggerServiceTest.java
+++ java/housekeeper/src/test/java/com/vmware/photon/controller/housekeeper/dcp/ImageSeederSyncTriggerServiceTest.java
@@ -15,9 +15,9 @@ package com.vmware.photon.controller.housekeeper.dcp;
 
 import com.vmware.photon.controller.common.clients.HostClient;
 import com.vmware.photon.controller.common.clients.HostClientFactory;
-import com.vmware.photon.controller.common.dcp.CloudStoreHelper;
-import com.vmware.photon.controller.common.dcp.QueryTaskUtils;
-import com.vmware.photon.controller.common.dcp.exceptions.BadRequestException;
+import com.vmware.photon.controller.common.xenon.CloudStoreHelper;
+import com.vmware.photon.controller.common.xenon.QueryTaskUtils;
+import com.vmware.photon.controller.common.xenon.exceptions.BadRequestException;
 import com.vmware.photon.controller.common.zookeeper.ServiceConfigFactory;
 import com.vmware.photon.controller.common.zookeeper.ZookeeperHostMonitor;
 import com.vmware.photon.controller.housekeeper.dcp.mock.HostClientMock;
