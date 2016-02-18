01584ee86f42e12c27bd1b6a2a80cd65b95e5ef2
Gokcen Iskender <giskender@vmware.com>
2016-02-18 11:42:23 -0800

    Devbox: Stop background tasks
    Change-Id: I5e2cd27576f4cb56cc892d193e9b0b25939b9cf1

--- java/deployer/src/dist/configuration-zookeeper/run.sh
+++ java/deployer/src/dist/configuration-zookeeper/run.sh
@@ -25,4 +25,20 @@ else
   echo "myid file does not exit"
 fi
 
+{{#PAUSE_APIFE_BACKGROUND}}
+if [ ! -f "pauseset.ini" ]
+then
+  echo "Pausing apife background"
+  exec /usr/bin/zkServer.sh start-foreground $CONFIG &
+  sleep 70
+  /usr/bin/zkCli.sh  <<EOF
+
+    create /config/apife/status PAUSED_BACKGROUND
+    quit
+EOF
+  exec touch pauseset.ini
+fi
+{{/PAUSE_APIFE_BACKGROUND}}
+{{^PAUSE_APIFE_BACKGROUND}}
 exec /usr/bin/zkServer.sh start-foreground $CONFIG
+{{/PAUSE_APIFE_BACKGROUND}}
