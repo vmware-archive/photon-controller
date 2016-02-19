6e243c65f9222cb8a0860250352b17bc68199ff8
Xuli Liu <xulil@vmware.com>
2016-02-19 10:31:17 -0800

    Support pagination for list of projects
    1. Change the interface to accept pageSize and pageLink as
       optional parameters
    2. UTs
    
    Change-Id: I583545fb01cf164fcc6172fe146a2234f4b906ff

--- java/api-frontend/management/src/test/java/com/vmware/photon/controller/apife/resources/TenantProjectsResourceTest.java
+++ java/api-frontend/management/src/test/java/com/vmware/photon/controller/apife/resources/TenantProjectsResourceTest.java
@@ -13,6 +13,7 @@
 
 package com.vmware.photon.controller.apife.resources;
 
+import com.vmware.photon.controller.api.ApiError;
 import com.vmware.photon.controller.api.Project;
 import com.vmware.photon.controller.api.ProjectCreateSpec;
 import com.vmware.photon.controller.api.QuotaLineItem;
@@ -20,8 +21,11 @@ import com.vmware.photon.controller.api.QuotaUnit;
 import com.vmware.photon.controller.api.ResourceList;
 import com.vmware.photon.controller.api.ResourceTicketReservation;
 import com.vmware.photon.controller.api.Task;
+import com.vmware.photon.controller.api.common.exceptions.external.ErrorCode;
 import com.vmware.photon.controller.api.common.exceptions.external.ExternalException;
+import com.vmware.photon.controller.api.common.exceptions.external.PageExpiredException;
 import com.vmware.photon.controller.apife.clients.ProjectFeClient;
+import com.vmware.photon.controller.apife.config.PaginationConfig;
 import com.vmware.photon.controller.apife.resources.routes.ProjectResourceRoutes;
 import com.vmware.photon.controller.apife.resources.routes.TaskResourceRoutes;
 import com.vmware.photon.controller.apife.resources.routes.TenantResourceRoutes;
@@ -31,9 +35,12 @@ import com.google.common.collect.ImmutableList;
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.Matchers;
 import org.mockito.Mock;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 import javax.ws.rs.client.Entity;
@@ -45,6 +52,9 @@ import javax.ws.rs.core.UriBuilder;
 
 import java.net.URI;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Tests {@link TenantProjectsResource}.
@@ -64,12 +74,18 @@ public class TenantProjectsResourceTest extends ResourceTest {
   @Mock
   private ProjectFeClient projectFeClient;
 
+  private PaginationConfig paginationConfig = new PaginationConfig();
   private ProjectCreateSpec spec;
   private ResourceTicketReservation ticket;
+  private Project p1 = new Project();
+  private Project p2 = new Project();
 
   @Override
   protected void setUpResources() throws Exception {
-    addResource(new TenantProjectsResource(projectFeClient));
+    paginationConfig.setMaxPageSize(PaginationConfig.DEFAULT_MAX_PAGE_SIZE);
+    paginationConfig.setDefaultPageSize(PaginationConfig.DEFAULT_DEFAULT_PAGE_SIZE);
+
+    addResource(new TenantProjectsResource(projectFeClient, paginationConfig));
 
     ticket = new ResourceTicketReservation();
     ticket.setName("rt1name");
@@ -78,6 +94,12 @@ public class TenantProjectsResourceTest extends ResourceTest {
     spec = new ProjectCreateSpec();
     spec.setName("p1name");
     spec.setResourceTicket(ticket);
+
+    p1.setName("p1name");
+    p1.setId("p1");
+
+    p2.setName("p2name");
+    p2.setId("p2");
   }
 
   @Test
@@ -132,52 +154,57 @@ public class TenantProjectsResourceTest extends ResourceTest {
     assertThat(createProject().getStatus(), is(400));
   }
 
-  @Test
-  public void testGetAllProjects() throws Exception {
-    Project p1 = new Project();
-    p1.setName("p1name");
-    p1.setId("p1");
-
-    Project p2 = new Project();
-    p2.setName("p2name");
-    p2.setId("p2");
-
-    when(projectFeClient.find(tenantId, Optional.<String>absent()))
+  @Test(dataProvider = "pageSizes")
+  public void testGetProjects(Optional<Integer> pageSize, List<Project> expectedProjects) throws Exception {
+    when(projectFeClient.find(tenantId, Optional.absent(), Optional.of(PaginationConfig.DEFAULT_DEFAULT_PAGE_SIZE)))
         .thenReturn(new ResourceList<>(ImmutableList.of(p1, p2)));
+    when(projectFeClient.find(tenantId, Optional.absent(), Optional.of(1)))
+        .thenReturn(new ResourceList<>(ImmutableList.of(p1), UUID.randomUUID().toString(), null));
+    when(projectFeClient.find(tenantId, Optional.absent(), Optional.of(2)))
+        .thenReturn(new ResourceList<>(ImmutableList.of(p1, p2)));
+    when(projectFeClient.find(tenantId, Optional.absent(), Optional.of(3)))
+        .thenReturn(new ResourceList<Project>(Collections.emptyList()));
 
-    Response response = getProjects(Optional.<String>absent());
-    assertThat(response.getStatus(), is(200));
+    Response response = getProjects(Optional.<String>absent(), pageSize, Optional.absent());
+    assertThat(response.getStatus(), is(Response.Status.OK.getStatusCode()));
 
     ResourceList<Project> projects = response.readEntity(
         new GenericType<ResourceList<Project>>() {
         }
     );
+    assertThat(projects.getItems().size(), is(expectedProjects.size()));
 
-    assertThat(projects.getItems().size(), is(2));
-    assertThat(projects.getItems().get(0), is(p1));
-    assertThat(p1.getSelfLink().endsWith(
-            UriBuilder.fromPath(ProjectResourceRoutes.PROJECT_PATH).build("p1").toString()),
-        CoreMatchers.is(true));
-    assertThat(projects.getItems().get(1), is(p2));
-    assertThat(p2.getSelfLink().endsWith(
-            UriBuilder.fromPath(ProjectResourceRoutes.PROJECT_PATH).build("p2").toString()),
-        CoreMatchers.is(true));
-
-    for (Project p : projects.getItems()) {
-      assertThat(new URI(p.getSelfLink()).isAbsolute(), CoreMatchers.is(true));
+    for (int i = 0; i < projects.getItems().size(); i++) {
+      Project retrievedProject = projects.getItems().get(i);
+      assertThat(retrievedProject, is(expectedProjects.get(i)));
+      assertThat(new URI(retrievedProject.getSelfLink()).isAbsolute(), is(true));
+      assertThat(retrievedProject.getSelfLink().endsWith(UriBuilder.fromPath(ProjectResourceRoutes.PROJECT_PATH)
+          .build(expectedProjects.get(i).getId()).toString()), is(true));
     }
+
+    verifyPageLinks(projects);
   }
 
   @Test
-  public void testGetProjectsByName() throws Exception {
-    Project p1 = new Project();
-    p1.setName("p1name");
-    p1.setId("p1");
+  public void testInvalidPageSize() throws ExternalException {
+    int pageSize = paginationConfig.getMaxPageSize() + 1;
+    Response response = getProjects(Optional.<String>absent(), Optional.of(pageSize), Optional.<String>absent());
+    assertThat(response.getStatus(), is(Response.Status.BAD_REQUEST.getStatusCode()));
 
-    when(projectFeClient.find(tenantId, Optional.of("p1name")))
+    String expectedErrorMsg = String.format("The page size '%d' is not between '1' and '%d'",
+        pageSize, PaginationConfig.DEFAULT_MAX_PAGE_SIZE);
+
+    ApiError errors = response.readEntity(ApiError.class);
+    assertThat(errors.getCode(), is(ErrorCode.INVALID_PAGE_SIZE.getCode()));
+    assertThat(errors.getMessage(), is(expectedErrorMsg));
+  }
+
+  @Test
+  public void testGetProjectsPageByName() throws Exception {
+    when(projectFeClient.find(tenantId, Optional.of("p1name"), Optional.of(PaginationConfig.DEFAULT_DEFAULT_PAGE_SIZE)))
         .thenReturn(new ResourceList<>(ImmutableList.of(p1)));
 
-    Response response = getProjects(Optional.of("p1name"));
+    Response response = getProjects(Optional.of("p1name"), Optional.absent(), Optional.absent());
 
     assertThat(response.getStatus(), is(200));
 
@@ -194,6 +221,44 @@ public class TenantProjectsResourceTest extends ResourceTest {
     assertThat(new URI(p1.getSelfLink()).isAbsolute(), CoreMatchers.is(true));
   }
 
+  @Test
+  public void testGetProjectsPage() throws Exception {
+    String pageLink = UUID.randomUUID().toString();
+    doReturn(new ResourceList<>(ImmutableList.of(p1), UUID.randomUUID().toString(), UUID.randomUUID().toString()))
+        .when(projectFeClient)
+        .getProjectsPage(pageLink);
+
+    Response response = getProjects(Optional.<String>absent(), Optional.<Integer>absent(), Optional.of(pageLink));
+        assertThat(response.getStatus(), is(Response.Status.OK.getStatusCode()));
+
+    ResourceList<Project> projects = response.readEntity(new GenericType<ResourceList<Project>>(){});
+    assertThat(projects.getItems().size(), is(1));
+
+    Project project = projects.getItems().get(0);
+    assertThat(project, is(p1));
+
+    String projectRoutePath = UriBuilder.fromPath(ProjectResourceRoutes.PROJECT_PATH).build(p1.getId()).toString();
+    assertThat(project.getSelfLink().endsWith(projectRoutePath), is(true));
+    assertThat(new URI(project.getSelfLink()).isAbsolute(), is(true));
+
+    verifyPageLinks(projects);
+  }
+
+  @Test
+  public void testInvalidProjectsPageLink() throws ExternalException {
+    String pageLink = UUID.randomUUID().toString();
+    doThrow(new PageExpiredException(pageLink)).when(projectFeClient).getProjectsPage(pageLink);
+
+    Response response = getProjects(Optional.<String>absent(), Optional.<Integer>absent(), Optional.of(pageLink));
+    assertThat(response.getStatus(), is(Response.Status.NOT_FOUND.getStatusCode()));
+
+    String expectedErrorMessage = "Page " + pageLink + " has expired";
+
+    ApiError errors = response.readEntity(ApiError.class);
+    assertThat(errors.getCode(), is(ErrorCode.PAGE_EXPIRED.getCode()));
+    assertThat(errors.getMessage(), is(expectedErrorMessage));
+  }
+
   private Response createProject() {
     return client()
         .target(tenantProjectsRoute)
@@ -201,12 +266,51 @@ public class TenantProjectsResourceTest extends ResourceTest {
         .post(Entity.entity(spec, MediaType.APPLICATION_JSON_TYPE));
   }
 
-  private Response getProjects(Optional<String> name) {
+  private Response getProjects(Optional<String> name, Optional<Integer> pageSize, Optional<String> pageLink) {
     WebTarget resource = client().target(tenantProjectsRoute);
     if (name.isPresent()) {
       resource = resource.queryParam("name", name.get());
     }
+    if (pageSize.isPresent()) {
+      resource = resource.queryParam("pageSize", pageSize.get());
+    }
+    if (pageLink.isPresent()) {
+      resource = resource.queryParam("pageLink", pageLink.get());
+    }
 
     return resource.request().get();
+  }
+
+  private void verifyPageLinks(ResourceList<Project> resourceList) {
+    String expectedPrefix = tenantProjectsRoute + "?pageLink=";
+
+    if (resourceList.getNextPageLink() != null) {
+      assertThat(resourceList.getNextPageLink().startsWith(expectedPrefix), Matchers.is(true));
+    }
+    if (resourceList.getPreviousPageLink() != null) {
+      assertThat(resourceList.getPreviousPageLink().startsWith(expectedPrefix), Matchers.is(true));
+    }
+  }
+
+  @DataProvider(name = "pageSizes")
+  private Object[][] getPageSize() {
+    return new Object[][] {
+        {
+            Optional.absent(),
+            ImmutableList.of(p1, p2)
+        },
+        {
+            Optional.of(1),
+            ImmutableList.of(p1)
+        },
+        {
+            Optional.of(2),
+            ImmutableList.of(p1, p2)
+        },
+        {
+            Optional.of(3),
+            Collections.emptyList()
+        }
+    };
   }
 }
