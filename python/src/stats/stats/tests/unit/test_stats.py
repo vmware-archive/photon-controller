e011646b0e6b350e9f2cde89940439c5e0ff6b7b
Touseef Liaqat <tliaqat@vmware.com>
2016-02-24 15:46:02 -0800

    stats: copy stats python plugin code from private repo to public
    During copy we are disabling the stats plugin in this change.
    The agent will call the stats's init method but that method has no
    code and will return without starting collector and publisher.
    
    History is not being kept because first there were not many commits in
    stats folder, second we cannot push history forcefully through gerrit.
    If we wanted to copy the history then each commit would individually have
    to go through gerrit, which we are avoiding here.
    
    Change-Id: I70fa6cb6dc43c4b739121b6ef5305f5052714fe8

---
+++ python/src/stats/stats/tests/unit/test_stats.py
@@ -0,0 +1,62 @@
+# Copyright 2015 VMware, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy
+# of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, without
+# warranties or conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the
+# License for then specific language governing permissions and limitations
+# under the License.
+
+import unittest
+
+from hamcrest import *  # noqa
+from matchers import *  # noqa
+from mock import MagicMock
+from mock import patch
+
+import common
+from common.service_name import ServiceName
+import stats.stats
+
+
+class TestStats(unittest.TestCase):
+
+    def setUp(self):
+        agent_config = MagicMock()
+        common.services.register(ServiceName.AGENT_CONFIG, agent_config)
+
+    @patch('stats.stats.StatsPublisher.start_publishing')
+    @patch('stats.stats.StatsPublisher.configure_publishers')
+    @patch('stats.stats.StatsCollector.start_collection')
+    @patch('stats.stats.StatsCollector.configure_collectors')
+    def test_stats_handler(self, _config_collectors, _start_collection,
+                           _config_publishers, _start_publishing):
+        handler = stats.stats.StatsHandler()
+        _config_collectors.assert_called_once_with()
+        _config_publishers.assert_called_once_with()
+        _start_collection.assert_not_called()
+        _start_publishing.assert_not_called()
+        handler.start()
+        _start_collection.assert_called_once_with()
+        _start_publishing.assert_called_once_with()
+
+    @patch('stats.stats.StatsHandler')
+    @patch('common.plugin.ThriftService')
+    @patch('common.plugin.Plugin.add_thrift_service')
+    def test_import_plugin(self, _add_svc, _thrift_svc_cls, _handler_cls):
+        mock_svc = MagicMock()
+        _thrift_svc_cls.return_value = mock_svc
+        import stats.plugin
+
+        assert_that(stats.plugin.plugin, not_none())
+        stats.plugin.plugin.init()
+        _handler_cls.assert_called_once_with()
+        assert_that(_thrift_svc_cls.call_count, is_(1))
+        _add_svc.assert_called_once_with(mock_svc)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ python/src/stats/stats/tests/unit/test_stats_publisher.py
@@ -0,0 +1,98 @@
+# Copyright 2015 VMware, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy
+# of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, without
+# warranties or conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the
+# License for then specific language governing permissions and limitations
+# under the License.
+
+""" test_stats_publisher.py
+
+    Unit tests for the stats publisher thread.
+"""
+
+import unittest
+
+from hamcrest import *  # noqa
+from mock import MagicMock, patch
+
+import common
+from common.service_name import ServiceName
+from stats.stats_publisher import StatsPublisher
+
+
+class TestStatsPublisher(unittest.TestCase):
+
+    def setUp(self):
+        agent_config = MagicMock()
+        agent_config.host_id = "fake-id"
+        agent_config.stats_store_address = "1.1.1.1"
+        self._mock_db = MagicMock()
+        common.services.register(ServiceName.AGENT_CONFIG, agent_config)
+
+    @patch('stats.stats_publisher.Periodic')
+    def test_start_stop_publishing(self, _periodic_cls):
+        mock_thread = MagicMock()
+        _periodic_cls.return_value = mock_thread
+        publisher = StatsPublisher(self._mock_db)
+
+        publisher.start_publishing()
+
+        assert_that(mock_thread.daemon, is_(True))
+        mock_thread.start.assert_called_once_with()
+        _periodic_cls.assert_called_once_with(
+            publisher.publish,
+            StatsPublisher.DEFAULT_PUBLISH_INTERVAL_SECS)
+
+        publisher.stop_publishing()
+
+        mock_thread.stop.assert_called_once_with()
+
+    @patch('stats.stats_publisher.GraphitePublisher')
+    def test_configure_publishers(self, _graphite_pub_cls):
+        publisher = StatsPublisher(self._mock_db)
+        mock_publisher = MagicMock()
+        _graphite_pub_cls.return_value = mock_publisher
+
+        publisher.configure_publishers()
+
+        _graphite_pub_cls.assert_called_once_with(host_id="fake-id",
+                                                  carbon_host='1.1.1.1')
+        assert_that(publisher._publishers, contains(mock_publisher))
+
+    @patch('stats.stats_publisher.GraphitePublisher')
+    def test_publish(self, _graphite_pub_cls):
+        publisher = StatsPublisher(self._mock_db)
+        mock_graphite_publisher = MagicMock()
+        _graphite_pub_cls.return_value = mock_graphite_publisher
+
+        publisher.configure_publishers()
+
+        metrics = {"key1": [(1000000, 1), (1000020, 2)]}
+        self._mock_db.get_keys.return_value = metrics.keys()
+
+        self._mock_db.get_values_since.return_value = metrics["key1"]
+
+        publisher.publish()
+
+        self._mock_db.get_values_since.assert_called_once_with(0, "key1")
+        mock_graphite_publisher.publish.assert_called_once_with(metrics)
+        self._mock_db.get_values_since.reset_mock()
+        mock_graphite_publisher.publish.reset_mock()
+
+        metrics = {"key1": [(1000040, 5)]}
+        self._mock_db.get_values_since.return_value = metrics["key1"]
+
+        publisher.publish()
+
+        # verify retrieval is from last seen time stamp
+        self._mock_db.get_values_since.assert_called_once_with(1000020, "key1")
+        mock_graphite_publisher.publish.assert_called_once_with(metrics)
+
+
+if __name__ == '__main__':
+    unittest.main()
