bab550353cb86b7cf98f7f389a14e0ea32f1a1c5
Varnika Goyal <goyalv@vmware.com>
2016-02-25 17:16:22 -0800

    Use run_cli cmd to implement cluster APIs
    Change-Id: Icaa11662893b75ea60d03bc8676afa844bd6985f

--- ruby/common/lib/common/go_cli_client/cluster_api.rb
+++ ruby/common/lib/common/go_cli_client/cluster_api.rb
@@ -17,32 +17,100 @@ module EsxCloud
       # @param [Hash] payload
       # @return [Cluster]
       def create_cluster(project_id, payload)
-        @api_client.create_cluster(id)
+        project = find_project_by_id(project_id)
+        tenant = @project_to_tenant[project.id]
+
+        cmd = "cluster create -t '#{tenant.name}' -p '#{project.name}' " +
+            "-n '#{payload[:name]}' -k '#{payload[:type]}' -v '#{payload[:vmFlavor]}' " +
+            "-d '#{payload[:diskFlavor]}' -w '#{payload[:vmNetworkId]}' -s #{payload[:slaveCount]} " +
+            "--dns '#{payload[:extendedProperties]["dns"]}' " +
+            "--gateway '#{payload[:extendedProperties]["gateway"]}' " +
+            "--netmask '#{payload[:extendedProperties]["netmask"]}' "
+        if payload[:type] === "KUBERNETES"
+          cmd += "--etcd1 '#{payload[:extendedProperties]["etcd_ip1"]}' "
+          cmd += "--etcd2 '#{payload[:extendedProperties]["etcd_ip2"]}' "
+          cmd += "--etcd3 '#{payload[:extendedProperties]["etcd_ip3"]}' "
+          cmd += "--master-ip '#{payload[:extendedProperties]["master_ip"]}' "
+          cmd += "--container-network '#{payload[:extendedProperties]["container_network"]}'"
+        elsif payload[:type] === "MESOS"
+          cmd += "--zookeeper1 '#{payload[:extendedProperties]["zookeeper_ip1"]}' "
+          cmd += "--zookeeper2 '#{payload[:extendedProperties]["zookeeper_ip2"]}' "
+          cmd += "--zookeeper3 '#{payload[:extendedProperties]["zookeeper_ip3"]}'"
+        elsif payload[:type] === "SWARM"
+          cmd += "--etcd1 '#{payload[:extendedProperties]["etcd_ip1"]}' "
+          cmd += "--etcd2 '#{payload[:extendedProperties]["etcd_ip2"]}' "
+          cmd += "--etcd3 '#{payload[:extendedProperties]["etcd_ip3"]}'"
+        end
+
+        cluster_id = run_cli(cmd).split("\n")[0]
+
+        find_cluster_by_id(cluster_id)
       end
 
       # @param [String] id
       # @return [Cluster]
       def find_cluster_by_id(id)
-        @api_client.find_cluster_by_id(id)
+        result = run_cli("cluster show #{id}")
+
+        get_cluster_from_response(result)
       end
 
       # @param [String] id
       # @return [VmList]
       def get_cluster_vms(id)
-        @api_client.get_cluster_vms(id)
+        result = run_cli ("cluster list_vms '#{id}'")
+
+        get_vm_list_from_response(result)
       end
 
       # @param [String] id
       # @param [int] new_slave_count
       # @return [Boolean]
       def resize_cluster(id, new_slave_count)
-        @api_client.resize_cluster(id, new_slave_count)
+        run_cli("cluster resize '#{id}' '#{new_slave_count}'")
+        true
       end
 
       # @param [String] id
       # @return [Boolean]
       def delete_cluster(id)
-        @api_client.delete_cluster(id)
+        run_cli("cluster delete '#{id}'")
+        true
+      end
+
+      private
+
+      def get_vm_list_from_response(result)
+        vms = result.split("\n").drop(2).map do |vm_info|
+          find_vm_by_id(vm_info.split("\t")[0])
+        end
+        VmList.new(vms)
+      end
+
+      def get_cluster_from_response(result)
+        values = result.split("\n")
+        cluster_attributes = values[0].split("\t")
+        cluster_hash = Hash.new
+        cluster_hash["id"]                 = cluster_attributes[0] unless cluster_attributes[0] == ""
+        cluster_hash["name"]               = cluster_attributes[1] unless cluster_attributes[1] == ""
+        cluster_hash["state"]              = cluster_attributes[2] unless cluster_attributes[2] == ""
+        cluster_hash["type"]               = cluster_attributes[3] unless cluster_attributes[3] == ""
+        cluster_hash["slaveCount"]         = cluster_attributes[4].to_i unless cluster_attributes[4] == ""
+        cluster_hash["extendedProperties"] = extendedProperties_to_hash(cluster_attributes[5])
+
+        Cluster.create_from_hash(cluster_hash)
+      end
+
+      # @param [String] properties
+      # @return hash
+      def extendedProperties_to_hash(properties)
+        hash_new = Hash.new
+        if properties.to_s != ''
+          properties.split(' ').each { |property|
+            values = property.split(':')
+            hash_new.merge!({ values[0] => values[1]})}
+        end
+        hash_new
       end
     end
   end
