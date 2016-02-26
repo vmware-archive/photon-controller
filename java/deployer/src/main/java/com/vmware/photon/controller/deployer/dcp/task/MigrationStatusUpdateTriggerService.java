c7b4e7806b9fc6a67208e345251d4673a81cb845
Adrian Schroeter <aschroeter@vmware.com>
2016-02-25 18:14:45 -0800

    upgrade/deployer: fixed migration status and copy state task
    - migration status had two issues, it was not triggered periodically and
      it contained both factory links for the esxcloud and photon based uris
      which caused the data migration cycles report to be always 0.
    - copy state task tries to copy states from all hsots in aprallel for any
      given entity, this resuts in trying to copy the same entity multiple times
    
    Change-Id: I4e6243f64d594f08c4f373159d61f17a1fa0ea22

--- java/deployer/src/main/java/com/vmware/photon/controller/deployer/dcp/task/MigrationStatusUpdateTriggerService.java
+++ java/deployer/src/main/java/com/vmware/photon/controller/deployer/dcp/task/MigrationStatusUpdateTriggerService.java
@@ -22,21 +22,22 @@ import com.vmware.photon.controller.common.xenon.validation.Immutable;
 import com.vmware.photon.controller.common.xenon.validation.NotNull;
 import com.vmware.photon.controller.deployer.dcp.util.HostUtils;
 import com.vmware.xenon.common.Operation;
-import com.vmware.xenon.common.OperationJoin;
 import com.vmware.xenon.common.OperationSequence;
 import com.vmware.xenon.common.ServiceDocument;
+import com.vmware.xenon.common.ServiceMaintenanceRequest;
+import com.vmware.xenon.common.ServiceMaintenanceRequest.MaintenanceReason;
 import com.vmware.xenon.common.StatefulService;
 import com.vmware.xenon.common.TaskState.TaskStage;
 import com.vmware.xenon.common.UriUtils;
 import com.vmware.xenon.common.Utils;
 import com.vmware.xenon.services.common.QueryTask;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -44,6 +45,8 @@ import java.util.stream.Collectors;
  * This service reports the status of the migration initialization.
  */
 public class MigrationStatusUpdateTriggerService extends StatefulService {
+
+  private static final long DEFAULT_TRIGGER_INTERVAL = TimeUnit.SECONDS.toMicros(5);
 
   /**
    * This class defines the document state associated with a single
@@ -63,6 +66,8 @@ public class MigrationStatusUpdateTriggerService extends StatefulService {
     super.toggleOption(ServiceOption.OWNER_SELECTION, true);
     super.toggleOption(ServiceOption.PERSISTENCE, true);
     super.toggleOption(ServiceOption.REPLICATION, true);
+    super.toggleOption(ServiceOption.PERIODIC_MAINTENANCE, true);
+    super.setMaintenanceIntervalMicros(DEFAULT_TRIGGER_INTERVAL);
   }
 
   @Override
@@ -76,8 +81,10 @@ public class MigrationStatusUpdateTriggerService extends StatefulService {
   }
 
   @Override
-  public void handleGet(Operation get) {
-    State currentState = getState(get);
+  public void handlePatch(Operation patch) {
+    patch.complete();
+    State currentState = getState(patch);
+
     Operation copyStateTaskQuery = generateKindQuery(CopyStateTaskService.State.class);
     Operation uploadVibTaskQuery = generateKindQuery(UploadVibTaskService.State.class);
 
@@ -85,7 +92,7 @@ public class MigrationStatusUpdateTriggerService extends StatefulService {
         .setCompletion((op, t) -> {
           if (t != null && !t.isEmpty()) {
             t.values().forEach((throwable) -> ServiceUtils.logSevere(this, throwable));
-            get.fail(t.get(copyStateTaskQuery.getId()));
+            ServiceUtils.logSevere(this, t.get(copyStateTaskQuery.getId()));
             return;
           }
 
@@ -93,15 +100,16 @@ public class MigrationStatusUpdateTriggerService extends StatefulService {
             Map<String, Integer> finishedCopyStateCounts
                 = countFinishedCopyStateTaskServices(copyStateTaskQuery, op);
             List<UploadVibTaskService.State> documents
-                = extractDocuments(op.get(uploadVibTaskQuery.getId()), UploadVibTaskService.State.class);
+                = QueryTaskUtils
+                    .getBroadcastQueryDocuments(UploadVibTaskService.State.class, op.get(uploadVibTaskQuery.getId()));
             long vibsUploaded = countTasks(documents, task -> task.taskState.stage == TaskStage.FINISHED);
             long vibsUploading = countTasks(
                 documents,
                 task -> task.taskState.stage == TaskStage.STARTED || task.taskState.stage == TaskStage.CREATED);
 
-            updateDeploymentService(get, currentState, finishedCopyStateCounts, vibsUploaded, vibsUploading);
+            updateDeploymentService(currentState, finishedCopyStateCounts, vibsUploaded, vibsUploading);
           } catch (Throwable throwable) {
-            get.fail(throwable);
+            ServiceUtils.logSevere(this, throwable);
           }
         })
         .sendWith(this);
@@ -110,18 +118,20 @@ public class MigrationStatusUpdateTriggerService extends StatefulService {
   private Map<String, Integer> countFinishedCopyStateTaskServices(
       Operation copyStateTaskQuery,
       Map<Long, Operation> op) {
-    List<String> sourceFactories = HostUtils.getDeployerContext(this)
+    Set<String> sourceFactories = HostUtils.getDeployerContext(this)
         .getFactoryLinkMapEntries().stream()
         .map(entry -> entry.getKey())
-        .collect(Collectors.toList());
+        .collect(Collectors.toSet());
 
     List<CopyStateTaskService.State> copyStateTasks
-        = extractDocuments(op.get(copyStateTaskQuery.getId()), CopyStateTaskService.State.class);
+        = QueryTaskUtils.getBroadcastQueryDocuments(
+            CopyStateTaskService.State.class, op.get(copyStateTaskQuery.getId()));
 
     Map<String, Integer> map = new HashMap<>();
     sourceFactories.stream().forEach(factoryLink -> map.put(appendIfNotExists(factoryLink, "/"), 0));
     copyStateTasks.stream().forEach(state -> {
-      if (state.taskState.stage == TaskStage.FINISHED && map.containsKey(state.sourceFactoryLink)) {
+      if (state.taskState.stage == TaskStage.FINISHED
+          && map.containsKey(state.sourceFactoryLink)) {
         Integer count = map.get(state.sourceFactoryLink);
         map.put(state.sourceFactoryLink, count + 1);
       }
@@ -130,27 +140,21 @@ public class MigrationStatusUpdateTriggerService extends StatefulService {
   }
 
   private void updateDeploymentService(
-      Operation get,
       State currentState,
       Map<String, Integer> finishedCopyStateCounts,
       long vibsUploaded,
       long vibsUploading) {
 
-    DeploymentService.State patch = buildPatch(finishedCopyStateCounts, vibsUploaded, vibsUploading);
-    sendRequest(
-        HostUtils.getCloudStoreHelper(this)
-            .createPatch(currentState.deploymentServiceLink)
-            .setBody(buildPatch(finishedCopyStateCounts, vibsUploaded, vibsUploading))
-            .setCompletion(
-                (completedOp, failure) -> {
-                  if (failure != null) {
-                    ServiceUtils.logSevere(this, failure);
-                    get.fail(failure);
-                  } else {
-                    get.setBody(currentState).complete();
-                  }
-                }
-            ));
+    HostUtils.getCloudStoreHelper(this)
+      .createPatch(currentState.deploymentServiceLink)
+      .setBody(buildPatch(finishedCopyStateCounts, vibsUploaded, vibsUploading))
+      .setCompletion(
+        (completedOp, failure) -> {
+          if (failure != null) {
+            ServiceUtils.logSevere(this, failure);
+          }
+        }
+        ).sendWith(this);
   }
 
   private String appendIfNotExists(String factoryLink, String string) {
@@ -158,13 +162,6 @@ public class MigrationStatusUpdateTriggerService extends StatefulService {
       return factoryLink;
     }
     return factoryLink + string;
-  }
-
-  private <T> List<T> extractDocuments(Operation operation, Class<T> type) {
-    Collection<Object> values = operation.getBody(QueryTask.class).results.documents.values();
-    return values.stream()
-        .map(entry -> Utils.fromJson(entry, type))
-        .collect(Collectors.toList());
   }
 
   private long countTasks(List<UploadVibTaskService.State> tasks, Predicate<UploadVibTaskService.State> predicate) {
@@ -176,38 +173,13 @@ public class MigrationStatusUpdateTriggerService extends StatefulService {
   @Override
   public void handleMaintenance(Operation maintenance) {
     maintenance.complete();
-    triggerStatusCollection();
-  }
-
-  private void triggerStatusCollection() {
-    Operation updateQuery = generateDataMigrationStatusUpdateTaskQuery();
-
-    OperationJoin.JoinedCompletionHandler joinedGetCompletionHandler = (o, failures) -> {
-      if (failures != null && failures.size() > 0) {
-        ServiceUtils.logSevere(this, failures.values());
-      }
-    };
-
-    updateQuery.setCompletion((o, t) -> {
-      if (t != null) {
-        ServiceUtils.logSevere(this, t);
-        return;
-      }
-      List<State> resultDocuments = QueryTaskUtils.getBroadcastQueryDocuments(State.class, o);
-      if (resultDocuments.isEmpty()) {
-        return;
-      }
-
-      List<Operation> opList = new ArrayList<Operation>(resultDocuments.size());
-      for (State state : resultDocuments) {
-        opList.add(Operation.createGet(this, state.deploymentServiceLink));
-      }
-
-      OperationJoin.create(opList)
-          .setCompletion(joinedGetCompletionHandler)
-          .sendWith(this);
-    });
-    sendRequest(updateQuery);
+    ServiceMaintenanceRequest serviceMaintenanceRequest = maintenance.getBody(ServiceMaintenanceRequest.class);
+    if (!serviceMaintenanceRequest.reasons.contains(MaintenanceReason.PERIODIC_SCHEDULE)) {
+      return;
+    }
+    Operation.createPatch(UriUtils.buildUri(getHost(), getSelfLink()))
+      .setBody(new State())
+      .sendWith(this);
   }
 
   private DeploymentService.State buildPatch(Map<String, Integer> progressMap, long vibsUploaded, long vibsUploading) {
@@ -227,21 +199,9 @@ public class MigrationStatusUpdateTriggerService extends StatefulService {
     querySpecification.options = EnumSet.of(QueryTask.QuerySpecification.QueryOption.EXPAND_CONTENT);
 
     return Operation
-        .createPost(UriUtils.buildUri(getHost(), ServiceUriPaths.CORE_LOCAL_QUERY_TASKS))
-        .setBody(QueryTask.create(querySpecification).setDirect(true));
-  }
-
-  private Operation generateDataMigrationStatusUpdateTaskQuery() {
-    QueryTask.Query typeClause = new QueryTask.Query()
-        .setTermPropertyName(ServiceDocument.FIELD_NAME_KIND)
-        .setTermMatchValue(Utils.buildKind(State.class));
-
-    QueryTask.QuerySpecification querySpecification = new QueryTask.QuerySpecification();
-    querySpecification.query = typeClause;
-    querySpecification.options = EnumSet.of(QueryTask.QuerySpecification.QueryOption.EXPAND_CONTENT);
-
-    return Operation
-        .createPost(UriUtils.buildUri(getHost(), ServiceUriPaths.CORE_LOCAL_QUERY_TASKS))
+        .createPost(UriUtils.buildBroadcastRequestUri(
+            UriUtils.buildUri(
+                getHost(), ServiceUriPaths.CORE_LOCAL_QUERY_TASKS), ServiceUriPaths.DEFAULT_NODE_SELECTOR))
         .setBody(QueryTask.create(querySpecification).setDirect(true));
   }
 }
