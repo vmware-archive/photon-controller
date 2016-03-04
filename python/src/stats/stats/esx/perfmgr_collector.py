d55db44ff9ffbf2c2057cfda5c0808f5b82a6417
Touseef Liaqat <tliaqat@vmware.com>
2016-03-04 06:22:05 -0800

    WIP: stats/agent: add network metrics for host
    Change-Id: Ida92ac405eadebe2188776bf5b182b8804360cac

--- python/src/stats/stats/esx/perfmgr_collector.py
+++ python/src/stats/stats/esx/perfmgr_collector.py
@@ -34,6 +34,12 @@ class PerfManagerCollector(Collector):
         [
             host_cpu_usage_metric_name,
             host_mem_usage_metric_name,
+            "net.usage",
+            "net.packetsRx",
+            "net.packetsTx",
+            "net.droppedRx",
+            "net.droppedTx",
+            "net.transmitted",
         ],
         # level 2
         [
