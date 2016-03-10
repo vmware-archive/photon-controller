be4ca4064de4b4febd01d9f5ef80b2e728f56909
Pankaj Lakhina <plakhina@vmware.com>
2016-03-10 15:45:57 -0800

    Helper method to process expiration on DELETE of cloud store services
    Change-Id: I9a35b22e0dae7659fab936012743a1c21c93ef74

--- java/common-dcp/src/test/java/com/vmware/photon/controller/common/xenon/ServiceUtilsTest.java
+++ java/common-dcp/src/test/java/com/vmware/photon/controller/common/xenon/ServiceUtilsTest.java
@@ -13,23 +13,34 @@
 
 package com.vmware.photon.controller.common.xenon;
 
+import com.vmware.xenon.common.Operation;
 import com.vmware.xenon.common.Service;
 import com.vmware.xenon.common.ServiceDocument;
 import com.vmware.xenon.common.ServiceDocumentDescription;
+import com.vmware.xenon.common.StatefulService;
 import com.vmware.xenon.common.TaskState;
 import com.vmware.xenon.common.UtilsHelper;
 
+import org.mockito.ArgumentCaptor;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.endsWith;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.number.BigDecimalCloseTo.closeTo;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.doReturn;
 import static org.powermock.api.mockito.PowerMockito.mock;
 
+import java.math.BigDecimal;
 import java.util.EnumSet;
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Test {@link ServiceUtils} class.
@@ -111,6 +122,96 @@ public class ServiceUtilsTest {
       String fmtMessage = ServiceUtils.getFmtMsg(service, fmt);
       assertThat(fmtMessage, containsString(requestId));
       assertThat(fmtMessage, endsWith(String.format("[%s] %s", service.getSelfLink(), fmt)));
+    }
+  }
+
+  /**
+   * Tests the expireDocumentOnDelete method.
+   */
+  public class ExpireDocumentOnDeleteTest {
+    private Service service;
+    private Operation deleteOperation;
+
+    @BeforeMethod
+    public void setup() {
+      service = mock(StatefulService.class);
+      deleteOperation = mock(Operation.class);
+    }
+
+    /**
+     * Test default expiration is not applied if it is already specified in current state.
+     *
+     * @throws Throwable
+     */
+    @Test
+    public void testDefaultExpirationIsNotAppliedIfItIsAlreadySpecifiedInCurrentState() throws Throwable {
+      ServiceDocument currentState = new ServiceDocument();
+      currentState.documentExpirationTimeMicros = ServiceUtils.computeExpirationTime(Integer.MAX_VALUE);
+
+
+      when(deleteOperation.hasBody()).thenReturn(true);
+      ServiceDocument deleteState = new ServiceDocument();
+      deleteState.documentExpirationTimeMicros = 0L;
+      when(deleteOperation.getBody(ServiceDocument.class)).thenReturn(deleteState);
+
+      ServiceUtils.expireDocumentOnDelete(service, currentState, ServiceDocument.class, deleteOperation);
+
+      ArgumentCaptor<ServiceDocument> stateArgument = ArgumentCaptor.forClass(ServiceDocument.class);
+      ArgumentCaptor<Operation> operationArgument = ArgumentCaptor.forClass(Operation.class);
+      verify(service, times(1)).setState(operationArgument.capture(), stateArgument.capture());
+      verify(deleteOperation, times(1)).complete();
+      assertThat(stateArgument.getValue().documentExpirationTimeMicros, not(0L));
+      assertThat(new BigDecimal(stateArgument.getValue().documentExpirationTimeMicros),
+          is(closeTo(
+              new BigDecimal(
+                  ServiceUtils.computeExpirationTime(Integer.MAX_VALUE)),
+              new BigDecimal(TimeUnit.MINUTES.toMicros(1)))));
+    }
+
+    /**
+     * Test default expiration is not applied if it is already specified in delete operation state.
+     *
+     * @throws Throwable
+     */
+    @Test
+    public void testDefaultExpirationIsNotAppliedIfItIsAlreadySpecifiedInDeleteOperation() throws Throwable {
+      ServiceDocument currentState = new ServiceDocument();
+      currentState.documentExpirationTimeMicros = 1L;
+
+      when(deleteOperation.hasBody()).thenReturn(true);
+      ServiceDocument deleteState = new ServiceDocument();
+      deleteState.documentExpirationTimeMicros = Long.MAX_VALUE;
+      when(deleteOperation.getBody(anyObject())).thenReturn(deleteState);
+
+      ServiceUtils.expireDocumentOnDelete(service, currentState, ServiceDocument.class, deleteOperation);
+
+      ArgumentCaptor<ServiceDocument> stateArgument = ArgumentCaptor.forClass(ServiceDocument.class);
+      ArgumentCaptor<Operation> operationArgument = ArgumentCaptor.forClass(Operation.class);
+      verify(service, times(1)).setState(operationArgument.capture(), stateArgument.capture());
+      verify(deleteOperation, times(1)).complete();
+      assertThat(stateArgument.getValue().documentExpirationTimeMicros, is(Long.MAX_VALUE));
+    }
+
+    /**
+     * Test expiration of deleted document using default value.
+     *
+     * @throws Throwable
+     */
+    @Test
+    public void testDeleteWithDefaultExpiration() throws Throwable {
+      ServiceDocument currentState = new ServiceDocument();
+      ServiceUtils.expireDocumentOnDelete(service, currentState, ServiceDocument.class, deleteOperation);
+
+      ArgumentCaptor<ServiceDocument> stateArgument = ArgumentCaptor.forClass(ServiceDocument.class);
+      ArgumentCaptor<Operation> operationArgument = ArgumentCaptor.forClass(Operation.class);
+      verify(service, times(1)).setState(operationArgument.capture(), stateArgument.capture());
+      verify(deleteOperation, times(1)).complete();
+      assertThat(stateArgument.getValue().documentExpirationTimeMicros, not(0L));
+      assertThat(new BigDecimal(stateArgument.getValue().documentExpirationTimeMicros),
+          is(closeTo(
+              new BigDecimal(
+                  ServiceUtils.computeExpirationTime(ServiceUtils.DEFAULT_ON_DELETE_DOC_EXPIRATION_TIME_MICROS)),
+              new BigDecimal(TimeUnit.MINUTES.toMicros(1)))));
     }
   }
 }
