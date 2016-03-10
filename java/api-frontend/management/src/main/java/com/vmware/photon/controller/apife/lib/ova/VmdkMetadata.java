94c51a8a0e00a1b06c700ef5afff3aa855290afa
Alain Roy <alainr@vmware.com>
2016-03-09 17:44:19 -0800

    Fix bug in parsing extent descriptions in VMDK
    Our code that parses extent descriptions in a VMDK to find the size of
    the VMDK incorrectly parsed the description if there was a space in
    the filename. This fixes that.
    
    Change-Id: Ib4b90a64c47a5db7def168baa2e9e4d9cc5b4bf7
    (cherry picked from commit 39667eb3aa6968b93b4e552159e3285a0cf924a4)

--- java/api-frontend/management/src/main/java/com/vmware/photon/controller/apife/lib/ova/VmdkMetadata.java
+++ java/api-frontend/management/src/main/java/com/vmware/photon/controller/apife/lib/ova/VmdkMetadata.java
@@ -18,6 +18,10 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Class encapsulating VMDK metadata elements.
@@ -27,6 +31,18 @@ public class VmdkMetadata {
   private static final Logger logger = LoggerFactory.getLogger(VmdkMetadata.class);
   private static final String EXTENT_DESCRIPTION_HEADER = "# Extent description";
   private static final String DISK_DATA_BASE_HEADER = "# The Disk Data Base";
+
+  // This is a regular expression for one token in an extent field. It's a doozy, so let's break it down.
+  //
+  // We'll match zero or more spaces, followed by a group (the token), then zero or more spaces.
+  //
+  // The token is one of two things:
+  //
+  // [^"]\S* matches a token that begins with a non-quote character, and is followed by any number of non-space
+  // characters
+  //
+  // ".*?" matches a token that begins with a space, has zero or more characters, a quote,
+  private static final Pattern EXTENT_TOKEN_PATTERN = Pattern.compile("\\s*([^\"]\\S*|\".+?\")\\s*");
 
   /**
    * Get the size of sector -> extent.
@@ -59,18 +75,34 @@ public class VmdkMetadata {
       logger.error("Invalid vmdk: multiple vmdks detected\n{}", s);
       throw new IllegalStateException("Invalid vmdk: multiple vmdks detected");
     }
-    String[] fields = s.split(" ");
-    if (fields.length != 4) {
-      logger.error("Invalid vmdk: fields length is {}\n{}", fields.length, s);
-      throw new IllegalStateException("Invalid vmdk: fields length is " + fields.length);
+    List<String> fields = extractExtentFields(s);
+    if (fields.size() != 4) {
+      logger.error("Invalid vmdk: fields length is {}\n{}", fields.size(), s);
+      throw new IllegalStateException("Invalid vmdk: fields length is " + fields.size());
     }
 
     try {
-      return Integer.parseInt(fields[1]);
+      return Integer.parseInt(fields.get(1));
     } catch (NumberFormatException e) {
       logger.error("Invalid string: second field is not integer\n{}", s);
       throw new IllegalStateException("Invalid string: second field is not integer", e);
     }
+  }
+
+  /**
+   * Break an extent description into a set of tokens. See the extent description above.
+   *
+   * We don't simply split the string around spaces because the filename may have spaces in it.
+   *
+   * This is public so that we can easily test it.
+   */
+  public static List<String> extractExtentFields(String extentDescription) {
+    List<String> extentFields = new ArrayList<String>();
+    Matcher m = EXTENT_TOKEN_PATTERN.matcher(extentDescription);
+    while (m.find()) {
+      extentFields.add(m.group(1).replace("\"", ""));
+    }
+    return extentFields;
   }
 
 }
