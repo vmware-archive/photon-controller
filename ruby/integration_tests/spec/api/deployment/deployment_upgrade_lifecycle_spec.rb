721d2cf9c1e17f34b7bab012a92786e03600cdb0
Umer Khan <ukhan@vmware.com>
2016-03-10 14:31:11 -0800

    integration test: add api integration tests for pause_background_tasks
    Change-Id: I3b648852f05c0b51097cef878b5332193316b636

--- ruby/integration_tests/spec/api/deployment/deployment_upgrade_lifecycle_spec.rb
+++ ruby/integration_tests/spec/api/deployment/deployment_upgrade_lifecycle_spec.rb
@@ -21,9 +21,6 @@ describe "deployment upgrade lifecycle", life_cycle: true do
 
     after(:each) do
       client.resume_system(deployment.id)
-    end
-
-    after(:each) do
       items_to_cleanup.each do |item|
         item.delete unless item.nil?
       end
@@ -56,7 +53,45 @@ describe "deployment upgrade lifecycle", life_cycle: true do
           items_to_cleanup << create_tenant(name: random_name("tenant-"))
         end
       end
-    end
+
+      it "should pause_background/resume system successfully" do
+        expect(client.find_all_api_deployments.items.size).to eq 1
+
+        client.pause_background_tasks(deployment.id)
+
+        # tests that POSTs are accepted while system is pause_background_tasks
+        items_to_cleanup << create_tenant(name: random_name("tenant-"))
+        client.resume_system(deployment.id)
+
+          # testing that after resuming the system we accept posts again
+          items_to_cleanup << create_tenant(name: random_name("tenant-"))
+        end
+
+        it "should pause/pause_background/resume system successfully" do
+          expect(client.find_all_api_deployments.items.size).to eq 1
+
+          client.pause_system(deployment.id)
+          begin
+            items_to_cleanup << create_tenant(name: random_name("tenant-"))
+            fail("pause_system should fail")
+          rescue EsxCloud::ApiError => e
+            expect(e.response_code).to eq 403
+            expect(e.errors.size).to eq 1
+            expect(e.errors[0].code).to eq "SystemPaused"
+            expect(e.errors[0].message).to match /System is paused/
+          end
+
+          client.pause_background_tasks(deployment.id)
+
+          # testing that after pause_background_tasks the system we accept posts
+          items_to_cleanup << create_tenant(name: random_name("tenant-"))
+
+          client.resume_system(deployment.id)
+
+          # testing that after resuming the system we accept posts again
+          items_to_cleanup << create_tenant(name: random_name("tenant-"))
+        end
+      end
 
     context "when deployment does not exist" do
       it "should fail to pause system" do
@@ -64,6 +99,19 @@ describe "deployment upgrade lifecycle", life_cycle: true do
         begin
           client.pause_system("non-existing-deployment")
           fail("pause system should fail when the deployment does not exist")
+        rescue EsxCloud::ApiError => e
+          expect(e.response_code).to eq 404
+          expect(e.errors.size).to eq 1
+          expect(e.errors.first.code).to eq("DeploymentNotFound")
+          expect(e.errors.first.message).to include(error_msg)
+        end
+      end
+
+      it "should fail to pause background tasks" do
+        error_msg = "Deployment #non-existing-deployment not found"
+        begin
+          client.pause_background_tasks("non-existing-deployment")
+          fail("pause background should fail when the deployment does not exist")
         rescue EsxCloud::ApiError => e
           expect(e.response_code).to eq 404
           expect(e.errors.size).to eq 1
