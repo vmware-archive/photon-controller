7213735a541cea2082b10d42e8e1054f69610df3
Xuli Liu <xulil@vmware.com>
2016-03-17 09:22:48 -0700

    Virtual network object returned from api-fe
    Change-Id: Iaa68b3ab9938c8b64a84cf928e15077bb63afe05

---
+++ java/api/src/main/java/com/vmware/photon/controller/api/VirtualNetwork.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2016 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.photon.controller.api;
+
+import com.vmware.photon.controller.api.base.VisibleModel;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.wordnik.swagger.annotations.ApiModel;
+import com.wordnik.swagger.annotations.ApiModelProperty;
+
+import javax.validation.constraints.NotNull;
+
+import java.util.Objects;
+
+/**
+ * VirtualNetwork represents an NSX network returned from api-fe to users.
+ */
+@ApiModel(value = "This class represents an NSX network")
+public class VirtualNetwork extends VisibleModel {
+
+  public static final String KIND = "virtualNetwork";
+
+  @JsonProperty
+  @ApiModelProperty(value = "kind=\"virtualNetwork\"", required = true)
+  private String kind = KIND;
+
+  @JsonProperty
+  @ApiModelProperty(value = "Description to the virtual network", required = false)
+  private String description;
+
+  @JsonProperty
+  @ApiModelProperty(value = "Supplies the state of the network",
+      allowableValues = "CREATING,READY,ERROR,DELETED,PENDING_DELETE",
+      required = true)
+  @NotNull
+  private NetworkState state;
+
+  @JsonProperty
+  @ApiModelProperty(value = "Whether allow the VMs on this network to access Internet", required = true)
+  @NotNull
+  private boolean allowToAccessInternet;
+
+  @Override
+  public String getKind() {
+    return kind;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public NetworkState getState() {
+    return state;
+  }
+
+  public void setState(NetworkState state) {
+    this.state = state;
+  }
+
+  public boolean getAllowToAccessInternet() {
+    return allowToAccessInternet;
+  }
+
+  public void setAllowToAccessInternet(boolean allowToAccessInternet) {
+    this.allowToAccessInternet = allowToAccessInternet;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+
+    if (o == null || this.getClass() != o.getClass()) {
+      return false;
+    }
+
+    VirtualNetwork other = (VirtualNetwork) o;
+
+    return Objects.equals(this.getName(), other.getName())
+        && Objects.equals(this.description, other.description)
+        && Objects.equals(this.state, other.state)
+        && Objects.equals(this.allowToAccessInternet, other.allowToAccessInternet);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), this.description, this.state, this.allowToAccessInternet);
+  }
+
+  @Override
+  public String toString() {
+    return com.google.common.base.Objects.toStringHelper(this)
+        .add("name", getName())
+        .add("description", description)
+        .add("state", state)
+        .add("allowToAccessInternet", allowToAccessInternet)
+        .toString();
+  }
+}
