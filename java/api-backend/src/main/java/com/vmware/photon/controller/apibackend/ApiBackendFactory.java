4fb4cfbb393a8ba7b98fa3d3794a3adc4a4b15ec
Umer Khan <ukhan@vmware.com>
2016-03-26 17:42:34 -0700

    api-backend: Add first xenon service with necessary xenon support
    - This includes testing support. No guice.
    - Real wiring with nsx will come next.
    
    Change-Id: Id8d62c5f349ac5c1d624304e02346a58807e9170

---
+++ java/api-backend/src/main/java/com/vmware/photon/controller/apibackend/ApiBackendFactory.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2015 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.photon.controller.apibackend;
+
+import com.vmware.photon.controller.apibackend.tasks.CreateLogicalRouterTaskService;
+import com.vmware.xenon.common.FactoryService;
+import com.vmware.xenon.common.Service;
+
+import com.google.common.collect.ImmutableMap;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
+/**
+ * Common factory used for ApiBackend.
+ */
+public class ApiBackendFactory {
+
+  private CloseableHttpAsyncClient httpAsyncClient;
+
+  /**
+   * All Xenon Factory Services in api-backend.
+   */
+  public static final Map<Class<? extends Service>, Supplier<FactoryService>> FACTORY_SERVICES_MAP = ImmutableMap.of(
+      CreateLogicalRouterTaskService.class, CreateLogicalRouterTaskService::createFactory
+  );
+  /**
+   * Overloaded Constructor.
+   *
+   * @param httpAsyncClient
+   */
+  public ApiBackendFactory(CloseableHttpAsyncClient httpAsyncClient) {
+    this.httpAsyncClient = httpAsyncClient;
+  }
+}
