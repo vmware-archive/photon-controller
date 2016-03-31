7cbe290b3b4e4be78fdf8a941e7123fe79108bfa
Umer Khan <ukhan@vmware.com>
2016-03-30 17:51:58 -0700

    nsx-client: add create/get/delete api calls for logical router.
    - removes thrift file as well.
    
    Change-Id: I2c3efe796c06fabb734a79983e889ff07957035f

---
+++ java/nsx-client/src/main/java/com/vmware/photon/controller/nsxclient/apis/LogicalRouterApi.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2016 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.photon.controller.nsxclient.apis;
+
+import com.vmware.photon.controller.nsxclient.RestClient;
+import com.vmware.photon.controller.nsxclient.models.LogicalRouter;
+import com.vmware.photon.controller.nsxclient.models.LogicalRouterCreateSpec;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.google.common.util.concurrent.FutureCallback;
+import org.apache.http.HttpStatus;
+
+import java.io.IOException;
+
+/**
+ * This is the class of the NSX Logical Router API implementation.
+ */
+public class LogicalRouterApi extends NsxClientApi {
+
+  public final String logicalRouterBasePath = basePath + "/logical-routers";
+
+  /**
+   * Constructs a LogicalRouterApi class.
+   *
+   * @param restClient
+   */
+  public LogicalRouterApi(RestClient restClient) {
+    super(restClient);
+  }
+
+  /**
+   * Creates a logical router.
+   */
+  public LogicalRouter createLogicalRouter(LogicalRouterCreateSpec spec) throws IOException {
+    return post(logicalRouterBasePath,
+        serializeObjectAsJson(spec),
+        HttpStatus.SC_CREATED,
+        new TypeReference<LogicalRouter>() {}
+    );
+  }
+
+  /**
+   * Creates a logical router async.
+   */
+  public void createLogicalRouterAsync(LogicalRouterCreateSpec spec, FutureCallback<LogicalRouter> responseCallback)
+      throws IOException {
+    postAsync(logicalRouterBasePath,
+        serializeObjectAsJson(spec),
+        HttpStatus.SC_CREATED,
+        new TypeReference<LogicalRouter>() {
+        },
+        responseCallback
+    );
+  }
+
+  /**
+   * Gets a logical router.
+   */
+  public LogicalRouter getLogicalRouter(String id) throws IOException {
+    return get(logicalRouterBasePath + "/" + id,
+        HttpStatus.SC_OK,
+        new TypeReference<LogicalRouter>() {}
+    );
+  }
+
+  /**
+   * Gets a logical router async.
+   */
+  public void getLogicalRouterAsync(String id, FutureCallback<LogicalRouter> responseCallback)
+      throws IOException {
+    getAsync(logicalRouterBasePath + "/" + id,
+        HttpStatus.SC_OK,
+        new TypeReference<LogicalRouter>() {},
+        responseCallback
+    );
+  }
+
+  /**
+   * Deletes a logical router.
+   */
+  public void deleteLogicalRouter(String id) throws IOException {
+    delete(logicalRouterBasePath + "/" + id, HttpStatus.SC_OK);
+  }
+
+  /**
+   * Deletes a logical router async.
+   */
+  public void deleteLogicalRouterAsync(String id, FutureCallback<Void> responseCallback)
+      throws IOException {
+    deleteAsync(logicalRouterBasePath + "/" + id, HttpStatus.SC_OK, responseCallback);
+  }
+}
---
+++ java/nsx-client/src/main/java/com/vmware/photon/controller/nsxclient/models/LogicalRouter.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2016 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.photon.controller.nsxclient.models;
+
+import com.vmware.photon.controller.nsxclient.datatypes.RouterType;
+import com.vmware.photon.controller.nsxclient.utils.ToStringHelper;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Objects;
+
+/**
+ * This class represents a LogicalRouter JSON structure.
+ */
+@JsonIgnoreProperties(ignoreUnknown =  true)
+public class LogicalRouter {
+
+  @JsonProperty(value = "id", required = true)
+  private String id;
+
+  @JsonProperty(value = "resource_type", required = false)
+  private String resourceType;
+
+  @JsonProperty(value = "config", required = false)
+  private LogicalRouterConfig logicalRouterConfig;
+
+  @JsonProperty(value = "router_type", required = true)
+  private RouterType routerType;
+
+  @JsonProperty(value = "display_name", required = false)
+  private String displayName;
+
+  @JsonProperty(value = "description", required = false)
+  private String description;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getResourceType() {
+    return resourceType;
+  }
+
+  public void setResourceType(String resourceType) {
+    this.resourceType = resourceType;
+  }
+
+  public RouterType getRouterType() {
+    return routerType;
+  }
+
+  public void setRouterType(RouterType routerType) {
+    this.routerType = routerType;
+  }
+
+  public String getDescription() {
+    return this.description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getDisplayName() {
+    return this.displayName;
+  }
+
+  public void setDisplayName(String displayName) {
+    this.displayName = displayName;
+  }
+
+  public LogicalRouterConfig getLogicalRouterConfig() {
+    return this.logicalRouterConfig;
+  }
+
+  public void setLogicalRouterConfig(LogicalRouterConfig logicalRouterConfig) {
+    this.logicalRouterConfig = logicalRouterConfig;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    LogicalRouter other = (LogicalRouter) o;
+    return Objects.equals(getId(), other.getId())
+        && Objects.equals(getResourceType(), other.getResourceType())
+        && Objects.deepEquals(getLogicalRouterConfig(), other.getLogicalRouterConfig())
+        && Objects.equals(getRouterType(), other.getRouterType())
+        && Objects.equals(getDisplayName(), other.getDisplayName())
+        && Objects.equals(getDescription(), other.getDescription());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(),
+        getId(),
+        getResourceType(),
+        getLogicalRouterConfig(),
+        getRouterType(),
+        getDisplayName(),
+        getDescription());
+  }
+
+  @Override
+  public String toString() {
+    return ToStringHelper.jsonObjectToString(this);
+  }
+}
