90db6cfcb299db0c08494a652e44d312159844fc
York Wei <wyork@vmware.com>
2016-04-08 17:16:21 -0700

    scheduler: move flatscheduler placement into a xenon service
    The PlacementTaskService replicates the behavior of the FlatSchedulerService
    place call, but as a Xenon service.
    The thrift PlaceRequest and PlaceResponse remain unchanged to preserve the
    interaction with apife.
    SchedulerDcpHost is renamed to SchedulerXenonHost.
    The SchedulerXenonHost includes factories and dependencies for the PlacementTaskService.
    ServiceUriPaths includes a path for scheduler services.
    
    Change-Id: Id745cb4255a75d1135d6e0d19f86eaf417bad693

---
+++ java/root-scheduler/src/test/java/com/vmware/photon/controller/rootscheduler/helpers/xenon/TestHost.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2016 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.photon.controller.rootscheduler.helpers.xenon;
+
+import com.vmware.photon.controller.common.xenon.BasicServiceHost;
+import com.vmware.xenon.common.Operation;
+
+import java.util.logging.LogManager;
+
+/**
+ * This class implements helper routines used to test service hosts in isolation.
+ */
+public class TestHost extends BasicServiceHost {
+
+  public TestHost() {
+    super();
+  }
+
+  public static TestHost create() throws Throwable {
+    TestHost host = new TestHost();
+    host.initialize();
+    host.startWithCoreServices();
+    return host;
+  }
+
+  @Override
+  public void destroy() throws Throwable {
+    super.destroy();
+    LogManager.getLogManager().reset();
+  }
+
+  @Override
+  public Operation sendRequestAndWait(Operation op) throws Throwable {
+    Operation operation = super.sendRequestAndWait(op);
+    // For tests we check status code 200 to see if the response is OK
+    // If nothing is changed in patch, it returns 304 which means not modified.
+    // We will treat 304 as 200
+    if (operation.getStatusCode() == 304) {
+      operation.setStatusCode(200);
+    }
+    return operation;
+  }
+}
