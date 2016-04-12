f8fb476928a6d541771f2ec8e14af58efe8907f1
Ashok Chandrasekar <ashokc@vmware.com>
2016-04-12 15:40:40 -0700

    hk: fix image_cleaner integration test
    This change fixes the housekeeper image cleaner integration test bug
    where image cleaner will fail if there is an existing image cleaner task
    which is running for the datastore that we are triggering a new image
    cleaner task for. We fix it by waiting for the currently running task to
    complete if any and make sure it completes fine, if not we create a new
    task.
    
    Change-Id: I6e6ea0f93e2b3135403f1174646a68e4098cc51e

--- ruby/integration_tests/spec/housekeeper/image_cleaner_spec.rb
+++ ruby/integration_tests/spec/housekeeper/image_cleaner_spec.rb
@@ -15,6 +15,7 @@ describe "Image Cleaner", housekeeper: true do
   let(:service_link) { "/photon/housekeeper/image-cleaners" }
   let(:client) { EsxCloud::Dcp::HouskeeperClient.instance }
   let(:task_completion_stages) { ["FINISHED", "FAILED", "CANCELLED"] }
+  let(:task_start_stages) { ["CREATED", "STARTED"] }
 
   it "runs succesfully" do
     payload = {
@@ -22,15 +23,40 @@ describe "Image Cleaner", housekeeper: true do
       imageDeleteWatermarkTime: Time.now.to_i
     }
 
-    task = run_task payload
+    task = get_existing_task
+    task = run_task payload if task.nil?
     expect(task["taskInfo"]["stage"]).to eq "FINISHED"
     expect(task["dataStoreCount"]).to be >= 1
     expect(task["finishedDeletes"]).to eq task["dataStoreCount"]
     expect(task["failedOrCanceledDeletes"]).to eq 0
   end
 
+  def get_existing_task
+    tasks = client.get service_link + "?expand"
+    running_task_count = 0
+    running_task = nil
+    tasks["documents"].each do |document|
+      task = document[1]
+      if task_start_stages.include? task["taskInfo"]["stage"]
+        running_task_count += 1
+        running_task = task
+      end
+    end
+
+    if !running_task.nil?
+      expect(running_task_count).to eq(1)
+      poll_task running_task
+    else
+      nil
+    end
+  end
+
   def run_task(payload)
     task = client.post service_link, payload
+    poll_task task
+  end
+
+  def poll_task(task)
     task = client.poll_task(task["documentSelfLink"], 10, 100) do |b|
       task_completion_stages.include? b["taskInfo"]["stage"]
     end
