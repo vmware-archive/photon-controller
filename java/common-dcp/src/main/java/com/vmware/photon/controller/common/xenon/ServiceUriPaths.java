18afea603eb1729a022a8a19c444715db9907bf4
Gokcen Iskender <giskender@vmware.com>
2016-04-18 10:30:01 -0700

    Revert "Bare Metal Provisioner component"
    This reverts commit 1d182bc15057a2148da301ec622437bf1c1bf521.
    
    Change-Id: I6fb23c43d5fac086ed2a7d832e7587c8e2107371

--- java/common-dcp/src/main/java/com/vmware/photon/controller/common/xenon/ServiceUriPaths.java
+++ java/common-dcp/src/main/java/com/vmware/photon/controller/common/xenon/ServiceUriPaths.java
@@ -29,8 +29,6 @@ public class ServiceUriPaths extends com.vmware.xenon.services.common.ServiceUri
 
   public static final String CLUSTERMANAGER_ROOT = SERVICES_ROOT + "/clustermanager";
 
-  public static final String BARE_METAL_PROVISIONER_ROOT = SERVICES_ROOT + "/bmp";
-
   public static final String APIBACKEND_ROOT = SERVICES_ROOT + "/apibackend";
 
   public static final String CLUSTER_RESIZE_TASK_SERVICE =
