b86f3a051de777d17959196c5a15a72f46d7f901
Gokcen Iskender <giskender@vmware.com>
2016-04-20 11:56:56 -0700

    Generate BMP OVA - dhcp-lease and dhcp-lease-expiry are not parameters to ova - we are using photon and tdnf install to install appliances - leases can be accessed from web browser
    Change-Id: If98c8e3cc98f4162aecb9b30d5970a6e6517422b

--- appliances/bmp/build.sh
+++ appliances/bmp/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -xe
 
 SSHD_ENABLE_ROOT_LOGIN=${SSHD_ENABLE_ROOT_LOGIN:-"false"}
-PHOTON_VM_OVA_URL=${PHOTON_VM_OVA_URL:-"https://dl.bintray.com/vmware/photon/ova/1.0TP2/x86_64/photon-1.0TP2.ova"}
-PHOTON_ISO_URL=${ISO_URL:="https://dl.bintray.com/vmware/photon/iso/1.0TP2/x86_64/photon-minimal-1.0TP2.iso"}
-PHOTON_ISO_SHA1=${ISO_SHA1:="a47567368458acd8c8ef5f1e3f793c5329063dac"}
+
+PHOTON_ISO_URL=${ISO_URL:="http://artifactory.ec.eng.vmware.com/artifactory/esxcloud-archives/userContent/bmp/photon-1.0-0185afd.iso"}
+PHOTON_ISO_SHA1=${ISO_SHA1:="6cc1c646677ff8b8b48570b75286e496c85790f8"}
 VBOX_ISO_URL=${VBOX_ISO_URL:="http://artifactory.ec.eng.vmware.com/artifactory/esxcloud-archives/userContent/vbox/VBoxGuestAdditions_5.0.14.iso"}
 VBOX_ISO_SHA256=${VBOX_ISO_SHA256:="cec0df18671adfe62a34d3810543f76f76206b212b2b61791fe026214c77507c"}
 
@@ -13,6 +13,28 @@ if [ ! -d "bmp/installer-pxe-modules" ]; then
   exit -1
 fi
 
+## build the base appliance ova
+#packer build -force \
+#  -var "photon_iso_url=$PHOTON_ISO_URL" \
+#  -var "photon_iso_sha1=$PHOTON_ISO_SHA1" \
+#  -var "vbox_iso_url=$VBOX_ISO_URL" \
+#  -var "vbox_iso_sha256=$VBOX_ISO_SHA256" \
+#  -var "sshd_enable_root_login=$SSHD_ENABLE_ROOT_LOGIN" \
+#  -var "photon_vm_ova=$PHOTON_VM_OVA_VB" \
+#  bmp-base-vm.json
+#
+#
+## modify the created ova to be usable by ovftools
+#cd build
+#tar -cf bmp-base-vm-vb.ova *.ovf *.vmdk
+#rm *.ovf *.vmdk
+#
+#cp bmp-base-vm-vb.ova ..
+#cd ..
+
+PHOTON_VM_OVA_VB=./bmp-base-vm-vb.ova
+PHOTON_CACHED_ISO=./packer_cache/3d61cc7aa2b844de59ed35aae984d70e4bb6158e379a02319cbd19ec65595e6e.iso
+
 # build the appliance ova
 packer build -force \
   -var "photon_iso_url=$PHOTON_ISO_URL" \
@@ -21,12 +43,9 @@ packer build -force \
   -var "vbox_iso_sha256=$VBOX_ISO_SHA256" \
   -var "sshd_enable_root_login=$SSHD_ENABLE_ROOT_LOGIN" \
   -var "photon_vm_ova=$PHOTON_VM_OVA_VB" \
+  -var "photon_cached_iso=$PHOTON_CACHED_ISO" \
   bmp-vm.json
 
-
-# modify the created ova to be usable by ovftools
 cd build
-tar -cf bmp-vm-vb.ova *.ovf *.vmdk
-rm *.ovf *.vmdk
 ../scripts/to-vmware-ovf.sh "bmp-vm-vb" "bmp-vm"
 cd ..
