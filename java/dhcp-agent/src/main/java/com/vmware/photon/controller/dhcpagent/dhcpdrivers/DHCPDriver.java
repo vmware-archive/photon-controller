43c027b096330d740c6a991875f4a41f07ee36d0
FatimaIjaz <fijaz@vmware.com>
2016-04-25 16:30:51 -0700

    Added DHCP Driver for usage by DHCP agent: 1) Added DHCP driver interface 2) Implemented Dnsmasq driver
    Change-Id: I1e68e345a17ab99bff7b19d48cee82332b677d2b

---
+++ java/dhcp-agent/src/main/java/com/vmware/photon/controller/dhcpagent/dhcpdrivers/DHCPDriver.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2015 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.photon.controller.dhcpagent.dhcpdrivers;
+
+/**
+ * Interface defining the functionality to be implemented by all DHCP drivers.
+ */
+public interface DHCPDriver {
+
+    DriverResponse releaseIP(String networkInterface, String ipAddress, String macAddress);
+
+    /**
+     * Class to hold the response for Driver operations.
+     */
+    class DriverResponse {
+        public int exitCode = 0;
+        public String stdError = "";
+    }
+}
---
+++ java/dhcp-agent/src/main/java/com/vmware/photon/controller/dhcpagent/dhcpdrivers/DnsmasqDriver.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2015 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.photon.controller.dhcpagent.dhcpdrivers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Class implements Driver interface for Dnsmasq DHCP server.
+ */
+public class DnsmasqDriver implements DHCPDriver {
+
+    String utilityPath = "/usr/local/bin";
+    String utility = "dhcp_release";
+
+    public DnsmasqDriver(String utilityPath) {
+        this.utilityPath = utilityPath;
+    }
+
+    public DriverResponse releaseIP(String networkInterface, String ipAddress, String macAddress) {
+        DriverResponse response = new DriverResponse();
+
+        try {
+            String command = String.format("%s/%s %s %s %s", utilityPath, utility, networkInterface,
+                    ipAddress, macAddress);
+            Process p = Runtime.getRuntime().exec(command);
+
+           if (p.exitValue() != 0) {
+               response.exitCode = p.exitValue();
+
+               String s;
+               BufferedReader stdError = new BufferedReader(new
+                       InputStreamReader(p.getErrorStream()));
+
+               while ((s = stdError.readLine()) != null) {
+                   response.stdError += s;
+               }
+            }
+        } catch (IOException e) {
+            response.exitCode = -1;
+            response.stdError = e.toString();
+        } finally {
+            return response;
+        }
+    }
+}
