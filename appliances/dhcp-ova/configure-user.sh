1a1cfa8d118f6b8b5e50c70672867d3a69ec4d3a
Pankaj Lakhina <plakhina@vmware.com>
2016-04-26 14:05:46 -0700

    Remove unused file from dhcp image building
    Change-Id: I988127558856cf5b9a9400157ab839fdace9b775

--- appliances/dhcp-ova/configure-user.sh
+++
@@ -1,7 +0,0 @@
-HOME=/home/photon
-mkdir $HOME
-groupadd photon
-useradd -G users,photon,sudo photon -d $HOME
-echo photon:changeme | chpasswd
-echo "photon ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-chown -R photon:photon $HOME
