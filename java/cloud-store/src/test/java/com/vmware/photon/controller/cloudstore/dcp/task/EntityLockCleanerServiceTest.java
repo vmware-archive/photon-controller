fa76198c3823f8024f61718d8186fcc146b27501
Pankaj Lakhina <plakhina@vmware.com>
2016-04-28 15:33:32 -0700

    Use idempotent posts for implementing entity locks
    As per best practices for Xenon, we should not delete documents that we
    expect to re-create again.
    (Replication is tricky when documents are deleted and recreated).
    Therefore this commit changes how we deal with Entity locks.
    Instead of deleting a lock when we are done with it, we mark it as
    released.
    The EntityLockCleaner currently detects if there is lock whose
    associated owner task is in terminal state and releases that lock.
    We need a follow up change to delete the lock if the the associated
    entity has been deleted from the system.
    
    Change-Id: I704852fe8eeb69d68b578420b100f6944dcae57d

--- java/cloud-store/src/test/java/com/vmware/photon/controller/cloudstore/dcp/task/EntityLockCleanerServiceTest.java
+++ java/cloud-store/src/test/java/com/vmware/photon/controller/cloudstore/dcp/task/EntityLockCleanerServiceTest.java
@@ -19,18 +19,13 @@ import com.vmware.photon.controller.cloudstore.dcp.entity.TaskService;
 import com.vmware.photon.controller.cloudstore.dcp.entity.TaskServiceFactory;
 import com.vmware.photon.controller.cloudstore.dcp.helpers.TestEnvironment;
 import com.vmware.photon.controller.common.xenon.BasicServiceHost;
-import com.vmware.photon.controller.common.xenon.ServiceHostUtils;
 import com.vmware.photon.controller.common.xenon.ServiceUtils;
 import com.vmware.photon.controller.common.xenon.exceptions.BadRequestException;
-import com.vmware.photon.controller.common.xenon.exceptions.DocumentNotFoundException;
 import com.vmware.xenon.common.Operation;
 import com.vmware.xenon.common.Service;
-import com.vmware.xenon.common.ServiceDocument;
-import com.vmware.xenon.common.ServiceHost;
 import com.vmware.xenon.common.TaskState;
 import com.vmware.xenon.common.UriUtils;
 import com.vmware.xenon.common.Utils;
-import com.vmware.xenon.services.common.QueryTask;
 import static com.vmware.photon.controller.cloudstore.dcp.entity.TaskService.State.TaskState.COMPLETED;
 import static com.vmware.photon.controller.cloudstore.dcp.entity.TaskService.State.TaskState.ERROR;
 import static com.vmware.photon.controller.cloudstore.dcp.entity.TaskService.State.TaskState.QUEUED;
@@ -43,19 +38,18 @@ import org.testng.annotations.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.startsWith;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.fail;
 
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Predicate;
 
 /**
  * Tests {@link EntityLockCleanerService}.
@@ -177,7 +171,7 @@ public class EntityLockCleanerServiceTest {
           {"taskState", state},
           {"isSelfProgressionDisabled", false},
           {"danglingEntityLocks", 0},
-          {"deletedEntityLocks", 0}
+          {"releasedEntityLocks", 0}
       };
     }
 
@@ -331,7 +325,7 @@ public class EntityLockCleanerServiceTest {
           (EntityLockCleanerService.State state) -> state.taskState.stage == TaskState.TaskStage.FINISHED);
 
       assertThat(response.danglingEntityLocks, is(0));
-      assertThat(response.deletedEntityLocks, is(0));
+      assertThat(response.releasedEntityLocks, is(0));
     }
 
     /**
@@ -355,10 +349,10 @@ public class EntityLockCleanerServiceTest {
           (EntityLockCleanerService.State state) -> state.taskState.stage == TaskState.TaskStage.FINISHED);
       assertThat(response.danglingEntityLocks,
           is(Integer.min(danglingEntityLocks, EntityLockCleanerService.DEFAULT_PAGE_LIMIT)));
-      assertThat(response.deletedEntityLocks,
+      assertThat(response.releasedEntityLocks,
           is(Integer.min(danglingEntityLocks, EntityLockCleanerService.DEFAULT_PAGE_LIMIT)));
 
-      verifyLockStatusAfterCleanup(machine, totalEntityLocks, danglingEntityLocks, hostCount);
+      verifyLockStatusAfterCleanup(machine, totalEntityLocks, danglingEntityLocks);
     }
 
     private void freeTestEnvironment(TestEnvironment machine) throws Throwable {
@@ -409,7 +403,8 @@ public class EntityLockCleanerServiceTest {
         // create associated entity lock
         EntityLockService.State entityLock = new EntityLockService.State();
         entityLock.entityId = "entity-id" + i;
-        entityLock.taskId = ServiceUtils.getIDFromDocumentSelfLink(createdTask.documentSelfLink);
+        entityLock.ownerTaskId = ServiceUtils.getIDFromDocumentSelfLink(createdTask.documentSelfLink);
+        entityLock.lockOperation = EntityLockService.State.LockOperation.ACQUIRE;
         entityLock.documentSelfLink = EntityLockServiceFactory.SELF_LINK + "/" + entityLock.entityId;
         Operation entityLockOperation = env.sendPostAndWait(EntityLockServiceFactory.SELF_LINK, entityLock);
         EntityLockService.State createdEntityLock = entityLockOperation.getBody(EntityLockService.State.class);
@@ -421,49 +416,19 @@ public class EntityLockCleanerServiceTest {
 
     private void verifyLockStatusAfterCleanup(TestEnvironment env,
                                               int totalEntityLocks,
-                                              int danglingEntityLocks,
-                                              int hostCount) throws Throwable {
+                                              int danglingEntityLocks) throws Throwable {
       Integer expectedNumberOfReleasedLocks =
           Integer.min(danglingEntityLocks, EntityLockCleanerService.DEFAULT_PAGE_LIMIT);
 
-      if (hostCount > 1 && expectedNumberOfReleasedLocks > 0) {
-        Integer expectedNumberOfValidLocksRemaining = totalEntityLocks - expectedNumberOfReleasedLocks;
-
-        QueryTask.Query kindClause = new QueryTask.Query()
-            .setTermPropertyName(ServiceDocument.FIELD_NAME_KIND)
-            .setTermMatchValue(Utils.buildKind(EntityLockService.State.class));
-
-        QueryTask.QuerySpecification spec = new QueryTask.QuerySpecification();
-        spec.query.addBooleanClause(kindClause);
-
-        QueryTask queryTask = QueryTask.create(spec)
-            .setDirect(true);
-
-        ServiceHostUtils.waitForQuery((ServiceHost) machine.getHosts()[0],
-            BasicServiceHost.REFERRER,
-            queryTask,
-            new Predicate<QueryTask>() {
-              @Override
-              public boolean test(QueryTask queryTask) {
-                return queryTask.results.documentLinks.size() == expectedNumberOfValidLocksRemaining;
-              }
-            });
-      }
-
       for (int i = 0; i < totalEntityLocks; i++) {
+        EntityLockService.State entityLock = env.getServiceState(EntityLockServiceFactory.SELF_LINK + "/entity-id" + i,
+            EntityLockService.State.class);
+        assertThat(entityLock, is(notNullValue()));
+        assertThat(entityLock.lockOperation, is(nullValue()));
         if (i < expectedNumberOfReleasedLocks) {
-          try {
-            URI uri = UriUtils.buildUri(machine.getHosts()[0], EntityLockServiceFactory.SELF_LINK + "/entity-id" + i);
-            Operation op = Operation.createGet(uri);
-            op.addPragmaDirective(Operation.PRAGMA_DIRECTIVE_NO_QUEUING); // this will make sure tests are not slow
-            machine.sendRequestAndWait(op, machine.getHosts()[0]);
-            fail("GET for entity lock should have failed");
-          } catch (DocumentNotFoundException ignored) {
-          }
+          assertThat(entityLock.ownerTaskId, is(nullValue()));
         } else {
-          EntityLockService.State state = env.getServiceState(EntityLockServiceFactory.SELF_LINK + "/entity-id" + i,
-              EntityLockService.State.class);
-          assertThat(state, is(notNullValue()));
+          assertThat(entityLock.ownerTaskId, is(notNullValue()));
         }
       }
     }
