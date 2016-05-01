712a238523f3462923ebcc93c43fc60297548f6a
Long Zhou <longz@vmware.com>
2016-05-01 15:26:19 -0700

    Agent: delete fake hypervisor
    Change-Id: I4a8a565e95afb43c4d4de71116a4e4d413844ef2

--- python/src/host/host/tests/unit/hypervisor/test_hypervisor.py
+++ python/src/host/host/tests/unit/hypervisor/test_hypervisor.py
@@ -14,14 +14,12 @@ import unittest
 
 from hamcrest import *  # noqa
 from mock import MagicMock
-from mock import patch
 
 from host.tests.unit.services_helper import ServicesHelper
 
 from agent.agent_config import AgentConfig
 from common.file_util import mkdtemp
 from host.hypervisor.hypervisor import Hypervisor
-from host.hypervisor.esx.vim_client import VimClient
 
 
 class TestHypervisor(unittest.TestCase):
@@ -34,22 +32,6 @@ class TestHypervisor(unittest.TestCase):
 
     def tearDown(self):
         self.services_helper.teardown()
-
-    @patch("pysdk.connect.Connect")
-    @patch("pyVmomi.vim.ServiceInstance")
-    @patch.object(VimClient, "_poll_updates")
-    @patch.object(VimClient, "_acquire_local_credentials", return_value=("username", "password"))
-    def test_hypervisor(self, _credentials_mock, update_mock, si_mock, connect_mock):
-        self.agent_config = AgentConfig(["--config-path", self.agent_config_dir, "--hypervisor", "fake"])
-        Hypervisor(self.agent_config)
-
-    def test_hypervisor_setter(self):
-        self.agent_config = AgentConfig(["--config-path", self.agent_config_dir, "--hypervisor", "fake"])
-        hypervisor = Hypervisor(self.agent_config)
-        hypervisor.set_cpu_overcommit(2.0)
-        assert_that(hypervisor.cpu_overcommit, equal_to(2.0))
-        hypervisor.set_memory_overcommit(3.0)
-        assert_that(hypervisor.memory_overcommit, equal_to(3.0))
 
     def test_unknown_hypervisor(self):
         self.agent_config = AgentConfig(["--config-path", self.agent_config_dir, "--hypervisor", "dummy"])
