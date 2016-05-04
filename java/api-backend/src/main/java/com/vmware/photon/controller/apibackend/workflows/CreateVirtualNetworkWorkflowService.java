9324603c119f8cf9dc62e5876f30540868c2f6c1
Yu Sheng <ysheng@vmware.com>
2016-05-04 11:49:31 -0700

    nsx: implement GetNsxConfiguration sub-stage in CreateVirtualNetworkWorkflow
    Change-Id: Ie930ffbce80f3d1d8b80c27ccaac0f1297eea280

--- java/api-backend/src/main/java/com/vmware/photon/controller/apibackend/workflows/CreateVirtualNetworkWorkflowService.java
+++ java/api-backend/src/main/java/com/vmware/photon/controller/apibackend/workflows/CreateVirtualNetworkWorkflowService.java
@@ -17,14 +17,22 @@ import com.vmware.photon.controller.api.NetworkState;
 import com.vmware.photon.controller.api.RoutingType;
 import com.vmware.photon.controller.apibackend.servicedocuments.CreateVirtualNetworkWorkflowDocument;
 import com.vmware.photon.controller.apibackend.utils.ServiceHostUtils;
+import com.vmware.photon.controller.cloudstore.dcp.entity.DeploymentService;
 import com.vmware.photon.controller.cloudstore.dcp.entity.VirtualNetworkService;
 import com.vmware.photon.controller.common.xenon.ControlFlags;
 import com.vmware.photon.controller.common.xenon.OperationUtils;
+import com.vmware.photon.controller.common.xenon.QueryTaskUtils;
 import com.vmware.photon.controller.common.xenon.ServiceUriPaths;
 import com.vmware.photon.controller.common.xenon.ServiceUtils;
 import com.vmware.xenon.common.FactoryService;
 import com.vmware.xenon.common.Operation;
+import com.vmware.xenon.common.ServiceDocument;
 import com.vmware.xenon.common.TaskState;
+import com.vmware.xenon.common.Utils;
+import com.vmware.xenon.services.common.NodeGroupBroadcastResponse;
+import com.vmware.xenon.services.common.QueryTask;
+
+import java.util.Set;
 
 /**
  * This class implements a Xenon service representing a workflow to create a virtual network.
@@ -126,10 +134,13 @@ public class CreateVirtualNetworkWorkflowService extends BaseWorkflowService<Cre
     }
   }
 
+  /**
+   * Processes the sub-stages of the workflow.
+   */
   private void processPatch(CreateVirtualNetworkWorkflowDocument state) throws Throwable {
     switch(state.taskState.subStage) {
       case GET_NSX_CONFIGURATION:
-        progress(state, CreateVirtualNetworkWorkflowDocument.TaskState.SubStage.CREATE_LOGICAL_SWITCH);
+        getNsxConfiguration(state);
         break;
       case CREATE_LOGICAL_SWITCH:
         progress(state, CreateVirtualNetworkWorkflowDocument.TaskState.SubStage.CREATE_LOGICAL_ROUTER);
@@ -141,6 +152,70 @@ public class CreateVirtualNetworkWorkflowService extends BaseWorkflowService<Cre
         finish(state);
         break;
     }
+  }
+
+  /**
+   * Gets NSX configuration from {@link DeploymentService.State} entity in cloud-store, and save
+   * the configuration in the document of the workflow service.
+   */
+  private void getNsxConfiguration(CreateVirtualNetworkWorkflowDocument state) {
+
+    QueryTask.QuerySpecification querySpecification = new QueryTask.QuerySpecification();
+    querySpecification.query = new QueryTask.Query()
+        .setTermPropertyName(ServiceDocument.FIELD_NAME_KIND)
+        .setTermMatchValue(Utils.buildKind(DeploymentService.State.class));
+    QueryTask queryTask = QueryTask.create(querySpecification).setDirect(true);
+
+    ServiceHostUtils.getCloudStoreHelper(getHost())
+        .createBroadcastPost(ServiceUriPaths.CORE_LOCAL_QUERY_TASKS, ServiceUriPaths.DEFAULT_NODE_SELECTOR)
+        .setBody(queryTask)
+        .setCompletion((op, ex) -> {
+          if (ex != null) {
+            fail(state, ex);
+            return;
+          }
+
+          NodeGroupBroadcastResponse queryResponse = op.getBody(NodeGroupBroadcastResponse.class);
+          Set<String> documentLinks = QueryTaskUtils.getBroadcastQueryDocumentLinks(queryResponse);
+          if (documentLinks.size() != 1) {
+            fail(state, new IllegalStateException(
+                String.format("Found %d deployment service(s).", documentLinks.size())));
+          }
+
+          getNsxConfiguration(state, documentLinks.iterator().next());
+        })
+        .sendWith(this);
+  }
+
+  /**
+   * Gets NSX configuration from {@link DeploymentService.State} entity in cloud-store, and save
+   * the configuration in the document of the workflow service.
+   */
+  private void getNsxConfiguration(CreateVirtualNetworkWorkflowDocument state,
+                                   String deploymentServiceStateLink) {
+    ServiceHostUtils.getCloudStoreHelper(getHost())
+        .createGet(deploymentServiceStateLink)
+        .setCompletion((op, ex) -> {
+          if (ex != null) {
+            fail(state, ex);
+            return;
+          }
+
+          try {
+            DeploymentService.State deploymentState = op.getBody(DeploymentService.State.class);
+            CreateVirtualNetworkWorkflowDocument patchState = buildPatch(
+                TaskState.TaskStage.STARTED,
+                CreateVirtualNetworkWorkflowDocument.TaskState.SubStage.CREATE_LOGICAL_SWITCH);
+            patchState.nsxManagerEndpoint = deploymentState.networkManagerAddress;
+            patchState.username = deploymentState.networkManagerUsername;
+            patchState.password = deploymentState.networkManagerPassword;
+            patchState.transportZoneId = deploymentState.networkZoneId;
+            progress(state, patchState);
+          } catch (Throwable t) {
+            fail(state, t);
+          }
+        })
+        .sendWith(this);
   }
 
   /**
