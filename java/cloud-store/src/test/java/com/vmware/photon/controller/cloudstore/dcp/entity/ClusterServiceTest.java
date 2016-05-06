917d537fc91f1f098549177efa00b37256db5d33
Pankaj Lakhina <plakhina@vmware.com>
2016-05-06 10:56:32 -0700

    Add ServiceOption.ON_DEMAND_LOAD to cloud store services.
    This will help avoid unnecessary restarts of services when a xenon host restarts.
    
    Change-Id: Ia25774a1fd050171317a7bd1540708acdd8595c8

--- java/cloud-store/src/test/java/com/vmware/photon/controller/cloudstore/dcp/entity/ClusterServiceTest.java
+++ java/cloud-store/src/test/java/com/vmware/photon/controller/cloudstore/dcp/entity/ClusterServiceTest.java
@@ -88,6 +88,7 @@ public class ClusterServiceTest {
           Service.ServiceOption.CONCURRENT_GET_HANDLING,
           Service.ServiceOption.PERSISTENCE,
           Service.ServiceOption.REPLICATION,
+          Service.ServiceOption.ON_DEMAND_LOAD,
           Service.ServiceOption.OWNER_SELECTION);
       assertThat(service.getOptions(), is(expected));
     }
