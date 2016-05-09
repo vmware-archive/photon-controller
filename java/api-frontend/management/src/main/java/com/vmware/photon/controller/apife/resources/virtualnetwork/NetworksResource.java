c463c3119a5e5e9e2e4c13e2a2029a7da43b37f4
Yu Sheng <ysheng@vmware.com>
2016-05-09 11:28:05 -0700

    apife: add virtual network related classes
    Change-Id: If7fe1438b2b9dd5cc1b91134ec539f878fb6d60f

---
+++ java/api-frontend/management/src/main/java/com/vmware/photon/controller/apife/resources/virtualnetwork/NetworksResource.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2016 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.photon.controller.apife.resources.virtualnetwork;
+
+import com.vmware.photon.controller.api.Task;
+import com.vmware.photon.controller.api.VirtualNetworkCreateSpec;
+import com.vmware.photon.controller.api.common.exceptions.external.ExternalException;
+import com.vmware.photon.controller.apife.clients.VirtualNetworkFeClient;
+import com.vmware.photon.controller.apife.resources.routes.NetworkResourceRoutes;
+import com.vmware.photon.controller.apife.resources.routes.TaskResourceRoutes;
+import static com.vmware.photon.controller.api.common.Responses.generateCustomResponse;
+
+import com.google.inject.Inject;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
+import io.dropwizard.validation.Validated;
+import org.glassfish.jersey.server.ContainerRequest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+
+/**
+ * This resource is for network related API.
+ */
+@Path(NetworkResourceRoutes.API)
+@Api(value = NetworkResourceRoutes.API)
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class NetworksResource {
+
+  private final VirtualNetworkFeClient virtualNetworkFeClient;
+
+  @Inject
+  public NetworksResource(VirtualNetworkFeClient virtualNetworkFeClient) {
+    this.virtualNetworkFeClient = virtualNetworkFeClient;
+  }
+
+  @POST
+  @ApiOperation(value = "Create a network", response = Task.class)
+  @ApiResponses(value = {
+      @ApiResponse(code = 201, message = "Network is getting created, progress communicated via the task")
+  })
+  public Response create(@Context Request request,
+                         @Validated VirtualNetworkCreateSpec spec)
+    throws ExternalException {
+    return generateCustomResponse(
+        Response.Status.CREATED,
+        virtualNetworkFeClient.create(spec),
+        (ContainerRequest) request,
+        TaskResourceRoutes.TASK_PATH);
+  }
+}
