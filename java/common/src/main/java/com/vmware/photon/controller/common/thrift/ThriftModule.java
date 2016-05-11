4252dc6bb3b2d05c9dd4804481e5e4e5ed9d6340
York Wei <wyork@vmware.com>
2016-05-11 15:07:33 -0700

    scheduler: remove guice injection of thrift
    This removes Guice injection for thrift for scheduler.
    Instead scheduler gets a HostClientFactory from the
    ThriftModule.
    Added comments in ZookeeperModule for functions used
    by scheduler.
    Remove unused test helper files.
    
    Change-Id: I1f9b10688f08fecbed66963b943c7dad634a8a1b

--- java/common/src/main/java/com/vmware/photon/controller/common/thrift/ThriftModule.java
+++ java/common/src/main/java/com/vmware/photon/controller/common/thrift/ThriftModule.java
@@ -13,9 +13,15 @@
 
 package com.vmware.photon.controller.common.thrift;
 
+import com.vmware.photon.controller.common.clients.HostClient;
+import com.vmware.photon.controller.common.clients.HostClientFactory;
+import com.vmware.photon.controller.common.zookeeper.ServiceNode;
+import com.vmware.photon.controller.common.zookeeper.ServiceNodeEventHandler;
+
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
+import com.google.inject.TypeLiteral;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 import org.apache.thrift.async.TAsyncClientManager;
 import org.apache.thrift.protocol.TCompactProtocol;
@@ -27,6 +33,7 @@ import javax.inject.Named;
 
 import java.io.IOException;
 import java.security.SecureRandom;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -79,5 +86,103 @@ public class ThriftModule extends AbstractModule {
   @Named("ClientProxyExecutor")
   ExecutorService getClientProxyExecutor() {
     return Executors.newCachedThreadPool();
+  }
+
+  /**
+   * Creates a TAsyncClientFactory of the given type.
+   * @param type the type of TAsyncClientFactory to create.
+   * @return
+   * @throws IOException
+   */
+  public TAsyncClientFactory getTAsyncClientFactory(TypeLiteral type) throws IOException {
+    final TAsyncClientManager clientManager = getTAsyncClientManager();
+    TAsyncClientFactory tAsyncClientFactory = new TAsyncClientFactory(type, clientManager);
+    return tAsyncClientFactory;
+  }
+
+  /**
+   * Creates a ClientPoolFactory that builds a new ClientPool based on given servers and options.
+   * @return
+   */
+  public ClientPoolFactory getClientPoolFactory(TAsyncClientFactory tAsyncClientFactory) {
+    ClientPoolFactory clientPoolFactory = new ClientPoolFactory() {
+      final SecureRandom random = getSecureRandom();
+      final TProtocolFactory protocolFactory = getTProtocolFactory();
+      final ScheduledExecutorService scheduledExecutorService = getClientPoolTimer();
+      final ThriftFactory thriftFactory = getThriftFactory();
+
+      @Override
+      public ClientPool create(ServerSet serverSet, ClientPoolOptions options) {
+        ClientPool clientPool = new ClientPoolImpl<>(random, tAsyncClientFactory, protocolFactory, thriftFactory,
+            scheduledExecutorService, serverSet, options);
+        return clientPool;
+      }
+
+      @Override
+      public ClientPool create(Set servers, ClientPoolOptions options) {
+        ClientPool clientPool = new BasicClientPool<>(random, tAsyncClientFactory, protocolFactory, thriftFactory,
+            scheduledExecutorService, servers, options);
+        return clientPool;
+      }
+    };
+    return clientPoolFactory;
+  }
+
+  /**
+   * Creates a ClientProxyFactory of the given type.
+   * @param type the type of ClientProxy to create.
+   * @return
+   */
+  public ClientProxyFactory getClientProxyFactory(TypeLiteral type) {
+    ClientProxyFactory clientProxyFactory = new ClientProxyFactory() {
+      final ExecutorService clientProxyExecutor = getClientProxyExecutor();
+
+      @Override
+      public ClientProxy create(ClientPool clientPool) {
+        ClientProxy clientProxy = new ClientProxyImpl<>(clientProxyExecutor, type, clientPool);
+        return clientProxy;
+      }
+    };
+    return clientProxyFactory;
+  }
+
+  /**
+   * Creates a ThriftFactory.
+   * @return
+   */
+  public ThriftFactory getThriftFactory() {
+    ThriftFactory thriftFactory = new ThriftFactory() {
+      final TProtocolFactory protocolFactory = getTProtocolFactory();
+      @Override
+      public ThriftEventHandler create(ServiceNodeEventHandler serviceNodeEventHandler, ServiceNode serviceNode) {
+        return new ThriftEventHandler(serviceNodeEventHandler, serviceNode);
+      }
+
+      @Override
+      public MultiplexedProtocolFactory create(String serviceName) {
+        return new MultiplexedProtocolFactory(protocolFactory, serviceName);
+      }
+    };
+    return thriftFactory;
+  }
+
+  /**
+   * Creates a HostClientFactory of the given type.
+   * @param type the ClientProxy type to create.
+   * @return
+   * @throws IOException
+   */
+  public HostClientFactory getHostClientFactory(TypeLiteral type) throws IOException {
+    TAsyncClientFactory tAsyncClientFactory = getTAsyncClientFactory(type);
+    ClientPoolFactory clientPoolFactory = getClientPoolFactory(tAsyncClientFactory);
+    ClientProxyFactory clientProxyFactory = getClientProxyFactory(type);
+
+    HostClientFactory hostClientFactory = new HostClientFactory() {
+      @Override
+      public HostClient create() {
+        return new HostClient(clientProxyFactory, clientPoolFactory);
+      }
+    };
+    return hostClientFactory;
   }
 }
