137132de878ff09148f02c8243abe3d219beeb3c
Ashok Chandrasekar 79715 <ashokc@vmware.com>
2016-05-12 16:44:06 -0700

    cloud-store: add logging to understand when factory services stop
    Change-Id: Ic347665a3cfa5e3655e38ed567779a61afcc903f

--- java/cloud-store/src/main/java/com/vmware/photon/controller/cloudstore/dcp/entity/PortGroupServiceFactory.java
+++ java/cloud-store/src/main/java/com/vmware/photon/controller/cloudstore/dcp/entity/PortGroupServiceFactory.java
@@ -14,7 +14,9 @@
 package com.vmware.photon.controller.cloudstore.dcp.entity;
 
 import com.vmware.photon.controller.common.xenon.ServiceUriPaths;
+import com.vmware.photon.controller.common.xenon.ServiceUtils;
 import com.vmware.xenon.common.FactoryService;
+import com.vmware.xenon.common.Operation;
 import com.vmware.xenon.common.Service;
 
 /**
@@ -33,5 +35,17 @@ public class PortGroupServiceFactory extends FactoryService {
   @Override
   public Service createServiceInstance() throws Throwable {
     return new PortGroupService();
+  }
+
+  @Override
+  public void handleStop(Operation stop) {
+    ServiceUtils.logWarning(this, "Stopping factory service %s", getSelfLink());
+    super.handleStop(stop);
+  }
+
+  @Override
+  public void handleDelete(Operation delete) {
+    ServiceUtils.logWarning(this, "Deleting factory service %s", getSelfLink());
+    super.handleDelete(delete);
   }
 }
