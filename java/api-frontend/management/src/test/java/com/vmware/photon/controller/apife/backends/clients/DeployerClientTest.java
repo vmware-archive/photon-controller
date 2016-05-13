c4bab54507b1b2257ea1673278151d6a0dc74aea
Gokcen Iskender <giskender@vmware.com>
2016-05-13 13:44:16 -0700

    -Add DeployerXenonRestClient -Change build to add dependency to deployer from api-fe -Convert Housekeeper and Deployer Status to Xenon based rather than Thrift based
    Change-Id: Ib17389c258d4b7430c61ef5311e6a2eb78cfaf5c

---
+++ java/api-frontend/management/src/test/java/com/vmware/photon/controller/apife/backends/clients/DeployerClientTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2015 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.photon.controller.apife.backends.clients;
+
+import com.vmware.photon.controller.api.UsageTag;
+import com.vmware.photon.controller.apife.entities.HostEntity;
+import com.vmware.photon.controller.apife.exceptions.external.SpecInvalidException;
+import com.vmware.photon.controller.apife.lib.UsageTagHelper;
+import com.vmware.photon.controller.cloudstore.dcp.entity.ClusterService;
+import com.vmware.photon.controller.clustermanager.servicedocuments.KubernetesClusterCreateTask;
+import com.vmware.photon.controller.deployer.dcp.task.ValidateHostTaskService;
+import com.vmware.xenon.common.Operation;
+import com.vmware.xenon.common.Service;
+
+import org.mockito.Mock;
+import org.powermock.modules.testng.PowerMockTestCase;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+/**
+ * Tests {@link DeployerClient}.
+ */
+public class DeployerClientTest {
+
+  @Test
+  private void dummy() {
+  }
+
+  /**
+   * Tests for the createKubernetesCluster method.
+   */
+  public static class CreateHostTest extends PowerMockTestCase {
+    @Mock
+    private DeployerXenonRestClient deployerXenonRestClient;
+    @Mock
+    private ApiFeXenonRestClient apiFeXenonRestClient;
+    private DeployerClient deployerClient;
+
+    @BeforeMethod
+    public void setUp() throws Throwable {
+      Operation operation = new Operation();
+      ValidateHostTaskService task = new ValidateHostTaskService();
+      operation.setBody(task);
+
+      when(deployerXenonRestClient.post(any(String.class), any(KubernetesClusterCreateTask.class)))
+          .thenReturn(operation);
+
+      when(apiFeXenonRestClient.post(any(String.class), any(ClusterService.State.class)))
+          .thenReturn(null);
+
+      deployerClient = new DeployerClient(deployerXenonRestClient, apiFeXenonRestClient);
+    }
+
+    private static HostEntity buildCreateSpec() {
+      HostEntity host = new HostEntity();
+      host.setAddress("1.1.1.1");
+      host.setUsername("user1");
+      host.setPassword("pwd");
+      host.setUsageTags(UsageTagHelper.serialize(new ArrayList<UsageTag>() {{
+        add(UsageTag.MGMT);
+      }}));
+      return host;
+    }
+
+    @Test
+    public void testCreateKubernetesCluster() throws SpecInvalidException {
+      HostEntity host = buildCreateSpec();
+      ValidateHostTaskService createTask = deployerClient.createHost(host);
+
+      assertEquals(createTask.getProcessingStage(), Service.ProcessingStage.CREATED);
+    }
+  }
+}
