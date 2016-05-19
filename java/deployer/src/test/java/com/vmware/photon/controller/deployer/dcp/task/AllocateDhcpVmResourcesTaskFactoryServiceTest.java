97e303ee69612684166d1e3eeb2013c41ca19f99
Pankaj Lakhina <plakhina@vmware.com>
2016-05-19 16:27:11 -0700

    Initial commit for AllocateDhcpVmResourcesTaskService
    I copied this from AllocateClusterManagerResoucesTaskService and made the following changes:
    Renamed the classes as needed to the service, its factories and the test classes. I changed the steps
    to include an image upload step. I also added the factory to the list of factories that should be started
    in deployer host.
    
    Change-Id: I51de50cef573f6c5c7ff65016ae6c9d48a15df33

---
+++ java/deployer/src/test/java/com/vmware/photon/controller/deployer/dcp/task/AllocateDhcpVmResourcesTaskFactoryServiceTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2015 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.photon.controller.deployer.dcp.task;
+
+import com.vmware.xenon.common.Service;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+
+import java.util.EnumSet;
+
+/**
+ * This class implements tests for the {@link AllocateDhcpVmResourcesTaskFactoryService} class.
+ */
+public class AllocateDhcpVmResourcesTaskFactoryServiceTest {
+
+  private AllocateDhcpVmResourcesTaskFactoryService allocateDhcpVmResourcesTaskFactoryService;
+
+  @BeforeClass
+  public void setUpClass() {
+    allocateDhcpVmResourcesTaskFactoryService = new AllocateDhcpVmResourcesTaskFactoryService();
+  }
+
+  @Test
+  public void testCapabilityInitialization() {
+
+    EnumSet<Service.ServiceOption> expected = EnumSet.of(
+        Service.ServiceOption.CONCURRENT_GET_HANDLING,
+        Service.ServiceOption.CONCURRENT_UPDATE_HANDLING,
+        Service.ServiceOption.FACTORY,
+        Service.ServiceOption.REPLICATION);
+
+    assertThat(allocateDhcpVmResourcesTaskFactoryService.getOptions(), is(expected));
+  }
+
+  @Test
+  public void testCreateServiceInstance() throws Throwable {
+    Service service = allocateDhcpVmResourcesTaskFactoryService.createServiceInstance();
+    assertThat(service, instanceOf(AllocateDhcpVmResourcesTaskService.class));
+  }
+}
