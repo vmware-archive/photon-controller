3b5dc5daee5312c4a8d4c578226ae7e5616022f5
FatimaIjaz <fijaz@vmware.com>
2016-05-20 12:27:46 -0700

    Network details is added in create VM response from agent: 1) Added get networks call in create VM functionality in agent. 2) Modified thrift interface to return network details in create VM call response. 3) Modified unit tests to validate the changes.
    Change-Id: If72aab42562c3bdd7738a4438f176b007e0608c2

--- python/src/host/host/tests/unit/test_host_handler.py
+++ python/src/host/host/tests/unit/test_host_handler.py
@@ -583,6 +583,7 @@ class HostHandlerTestCase(unittest.TestCase):
         state = common.services.get(ServiceName.MODE)
         state.set_mode(MODE.ENTERING_MAINTENANCE)
         request = MagicMock()
+        handler.hypervisor.vm_manager.get_vm_network = MagicMock()
         response = handler.create_vm(request)
         assert_that(response.result, equal_to(CreateVmResultCode.OPERATION_NOT_ALLOWED))
 
@@ -615,6 +616,7 @@ class HostHandlerTestCase(unittest.TestCase):
         vm_manager.get_location_id.assert_called_once_with(vm.id)
         assert_that(vm.location_id, equal_to(vm_location_id))
         vm.to_thrift.assert_called_once()
+        handler.hypervisor.vm_manager.get_vm_network.assert_called_once_with(request)
 
         # Test lazy image copy
         assert_that(im.copy_image.called, is_(False))
@@ -668,6 +670,7 @@ class HostHandlerTestCase(unittest.TestCase):
         handler._datastores_for_image = MagicMock()
         handler.hypervisor.datastore_manager.datastore_type.return_value = DatastoreType.EXT3
         handler.hypervisor.datastore_manager.image_datastores = MagicMock(return_value=set("ds2"))
+        handler.hypervisor.vm_manager.get_vm_network = MagicMock()
         im = handler.hypervisor.image_manager
         im.get_image_refcount_filename.return_value = os.path.join(self.agent_conf_dir, vm.id)
         im.get_image_id_from_disks.return_value = image_id
