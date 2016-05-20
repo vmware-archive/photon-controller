1374f671a77398d97eb4faf16638bc0c311b3de8
Gokcen Iskender <giskender@vmware.com>
2016-05-19 22:00:15 -0700

    Convert host deprovision and provision from thrift to xenon
    Change-Id: Idb31c6a7bdf15c58121f1a2b757c2f5914f59483

--- java/api/src/main/java/com/vmware/photon/controller/api/Host.java
+++ java/api/src/main/java/com/vmware/photon/controller/api/Host.java
@@ -21,6 +21,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.wordnik.swagger.annotations.ApiModel;
 import com.wordnik.swagger.annotations.ApiModelProperty;
+import org.slf4j.LoggerFactory;
 
 import javax.persistence.ElementCollection;
 import javax.validation.constraints.NotNull;
@@ -66,7 +67,7 @@ public class Host extends Base {
 
   @ElementCollection
   @JsonProperty
-  @ApiModelProperty(value = "Custom metadata that needs to be stored but not represented by individually " +
+  @ApiModelProperty(value = "Custom metadata that needs to be stored but not represented by individually " +V
       "defined properties")
   private Map<String, String> metadata = new HashMap<String, String>();
 
@@ -178,6 +179,10 @@ public class Host extends Base {
   }
 
   public void setDatastores(List<HostDatastore> datastores) {
+    LoggerFactory.getLogger(Host.class).debug("setDatastores {}", datastores);
+    for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {
+      LoggerFactory.getLogger(Host.class).debug(ste.getFileName() + ste.getLineNumber());
+    }
     this.datastores = datastores;
   }
 
