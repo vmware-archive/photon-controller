10ae65200e8d21f05ac5865332dbcd614dead828
Xuli Liu <xulil@vmware.com>
2016-05-24 10:07:48 -0700

    cleanup: Remove transient resources in TaskEntity.
    Both StepEntity and TaskEntity have transient resoures. The one
    in StepEntity is being used. Removing the one in TaskEntity to
    avoid confusion.
    
    Change-Id: Id08e1ddd5245c9e7a3ba7c9808d33eea8bea19b1

--- java/api-frontend/management/src/main/java/com/vmware/photon/controller/apife/entities/TaskEntity.java
+++ java/api-frontend/management/src/main/java/com/vmware/photon/controller/apife/entities/TaskEntity.java
@@ -21,9 +21,7 @@ import com.google.common.base.Objects;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -50,8 +48,6 @@ public class TaskEntity extends BaseEntity {
 
   private String projectId;
   private List<StepEntity> steps = new ArrayList<>();
-
-  private Map<String, Object> transientResources = new HashMap<>();
 
   public List<BaseEntity> getToBeLockedEntities() {
     return this.toBeLockedEntities;
@@ -186,14 +182,6 @@ public class TaskEntity extends BaseEntity {
       }
     }
     return false;
-  }
-
-  public Object getTransientResources(String key) {
-    return transientResources.get(key);
-  }
-
-  public void setTransientResources(String key, Object value) {
-    transientResources.put(key, value);
   }
 
   @Override
