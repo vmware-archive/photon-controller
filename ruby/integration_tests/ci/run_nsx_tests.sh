db218a05508c3f63aff02599a51104fb1c55babe
Xuli Liu <xulil@vmware.com>
2016-05-26 14:29:50 -0700

    Virtual network life cycle test
    Change-Id: I8478449dc23c0379dc5cf503add5f352dbe5dcea

---
+++ ruby/integration_tests/ci/run_nsx_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -ex
+
+source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
+
+# Define any custom config process
+if [ -n "$CUSTOM_TEST_CONFIG" ]; then
+    echo Using custom settings in "$CUSTOM_TEST_CONFIG"
+    # Note: Do NOT put quotes around this. $CUSTOM_TEST_CONFIG has been improperly
+    # used on our build machines, and isn't just a path to a file, but includes
+    # arguments that must be passed to that file. Using quotes will break that.
+    source $CUSTOM_TEST_CONFIG
+else
+    echo No CUSTOM_TEST_CONFIG to override default test behavior
+fi
+
+env
+
+cd "$TESTS"
+
+# Run the virtual network tests only if it is enabled
+if [ -n "$DISABLE_PHYSICAL_NETWORK" ]; then
+  bundle exec rake nsx:virtual_network
+fi
