2249782a0d8ec58846dc96fd6771f5d73fd0b39b
Xiao Zhou <xiaoz@vmware.com>
2016-05-31 15:25:05 -0700

    delete auth_tool from ruby
    Change-Id: I785d8f51f37708ad8617c0b83e2ae71a68edef8d

--- ruby/common/lib/common/auth_helper.rb
+++ ruby/common/lib/common/auth_helper.rb
@@ -19,30 +19,17 @@ module EsxCloud
     # @param [String] url of the authentication service
     # @param [String] absolute path to the auth-token tool.
     # @return [String] access_token
-    def self.get_access_token(username, password, service_locator_url, auth_tool_path)
-      if auth_tool_path.nil? || !File.exists?(auth_tool_path)
-        raise EsxCloud::Error, "Could not find Auth-Token tool under #{auth_tool_path}"
-      end
+    def self.get_access_token(username, password, service_locator_url, auth_tool_path = nil)
+      token_path = "/openidconnect/token"
+      password_grant_format_string =
+          "grant_type=password&username=#{username}&password=#{password}&scope=openid offline_access"
 
-      tenant = extract_tenant username
-      command = "java -jar #{auth_tool_path} get-access-token -t #{tenant} -a #{service_locator_url} -u " +
-          Shellwords.escape(username) + " -p "  + Shellwords.escape(password)
-
-      EsxCloud::CmdRunner.run(command, true, /.*Exception:.*/)
+      http_client = HttpClient.new(service_locator_url)
+      access_token = http_client.post(token_path, password_grant_format_string,
+                       {"Content-Type" => "application/x-www-form-urlencoded"}).body["access_token"];
+      puts "access_token: #{access_token}"
+      access_token
     end
 
-    private
-
-    def self.extract_tenant(username)
-      raise EsxCloud::Error, "User name not provided" if username.nil?
-
-      tokens = username.split("\\")
-      return tokens[0] if tokens.size == 2
-
-      tokens = username.split("@")
-      return tokens[1] if tokens.size == 2
-
-      raise EsxCloud::Error, "User name should be provided as: 'tenant\\user' or 'user@tenant'"
-    end
   end
 end
