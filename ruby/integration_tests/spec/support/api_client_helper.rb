d62ae0819a4f49a6d55237e59579865bb9cb5ddf
Gokcen Iskender <giskender@vmware.com>
2016-06-08 10:48:32 -0700

    Devbox: Add lightwave container
    Change-Id: I8b3fa70367b2ebcc77b2509e9fc2c9c6ad0f4024

--- ruby/integration_tests/spec/support/api_client_helper.rb
+++ ruby/integration_tests/spec/support/api_client_helper.rb
@@ -26,10 +26,10 @@ class ApiClientHelper
 
       if ENV["ENABLE_AUTH"] && ENV["ENABLE_AUTH"] == "true"
          protocol ||= "https"
-         port ||= "443"
+         port ||= (ENV["API_FE_PORT"] || "443").strip
       else
          protocol ||= "http"
-         port ||= "28080"
+         port ||= (ENV["API_FE_PORT"] || "28080").strip
       end
 
       setup_client(protocol, address, port)
