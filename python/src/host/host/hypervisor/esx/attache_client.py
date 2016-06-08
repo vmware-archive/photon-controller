8e9eb0c73feb5540ed4f1a467cb0effac662750d
Long Zhou <longz@vmware.com>
2016-06-08 16:31:21 -0700

    Agent: Revert attache poll interval change
    Change-Id: I988df3378d10e6e5ac3cffa144078ce6f18c67ae

--- python/src/host/host/hypervisor/esx/attache_client.py
+++ python/src/host/host/hypervisor/esx/attache_client.py
@@ -394,7 +394,7 @@ class AttacheVmConfigSpec(VmConfigSpec):
 class SyncAttacheCacheThread(threading.Thread):
     """ Periodically sync vm cache with remote esx server
     """
-    def __init__(self, attache_client, min_interval=1):
+    def __init__(self, attache_client, min_interval=20):
         super(SyncAttacheCacheThread, self).__init__()
         self._logger = logging.getLogger(__name__)
         self.setDaemon(True)
