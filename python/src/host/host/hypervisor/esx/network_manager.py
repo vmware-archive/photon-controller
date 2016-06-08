317acc2ed54befc6c29ad967f28b5379e913555f
Long Zhou <longz@vmware.com>
2016-06-08 16:31:21 -0700

    Agent: Revert attache poll interval change
    Change-Id: I988df3378d10e6e5ac3cffa144078ce6f18c67ae

--- python/src/host/host/hypervisor/esx/network_manager.py
+++ python/src/host/host/hypervisor/esx/network_manager.py
@@ -25,8 +25,9 @@ class EsxNetworkManager(NetworkManager):
         self.logger = logging.getLogger(__name__)
         self._configured_networks = configured_networks
 
-    def _validate_networks(self, configured_networks):
-        """ Validates the list of configured networks against the actual
+    def get_vm_networks(self):
+        """ Return the list of networks to use on this ESX server.
+            Validates the list of configured networks against the actual
             list of VM networks available on the host and returns the
             intersection unless the configured networks is an empty list.
             If the configured networks is empty, it simply returns the
@@ -34,19 +35,15 @@ class EsxNetworkManager(NetworkManager):
         """
         networks = []
         actual_networks = self.vim_client.get_networks()
-        if configured_networks:
-            for network in configured_networks:
+        if self._configured_networks:
+            for network in self._configured_networks:
                 if network not in actual_networks:
                     self.logger.warning("Unknown network %s: Skipping" % network)
                     continue
-                networks.append(network)
+                networks.append(str(network))
         else:
             networks = actual_networks
         return networks
-
-    def get_vm_networks(self):
-        """ Return the list of networks to use on this ESX server. """
-        return self._validate_networks(self._configured_networks)
 
     def get_networks(self):
         """ This method will call vim_client to get a list of VM networks and
