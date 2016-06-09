e46b2337e49c093e1a241056fc018975505c12c8
Pankaj Lakhina <plakhina@vmware.com>
2016-06-09 16:46:42 -0700

    Wildcard the version part of the dhcp agent tar name while building the dhcp server image
    The name of the dhcp agent tar that the build generates changed recently.
    This broke the image building script since it was latched to the exact name of the tar.
    By using a wildcard to locate the tar path it becomes a bit more resilient to such changes.
    And of course this fixes the current promote break.
    
    Change-Id: Ie086e0a548ecf7591fc19918ddc3501fd417a999

--- appliances/dhcp-ova/build.sh
+++ appliances/dhcp-ova/build.sh
@@ -21,6 +21,8 @@ cd ../../java/dhcp-agent
 ../gradlew disttar
 cd ../../appliances/dhcp-ova
 
+DHCP_AGENT_TAR_PATH=(../../java/dhcp-agent/build/distributions/dhcp-agent-*.tar)
+
 SSHD_ENABLE_ROOT_LOGIN=${SSHD_ENABLE_ROOT_LOGIN:-"false"}
 PHOTON_OVA_URL=${PHOTON_OVA_URL:="../photon-ova/build/photon-ova-virtualbox.ova"}
 DNSMASQ_URL=${DNSMASQ_URL:="http://artifactory.ec.eng.vmware.com/artifactory/esxcloud-archives/external/dnsmasq-v2-75/dnsmasq"}
@@ -31,6 +33,7 @@ packer build -force \
 	-var "dnsmasq_url=$DNSMASQ_URL" \
 	-var "dnsmasq_dhcp_release_url=$DNSMASQ_DHCP_RELEASE_URL" \
 	-var "sshd_enable_root_login=$SSHD_ENABLE_ROOT_LOGIN" \
+	-var "dhcpAgentTarPath=$DHCP_AGENT_TAR_PATH" \
         dhcp-ova.json
 
 cd build
