02c797e116760cdf7f0dba461f1b02b20b1fffb5
Borne Mace <bmace@vmware.com>
2016-06-16 11:46:11 -0700

    Removed no longer relevant deployer Main.java and config files
    - Also updated some old housekeeper port numbers to reflect the new
      correct values since the XenonHost merge.
    
    Change-Id: Ibdbfd40303beb344e54f0cd9ead663cacabe80a4

--- ruby/common/spec/api_client/status_api_spec.rb
+++ ruby/common/spec/api_client/status_api_spec.rb
@@ -25,11 +25,11 @@ describe EsxCloud::ApiClient do
     {
       "components" => [
         {
-          "component" => "HOUSEKEEPER",
+          "component" => "PHOTON_CONTROLLER",
           "status" => "READY",
           "instances" => [
             {
-              "address" => "/172.31.253.66=>16000",
+              "address" => "/172.31.253.66=>19000",
               "status" => "READY"
             }
           ],
@@ -43,19 +43,6 @@ describe EsxCloud::ApiClient do
           "instances" => [
             {
               "address" => "/172.31.253.66=>13000",
-              "status" => "READY"
-            }
-          ],
-          "stats" => {
-            "READY" => "1"
-          }
-        },
-        {
-          "component" => "ROOT_SCHEDULER",
-          "status" => "READY",
-          "instances" => [
-            {
-              "address" => "/172.31.253.66=>13010",
               "status" => "READY"
             }
           ],
