c6e0cca8cec668313ae5a8e4d6231f320332b826
Vikash Rai <vrai@vmware.com>
2016-06-27 15:01:37 -0700

    PhotonControllerConfig: Adding test for photon controller config.
    Change-Id: Ia72e2e8957fc64163864e57faf97fc41d12e19e4

---
+++ java/photon-controller-core/src/test/java/com/vmware/photon/controller/core/ConfigTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2015 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.photon.controller.core;
+
+
+import com.vmware.photon.controller.common.config.BadConfigException;
+import com.vmware.photon.controller.common.config.ConfigBuilder;
+import com.vmware.photon.controller.common.zookeeper.ZookeeperConfig;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Tests {@link PhotonControllerConfig}.
+ */
+public class ConfigTest {
+
+  /**
+   * This class implements tests for the default test configuration file.
+   */
+  public class DefaultPhotonControllerCoreConfigTest {
+
+    private PhotonControllerConfig photonControllereConfig;
+
+    @BeforeClass
+    public void setUp() throws BadConfigException {
+      photonControllereConfig = ConfigBuilder.build(PhotonControllerConfig.class,
+          ConfigTest.class.getResource("/config.yml").getPath());
+    }
+
+    @Test
+    public void testXenonConfig() {
+      com.vmware.photon.controller.common.xenon.host.XenonConfig xenonConfig = photonControllereConfig.getXenonConfig();
+      assertThat(xenonConfig.getBindAddress(), is("0.0.0.0"));
+      assertThat(xenonConfig.getPeerNodes(), arrayContaining("http://127.0.0.1:16000"));
+      assertThat(xenonConfig.getPort(), is(16000));
+      assertThat(xenonConfig.getRegistrationAddress(), is("127.0.0.1"));
+      assertThat(xenonConfig.getStoragePath(), is("/tmp/xenon/housekeeper/"));
+    }
+
+    @Test
+    public void testZookeeperConfig() {
+      assertThat(photonControllereConfig.getZookeeper(), instanceOf(ZookeeperConfig.class));
+    }
+  }
+
+  /**
+   * Tests that a minimal photonControllerConfig file can be loaded.
+   */
+  protected class Minimal {
+
+    private PhotonControllerConfig photonControllerConfig;
+
+    @BeforeMethod
+    public void setUp() throws BadConfigException {
+      photonControllerConfig = ConfigBuilder.build(PhotonControllerConfig.class,
+          ConfigTest.class.getResource("/config_min.yml").getPath());
+    }
+
+    @Test
+    public void testXenonConfig() {
+      com.vmware.photon.controller.common.xenon.host.XenonConfig xenonConfig = photonControllerConfig.getXenonConfig();
+      assertThat(xenonConfig.getBindAddress(), is("0.0.0.0"));
+      assertThat(xenonConfig.getPeerNodes(), arrayContaining("http://127.0.0.1:16000"));
+      assertThat(xenonConfig.getPort(), is(16000));
+      assertThat(xenonConfig.getRegistrationAddress(), is("127.0.0.1"));
+      assertThat(xenonConfig.getStoragePath(), is("/tmp/xenon/16000"));
+    }
+  }
+}
