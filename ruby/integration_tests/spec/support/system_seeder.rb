76cf15b0fd58d847ea1349123b75958ce48089bc
Xuli Liu <xulil@vmware.com>
2016-06-28 14:46:35 -0700

    For test purpose only
    Change-Id: Ie196ada9c37aaf5f991d4a758acea8c0f4bbf6aa

--- ruby/integration_tests/spec/support/system_seeder.rb
+++ ruby/integration_tests/spec/support/system_seeder.rb
@@ -127,6 +127,12 @@ module EsxCloud
     end
 
     def network!
+      if @network.nil?
+        puts "@network is nil, and we need to create one"
+      end
+      if network.nil?
+        puts "network is nil, and we need to create one really"
+      end
       network || create_network
     end
 
@@ -299,8 +305,13 @@ module EsxCloud
     end
 
     def create_network
-      spec = EsxCloud::NetworkCreateSpec.new(random_name("network-"), "Seeder Network", [get_vm_port_group])
-      network = EsxCloud::Config.client.create_network(spec.to_hash)
+      unless ENV["PHYSICAL_NETWORK_DISABLED"] then
+        spec = EsxCloud::NetworkCreateSpec.new(random_name("network-"), "Seeder Network", [get_vm_port_group])
+        network = EsxCloud::Config.client.create_network(spec.to_hash)
+      else
+        spec = EsxCloud::VirtualNetworkCreateSpec.new(random_name("network-"), "Seeder Virtual Network", "ROUTED")
+        network = EsxCloud::VirtualNetwork.create(project!.id, spec)
+      end
       EsxCloud::Config.client.set_default(network.id)
       network
     end
