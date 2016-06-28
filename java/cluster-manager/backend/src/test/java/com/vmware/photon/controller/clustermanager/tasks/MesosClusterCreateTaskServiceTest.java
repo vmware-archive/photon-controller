65e0ff0d082e9f5e79b2d6dd2a87611dc67bf1fe
Alain Roy <alainr@vmware.com>
2016-06-28 13:46:46 -0700

    Improve cluster manager wait for VM IP address
    The cluster manager waits for VMs to get a public IP address. We were
    using the presence of a network name (portgroup name) as a sign that
    it was a public address, but in some cases the underlying system
    doesn't report the portgroup.
    
    This switches from looking at the portgroup and instead uses the MAC
    address OUI--the first three octets, which are vendor-specific.
    External NICs will use the VMware OUI while all other NICs will not.
    
    This pattern was validated in our kube-up implementation for Kubernetes.
    
    Change-Id: I3c9d9dc11818d5a6ca13510bf08273f663eeafb6

--- java/cluster-manager/backend/src/test/java/com/vmware/photon/controller/clustermanager/tasks/MesosClusterCreateTaskServiceTest.java
+++ java/cluster-manager/backend/src/test/java/com/vmware/photon/controller/clustermanager/tasks/MesosClusterCreateTaskServiceTest.java
@@ -799,6 +799,7 @@ public class MesosClusterCreateTaskServiceTest {
       NetworkConnection networkConnection = new NetworkConnection();
       networkConnection.setNetwork("VM VLAN");
       networkConnection.setIpAddress("127.0.0.1");
+      networkConnection.setMacAddress("00:0c:29:9e:6b:00");
 
       VmNetworks vmNetworks = new VmNetworks();
       vmNetworks.setNetworkConnections(Collections.singleton(networkConnection));
