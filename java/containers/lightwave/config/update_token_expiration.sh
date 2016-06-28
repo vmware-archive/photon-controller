a6505fe61f03574ceb3618cf72f0a2664722b596
York Wei <wyork@vmware.com>
2016-06-28 10:50:38 -0700

    devbox: configure and start Lightwave in devbox
    This change will configure and start a Lightwave server
    in the devbox environment. This will allow developers
    to test with authentication/authorization enabled locally.
    New environment variables are introduced to configure
    the Lightwave server:
    - LW_DOMAIN_NAME, the domain for lightwave e.g. "photon.vmware.com"
    - LW_HOSTNAME, the hostname for lightwave e.g. "lightwave"
    - LW_PASSWORD, the password for the lightwave tenant (administrator)
    
    Note: The hostname set in lightwave-server.cfg is set to the Lightwave
    public IP because Photon Controller is expecting the hostname to be
    the lightwave IP and matches the certificate provided.
    The value of the hostname will be changed after Photon Controller
    configures DNS and joins the Lightwave domain.
    
    Change-Id: I5833ac5ee19bca3ebbfa75e456d0f07ae5524e4f

---
+++ java/containers/lightwave/config/update_token_expiration.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+admin_password=$1
+domain_name=$2
+hostname=$3
+expiration_limit=3000000
+
+# Update tenant token expiration time to expiration limit
+
+token=$(curl -X POST --insecure -sS \
+ -d "username=Administrator@${domain_name}&password=${admin_password}&&grant_type=password&scope=openid offline_access id_groups at_groups rs_admin_server" \
+https://${hostname}.${domain_name}/openidconnect/token | jq -r .access_token)
+
+output_tmp=result.tmp
+
+curl -X GET --insecure -sS -o ${output_tmp} \
+ -H "Authorization: Bearer ${token}" \
+ -H "Content-Type: application/json" \
+ https://${hostname}.${domain_name}/idm/tenant/${domain_name}/config
+
+# Replace expiration limit in the tenant configuration json
+rb="\
+require 'json';\
+_hash=JSON.parse(File.open(\"${output_tmp}\", \"rb\").read);\
+_hash[\"tokenPolicy\"][\"maxBearerTokenLifeTimeMillis\"]=${expiration_limit};\
+puts _hash.to_json"
+
+updated_config=$(ruby -e "${rb}" | jq -r .tokenPolicy)
+
+config=$(curl -X PUT --insecure -sS -H "Authorization: Bearer ${token}" \
+ -H "Content-Type: application/json" \
+ -d "${updated_config}" \
+ https://${hostname}.${domain_name}/idm/tenant/${domain_name}/config)
+
+rm -rf ${output_tmp}
+
+echo "Updated tenant token expiration time updated to ${expiration_limit}"
