bcbb7320d8349860aab2d31e5c40dcc626a6d7ba
Pankaj Lakhina <plakhina@vmware.com>
2016-07-01 14:48:51 -0700

    Add IpAllocatorService xenon service
    This is an initial commit.
    More functionality will be added in future commits.
    
    Change-Id: Ie76e9ad242f1d2541f3c6f46f5d42882e38cf573

---
+++ java/cloud-store/src/main/java/com/vmware/photon/controller/cloudstore/xenon/entity/IpAllocatorService.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2016 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.photon.controller.cloudstore.xenon.entity;
+
+import com.vmware.photon.controller.common.Constants;
+import com.vmware.photon.controller.common.xenon.InitializationUtils;
+import com.vmware.photon.controller.common.xenon.ServiceUriPaths;
+import com.vmware.photon.controller.common.xenon.ServiceUtils;
+import com.vmware.photon.controller.common.xenon.ValidationUtils;
+import com.vmware.photon.controller.common.xenon.deployment.MigrateDuringDeployment;
+import com.vmware.photon.controller.common.xenon.migration.MigrateDuringUpgrade;
+import com.vmware.photon.controller.common.xenon.migration.MigrationUtils;
+import com.vmware.photon.controller.common.xenon.validation.Immutable;
+import com.vmware.xenon.common.FactoryService;
+import com.vmware.xenon.common.Operation;
+import com.vmware.xenon.common.ServiceDocument;
+import com.vmware.xenon.common.StatefulService;
+
+import java.util.List;
+
+
+/**
+ * Used for allocating IPs from a subnet and also to track ownership of a CIDR range by a network.
+ */
+public class IpAllocatorService extends StatefulService {
+
+  public static final String FACTORY_LINK = ServiceUriPaths.CLOUDSTORE_ROOT + "/ip-allocators";
+
+  public static FactoryService createFactory() {
+    return FactoryService.create(IpAllocatorService.class, IpAllocatorService.State.class);
+  }
+
+  public IpAllocatorService() {
+    super(State.class);
+    super.toggleOption(ServiceOption.PERSISTENCE, true);
+    super.toggleOption(ServiceOption.REPLICATION, true);
+    super.toggleOption(ServiceOption.OWNER_SELECTION, true);
+    super.toggleOption(ServiceOption.INSTRUMENTATION, true);
+  }
+
+  @Override
+  public void handleCreate(Operation createOperation) {
+    ServiceUtils.logInfo(this, "Creating service %s", getSelfLink());
+    try {
+      State startState = createOperation.getBody(State.class);
+      InitializationUtils.initialize(startState);
+      ValidationUtils.validateState(startState);
+      createOperation.complete();
+    } catch (IllegalStateException t) {
+      ServiceUtils.failOperationAsBadRequest(this, createOperation, t);
+    } catch (Throwable t) {
+      ServiceUtils.logSevere(this, t);
+      createOperation.fail(t);
+    }
+  }
+
+  @Override
+  public void handleDelete(Operation deleteOperation) {
+    ServiceUtils.logInfo(this, "Deleting service %s", getSelfLink());
+    ServiceUtils.expireDocumentOnDelete(this, State.class, deleteOperation);
+  }
+
+  /**
+   * Persistent virtual network state data.
+   */
+  @MigrateDuringUpgrade(transformationServicePath = MigrationUtils.REFLECTION_TRANSFORMATION_SERVICE_LINK,
+      sourceFactoryServicePath = FACTORY_LINK,
+      destinationFactoryServicePath = FACTORY_LINK,
+      serviceName = Constants.CLOUDSTORE_SERVICE_NAME)
+  @MigrateDuringDeployment(
+      factoryServicePath = FACTORY_LINK,
+      serviceName = Constants.CLOUDSTORE_SERVICE_NAME)
+  public static class State extends ServiceDocument {
+
+    /**
+     * This is the CIDR allocated to the subnet.
+     */
+    @Immutable
+    public String cidr;
+
+    /**
+     * This is the smallest IP of this subnet. It is reserved as Network Address.
+     */
+    @Immutable
+    public long lowIp;
+
+    /**
+     * This is the biggest IP of this subnet. It is reserved as Broadcast Address.
+     */
+    @Immutable
+    public long highIp;
+
+    /**
+     * This is the smallest IP of the range from which IPs will be allocated to VMs/MACs.
+     */
+    @Immutable
+    public long lowIpDynamic;
+
+    /**
+     * This is the biggest IP of the range from which IPs will be allocated to VMs/MACs.
+     */
+    @Immutable
+    public long highIpDynamic;
+
+    /**
+     * This is a list of IPs reserved for infrastructure use e.g. address for DHCP Relay router for the subnet.
+     * We will include the lowIp and highIp in this list. This is calculated only for display purposes for the user.
+     */
+    @Immutable
+    public List<Long> reservedIpList;
+
+    /**
+     * This is the smallest IP of the range from which IPs will be excluded for allocations to VMs/MACs.
+     * This is calculated only for display purposes for the user.
+     */
+    @Immutable
+    public long lowIpStatic;
+
+    /**
+     * This is the biggest IP of the range from which IPs will be excluded for allocations to VMs/MACs.
+     * This is calculated only for display purposes for the user.
+     */
+    @Immutable
+    public long highIpStatic;
+
+
+    @Override
+    public String toString() {
+      return com.google.common.base.Objects.toStringHelper(this)
+          .add("documentSelfLink", documentSelfLink)
+          .add("cidr", cidr)
+          .toString();
+    }
+  }
+}
---
+++ java/cloud-store/src/test/java/com/vmware/photon/controller/cloudstore/xenon/entity/IpAllocatorServiceTest.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2016 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.photon.controller.cloudstore.xenon.entity;
+
+import com.vmware.photon.controller.cloudstore.xenon.CloudStoreServiceGroup;
+import com.vmware.photon.controller.cloudstore.xenon.helpers.TestHelper;
+import com.vmware.photon.controller.common.thrift.StaticServerSet;
+import com.vmware.photon.controller.common.xenon.BasicServiceHost;
+import com.vmware.photon.controller.common.xenon.ServiceHostUtils;
+import com.vmware.photon.controller.common.xenon.ServiceUtils;
+import com.vmware.photon.controller.common.xenon.XenonRestClient;
+import com.vmware.xenon.common.Operation;
+
+import org.apache.http.HttpStatus;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Tests {@link IpAllocatorService}.
+ */
+public class IpAllocatorServiceTest {
+  private static BasicServiceHost host;
+  private static XenonRestClient xenonClient;
+
+  @BeforeSuite
+  public void beforeSuite() throws Throwable {
+    host = BasicServiceHost.create();
+    ServiceHostUtils.startFactoryServices(host, CloudStoreServiceGroup.FACTORY_SERVICES_MAP);
+
+    StaticServerSet serverSet = new StaticServerSet(new InetSocketAddress(host.getPreferredAddress(), host.getPort()));
+    xenonClient = new XenonRestClient(serverSet, Executors.newFixedThreadPool(128));
+    xenonClient.start();
+  }
+
+  @AfterSuite
+  public void afterSuite() throws Throwable {
+    xenonClient.stop();
+    host.destroy();
+  }
+
+  @Test(enabled = false)
+  private void dummy() {
+  }
+
+  /**
+   * Tests for handleStart.
+   */
+  public static class HandleStartTest {
+
+    @AfterMethod
+    public void afterMethod() throws Throwable {
+      ServiceHostUtils.deleteAllDocuments(host, "test-host");
+    }
+
+    @Test
+    public void testSuccessfulCreation() throws Throwable {
+      IpAllocatorService.State startState = createInitialState();
+
+      Operation result = xenonClient.post(IpAllocatorService.FACTORY_LINK, startState);
+      assertThat(result.getStatusCode(), is(HttpStatus.SC_OK));
+
+      IpAllocatorService.State createdState = result.getBody(IpAllocatorService.State.class);
+      assertThat(ServiceUtils.documentEquals(IpAllocatorService.State.class, startState, createdState), is(true));
+
+      IpAllocatorService.State savedState = host.getServiceState(IpAllocatorService.State.class,
+          createdState.documentSelfLink);
+      assertThat(ServiceUtils.documentEquals(IpAllocatorService.State.class, startState, savedState), is(true));
+    }
+  }
+
+  /**
+   * Tests for handleDelete method.
+   */
+  public class HandleDeleteTest {
+    private IpAllocatorService.State startState;
+
+    @BeforeMethod
+    public void beforeMethod() throws Throwable {
+      startState = createInitialState();
+    }
+
+    @AfterMethod
+    public void afterMethod() throws Throwable {
+      ServiceHostUtils.deleteAllDocuments(host, "test-host");
+    }
+
+    @Test
+    public void testUsingDefaultExpiration() throws Throwable {
+      TestHelper.testExpirationOnDelete(
+          xenonClient,
+          host,
+          IpAllocatorService.FACTORY_LINK,
+          startState,
+          IpAllocatorService.State.class,
+          0L,
+          0L,
+          ServiceUtils.computeExpirationTime(ServiceUtils.DEFAULT_ON_DELETE_DOC_EXPIRATION_TIME_MICROS)
+      );
+    }
+
+    @Test
+    public void testUsingExpirationInCurrentState() throws Throwable {
+      TestHelper.testExpirationOnDelete(
+          xenonClient,
+          host,
+          IpAllocatorService.FACTORY_LINK,
+          startState,
+          IpAllocatorService.State.class,
+          ServiceUtils.computeExpirationTime(Integer.MAX_VALUE),
+          0L,
+          ServiceUtils.computeExpirationTime(Integer.MAX_VALUE)
+      );
+    }
+
+    @Test
+    public void testUsingExpirationInDeleteOperation() throws Throwable {
+      TestHelper.testExpirationOnDelete(
+          xenonClient,
+          host,
+          IpAllocatorService.FACTORY_LINK,
+          startState,
+          IpAllocatorService.State.class,
+          ServiceUtils.computeExpirationTime(TimeUnit.MINUTES.toMicros(1)),
+          ServiceUtils.computeExpirationTime(Integer.MAX_VALUE),
+          ServiceUtils.computeExpirationTime(Integer.MAX_VALUE)
+      );
+    }
+  }
+
+  private static IpAllocatorService.State createInitialState() {
+    IpAllocatorService.State startState = new IpAllocatorService.State();
+    startState.cidr = "192.168.0.0/16";
+    return startState;
+  }
+}
