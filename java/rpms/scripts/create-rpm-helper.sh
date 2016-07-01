f008ff9aa0c6c99cf6477a45672142eaadb5784e
Touseef Liaqat <tliaqat@vmware.com>
2016-06-30 18:43:39 -0700

    Add default configuration, service file, run scripts in RPM package
     * Build distTar outside of rpm build container as gradle dependency.
     * Create RPM repo files for yum/tdnf to consume this repo and rpms.
     * Use mustache in RPM spec to create default configurations.
     * Add separate Dockerfile for rpm-builder container.
     * Update configuration json files with any missing defaults.
     * run.sh accepts paths as parameter so they can be called with
       different paths then old default paths which we want to change gradually.
    
    Change-Id: I48ffdabd14062602f8cb1ad11acfce7a4c904c15

---
+++ java/rpms/scripts/create-rpm-helper.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -x -e
+
+VERSION=$1
+chown root:root ./SPECS/*
+chown root:root /usr/src/photon/SOURCES/*
+rpmbuild -ba --define="pkg_version $VERSION" ./SPECS/photon-controller.spec
+createrepo --database /usr/src/photon/RPMS
+tar -czf ../build/photon-controller-rpmrepo-"$VERSION".tar.gz -C /usr/src/photon/ RPMS
