e801cf0562f118af68ab77fe21a56479c5e8a5ce
Adrian Schroeter <aschroeter@vmware.com>
2016-07-06 08:40:18 -0700

    devbox: adjusted Vagrant file to work with PhotonOS v1.0
    - had to change some permissions to make installed binaries accessible
      PhotonOS is not setting global permissions through chmod unless they
      are explicitly set
    - stopped building the lightwave container it is now part of the box
      image
    - ensured that zookeeper is started in standalone mode
    - fixed small issues
    - added proper wait for docker
    - removed fake agent left overs
    - created photon-controller-core container build target that doesn't force
      a servicebase container rebuild
    - not starting lightwave vm if auth is disabled
    
    Change-Id: I03a82c63323117ded5059766410859fa07f8b4d2

--- java/devbox/compileservices/agent_compile_install.sh
+++ java/devbox/compileservices/agent_compile_install.sh
@@ -12,16 +12,3 @@ sed -i 's/$BINARIES\/bin\/vibauthor/vibauthor/' /devbox_data/python/vib/create_v
 # Compile agent
 #
 cd /devbox_data/python && make clean dist && make vib-only
-
-#
-# Install agent
-#
-INSTALL_PATH="/usr/lib/esxcloud/agent"
-RELEASE_PATH="/devbox_data/python/dist"
-
-rm -rf $INSTALL_PATH
-mkdir -p $INSTALL_PATH
-
-virtualenv $INSTALL_PATH
-. $INSTALL_PATH/bin/activate
-pip install -q -f file://${RELEASE_PATH} photon.controller.agent
