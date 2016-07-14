3fddd95385caa43fee7ace49cf91f6ccb24e6e6d
Touseef Liaqat <tliaqat@vmware.com>
2016-07-13 17:21:31 -0700

    Copy vibs in the RPM and move to old path names for now
    Change-Id: I65090f92245c9baa33951088027976f518e658c8

--- java/rpms/scripts/create-rpm.sh
+++ java/rpms/scripts/create-rpm.sh
@@ -5,14 +5,16 @@ set -x -e
 # we cannot have SNAPSHOT at the end as we have with tar file.
 # Resulting RPM will be named as 'photon-controller-core-develop.rpm' for develop branch
 # and 'photon-controller-core-v0.9.1.rpm' for v0.9.1 branch.
-VERSION=${GERRIT_BRANCH:-`git rev-parse --abbrev-ref HEAD`}
-
+BRANCH=${GERRIT_BRANCH:-`git rev-parse --abbrev-ref HEAD`}
+VERSION=${BRANCH}
 ROOT=`git rev-parse --show-toplevel`
 SOURCES_DIR="${ROOT}/java/rpms/SOURCES"
 TEMP_DIR=$(mktemp -d "${ROOT}/create_tar.XXXXX")
 TAR_PATH="/java/photon-controller-core/build/distributions/"
 TAR_PREFIX="photon-controller-core"
 RPM_PREFIX="photon-controller"
+ENVOY_VIB_URL="http://artifactory.ec.eng.vmware.com/artifactory/esxcloud-archives/envoy/${BRANCH}/latest/vmware-envoy-latest.vib"
+
 trap "rm -rf ${TEMP_DIR}; rm -rf ${SOURCES_DIR};" EXIT
 
 # Create source tar file for use by RPM spec file
@@ -26,6 +28,15 @@ mv ${TAR_PREFIX}* "${RPM_DIR}"
 tar -czf "${SOURCES_DIR}/${RPM_DIR}.tar" "${RPM_DIR}"
 
 cp "${SOURCES_DIR}"/../SPECS/* "${SOURCES_DIR}"
+
+# Download pre-build ENVOY vib from artifactory. Ignore if not found, in the case when branches do not match in the URL.
+wget "${ENVOY_VIB_URL}" -P "${SOURCES_DIR}" || true
+
+# Build the Photon-Controller vib from local repo and copy to SROUCES folder
+cd "${ROOT}"/python
+make vib-only
+cp "${ROOT}"/python/dist/* "${SOURCES_DIR}"
+
 
 DEBUG_OPTIONS=""
 if [ "$DEBUG" == "true" ]; then
@@ -47,4 +58,4 @@ docker run -i ${DEBUG_OPTIONS} \
 docker run -i \
   -v "${ROOT}"/java/build/RPMS/x86_64:/rpms \
   vmware/photon-controller-rpm-builder \
-  bash -c 'ls /rpms/photon-controller*.rpm | xargs rpm -Uvh && [ -d /opt/vmware/photon-controller ]'
+  bash -c 'ls /rpms/photon-controller*.rpm | xargs rpm -Uvh && [ -d /usr/lib/esxcloud/photon-controller-core ]'
