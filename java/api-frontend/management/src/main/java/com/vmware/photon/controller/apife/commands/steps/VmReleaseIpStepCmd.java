3d35756934a8f32dc2bcab4783a832bafd276335
Xiao Zhou <xiaoz@vmware.com>
2016-07-14 12:56:50 -0700

    apife: VmReleaseIpStepCmd call release ip in IpAllocatorService
    Change-Id: Ie2824627b84534d29545e92c1d2ed86ccdf9cde4

--- java/api-frontend/management/src/main/java/com/vmware/photon/controller/apife/commands/steps/VmReleaseIpStepCmd.java
+++ java/api-frontend/management/src/main/java/com/vmware/photon/controller/apife/commands/steps/VmReleaseIpStepCmd.java
@@ -15,13 +15,23 @@ package com.vmware.photon.controller.apife.commands.steps;
 
 import com.vmware.photon.controller.api.common.exceptions.ApiFeException;
 import com.vmware.photon.controller.apife.backends.StepBackend;
+import com.vmware.photon.controller.apife.backends.clients.PhotonControllerXenonRestClient;
 import com.vmware.photon.controller.apife.commands.tasks.TaskCommand;
 import com.vmware.photon.controller.apife.entities.StepEntity;
+import com.vmware.photon.controller.apife.exceptions.external.VmNotFoundException;
+import com.vmware.photon.controller.cloudstore.xenon.entity.IpAllocatorService;
+import com.vmware.photon.controller.cloudstore.xenon.entity.VmService;
+import com.vmware.photon.controller.cloudstore.xenon.entity.VmServiceFactory;
 import com.vmware.photon.controller.common.clients.exceptions.RpcException;
+import com.vmware.photon.controller.common.xenon.exceptions.DocumentNotFoundException;
+import com.vmware.xenon.common.Operation;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This is a step to release the VM ip which will be invoked when this VM
@@ -40,6 +50,12 @@ public class VmReleaseIpStepCmd extends StepCommand {
     String vmId = (String) step.getTransientResource(ResourceReserveStepCmd.VM_ID);
     checkNotNull(vmId, "VM id is not available");
 
+    Map<String, VmService.NetworkInfo> networkInfoMap = getNetworkInfo(vmId);
+
+    for (VmService.NetworkInfo networkInfo : networkInfoMap.values()) {
+      patchIpAllocation(networkInfo.id, networkInfo.macAddress);
+    }
+
     logger.info("Released IP for VM {}", vmId);
   }
 
@@ -47,4 +63,29 @@ public class VmReleaseIpStepCmd extends StepCommand {
   protected void cleanup() {
   }
 
+  private Map<String, VmService.NetworkInfo> getNetworkInfo(String vmId) throws VmNotFoundException {
+    PhotonControllerXenonRestClient photonControllerXenonRestClient = taskCommand
+        .getPhotonControllerXenonRestClient();
+    try {
+      Operation result = photonControllerXenonRestClient.get(VmServiceFactory.SELF_LINK + "/" + vmId);
+      VmService.State state = result.getBody(VmService.State.class);
+      return state.networkInfo == null ? new HashMap<>() : state.networkInfo;
+    } catch (DocumentNotFoundException e) {
+      throw new VmNotFoundException("Cannot get document for vm " + vmId);
+    }
+  }
+
+  private void patchIpAllocation(String subnetId, String macAddress) {
+    if (macAddress == null || macAddress.isEmpty()) {
+      logger.info("Skip releasing one network info entry for vm , it is null or empty.");
+      return;
+    }
+    logger.info("Releasing one network info entry for vm, macAddress is {}", macAddress);
+
+    IpAllocatorService.IpOperationPatch patch = new IpAllocatorService.IpOperationPatch(IpAllocatorService
+        .IpOperationPatch.Kind.ReleaseIpForMac, macAddress);
+
+    PhotonControllerXenonRestClient photonControllerXenonRestClient = taskCommand.getPhotonControllerXenonRestClient();
+    photonControllerXenonRestClient.patch(IpAllocatorService.FACTORY_LINK + "/" + subnetId, patch);
+  }
 }
