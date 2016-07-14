909526e33a0def7c6138fff34dbbd9b011ded2c5
Yu Sheng <ysheng@vmware.com>
2016-07-14 11:09:13 -0700

    nsx-client: add JsonIgnoreProperties for DHCP related model classes
    Change-Id: Id572b39a309c6f971554792d48ffe9da4ea2e87d

--- java/nsx-client/src/main/java/com/vmware/photon/controller/nsxclient/models/DhcpRelayServiceCreateSpec.java
+++ java/nsx-client/src/main/java/com/vmware/photon/controller/nsxclient/models/DhcpRelayServiceCreateSpec.java
@@ -16,6 +16,7 @@ package com.vmware.photon.controller.nsxclient.models;
 import com.vmware.photon.controller.nsxclient.datatypes.LogicalServiceResourceType;
 import com.vmware.photon.controller.nsxclient.utils.ToStringHelper;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
@@ -24,6 +25,7 @@ import java.util.Objects;
 /**
  * This class represents a DhcpRelayServiceCreateSpec JSON structure.
  */
+@JsonIgnoreProperties(ignoreUnknown =  true)
 public class DhcpRelayServiceCreateSpec {
 
   @JsonProperty(value = "resource_type", required = true)
