1bc00b8820c3adb39ea401804e78920422ae8deb
Umer Khan <ukhan@vmware.com>
2016-07-14 20:01:31 -0700

    java-ruby: add more logging to detect NO_SUCH_RESOURCE bug.
    - Schedular fails to find host with (recently set) availability zone
    - This bug is in PROMOTE and is inconsistent.
    
    Change-Id: Ib6036e8da399dea01ca74b20cbbf7aecccc5907d

--- java/common-xenon/src/main/java/com/vmware/photon/controller/common/xenon/QueryTaskUtils.java
+++ java/common-xenon/src/main/java/com/vmware/photon/controller/common/xenon/QueryTaskUtils.java
@@ -373,4 +373,20 @@ public class QueryTaskUtils {
     }
     ServiceUtils.logInfo(service, summary);
   }
+
+  /**
+   * Checks if propertyName is present in query's booleanClauses.
+   *
+   * @param query
+   * @param propertyName
+   * @return
+   */
+  public static boolean queryClausesContainProperty(QueryTask.Query query, String propertyName) {
+    for (QueryTask.Query clause : query.booleanClauses) {
+      if (clause.term != null && clause.term.propertyName != null && clause.term.propertyName.equals(propertyName)) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
