de121347163d451d469b26f3bc747686792d2934
Touseef Liaqat <tliaqat@vmware.com>
2016-07-15 18:15:32 -0700

    Add deployer configuration and more fixes
    Add Dockerfile that takes tar file being created by packer.
    
    Change-Id: Ieb8ddfb3cb2fd07324397badf79462d4eb1110ea

--- artifacts/create-docker.sh
+++ artifacts/create-docker.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -x -e
 
+# Create fresh RPM before creating docker container
+SCRIPT_DIR="$(dirname "$0")"
+"${SCRIPT_DIR}"/create-rpm.sh
+
 export BRANCH=${GERRIT_BRANCH:-`git rev-parse --abbrev-ref HEAD`}
 export VERSION=${BRANCH}
-packer build ./packer/docker.json
+
+rm -rf "${SCRIPT_DIR}"/build/docker/photon-controller-docker*
+
+packer build "${SCRIPT_DIR}"/packer/docker.json
+
+# Remove old docker image with same name
+docker rmi vmware/photon-controller || true
+
+# Build new container
+docker build "${SCRIPT_DIR}" -t vmware/photon-controller
