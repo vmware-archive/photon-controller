a097d4fe5286dd2450feaefd490619765383e499
Touseef Liaqat <tliaqat@vmware.com>
2016-07-20 17:42:39 -0700

    Several fixes for RPM related bugs
     * Fix container configuration paths
     * Fix branch name with - bug for building rpm
     * Add commit hash in output RPM file
     * Clean old vib before building
     * Change run.sh path pointing to persistent install location
     * Move photon-controller service file to RPM
    
    Change-Id: Ide57046124ceb2f71e6f869645ccf53a8563b16d

--- artifacts/create-rpm-helper.sh
+++ artifacts/create-rpm-helper.sh
@@ -2,7 +2,8 @@
 set -x -e
 
 VERSION=$1
-DEBUG=$2
+COMMIT=$2
+DEBUG=$3
 
 cd rpms
 chown root:root ./SPECS/*
@@ -11,7 +12,7 @@ chown root:root /usr/src/photon/SOURCES/*
 if [ "$DEBUG" == "true" ]; then
   bash
 else
-  rpmbuild -ba --define="pkg_version $VERSION" ./SPECS/photon-controller.spec
+  rpmbuild -ba --define="pkg_version $VERSION" --define="pkg_commit $COMMIT" ./SPECS/photon-controller.spec
   createrepo --database /usr/src/photon/RPMS
   tar -czf ../build/photon-controller-rpmrepo-"$VERSION".tar.gz -C /usr/src/photon/ RPMS
 fi
