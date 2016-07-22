a025d0c88dafdad80441640f15986b73a17bd29b
Touseef Liaqat <tliaqat@vmware.com>
2016-07-22 16:07:04 -0700

    Add increasing release number in RPM for tdnf updates to work
    Change-Id: Ia818e1b5482258224ff8ade156a0db81a2dfd180

--- artifacts/create-rpm-helper.sh
+++ artifacts/create-rpm-helper.sh
@@ -3,7 +3,8 @@ set -x -e
 
 VERSION=$1
 COMMIT=$2
-DEBUG=$3
+COMMIT_COUNT=$3
+DEBUG=$4
 
 cd rpms
 chown root:root ./SPECS/*
@@ -12,7 +13,13 @@ chown root:root /usr/src/photon/SOURCES/*
 if [ "$DEBUG" == "true" ]; then
   bash
 else
-  rpmbuild -ba --define="pkg_version $VERSION" --define="pkg_commit $COMMIT" ./SPECS/photon-controller.spec
+  rpmbuild -ba \
+    --define="pkg_version $VERSION" \
+    --define="pkg_commit $COMMIT" \
+    --define="pkg_commit_count $COMMIT_COUNT" \
+    ./SPECS/photon-controller.spec
+
   createrepo --database /usr/src/photon/RPMS
+
   tar -czf ../build/photon-controller-rpmrepo-"$VERSION".tar.gz -C /usr/src/photon/ RPMS
 fi
