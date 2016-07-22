b982d262c33f22184ed419f7e321a140cf84d104
Pankaj Lakhina <plakhina@vmware.com>
2016-07-22 12:34:25 -0700

    Implement handleExtractSubnet in DhcpSubnetService
    Also added some validation in handleCreate and some
    validation annotation not he state fields.
    
    Change-Id: I7c5c36a839db5e06a797f1122972308ad5056703

--- java/cloud-store/src/test/java/com/vmware/photon/controller/cloudstore/xenon/entity/DhcpSubnetServiceTest.java
+++ java/cloud-store/src/test/java/com/vmware/photon/controller/cloudstore/xenon/entity/DhcpSubnetServiceTest.java
@@ -15,13 +15,18 @@ package com.vmware.photon.controller.cloudstore.xenon.entity;
 
 import com.vmware.photon.controller.cloudstore.xenon.CloudStoreServiceGroup;
 import com.vmware.photon.controller.cloudstore.xenon.helpers.TestHelper;
+import com.vmware.photon.controller.common.IpHelper;
 import com.vmware.photon.controller.common.thrift.StaticServerSet;
 import com.vmware.photon.controller.common.xenon.BasicServiceHost;
 import com.vmware.photon.controller.common.xenon.ServiceHostUtils;
 import com.vmware.photon.controller.common.xenon.ServiceUtils;
 import com.vmware.photon.controller.common.xenon.XenonRestClient;
 import com.vmware.xenon.common.Operation;
+import com.vmware.xenon.common.Service;
+import com.vmware.xenon.common.UriUtils;
 
+import com.google.common.net.InetAddresses;
+import org.apache.commons.net.util.SubnetUtils;
 import org.apache.http.HttpStatus;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
@@ -31,6 +36,8 @@ import org.testng.annotations.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -81,13 +88,14 @@ public class DhcpSubnetServiceTest {
       assertThat(result.getStatusCode(), is(HttpStatus.SC_OK));
 
       DhcpSubnetService.State createdState = result.getBody(DhcpSubnetService.State.class);
+      startState.size = startState.highIp - startState.lowIp;
+
       assertThat(ServiceUtils.documentEquals(DhcpSubnetService.State.class, startState, createdState), is(true));
 
       DhcpSubnetService.State savedState = host.getServiceState(DhcpSubnetService.State.class,
           createdState.documentSelfLink);
       assertThat(ServiceUtils.documentEquals(DhcpSubnetService.State.class, startState, savedState), is(true));
       assertThat(startState.doGarbageCollection, is(false));
-
     }
   }
 
@@ -150,9 +158,62 @@ public class DhcpSubnetServiceTest {
     }
   }
 
+  /**
+   * Tests for the handlePatch method.
+   */
+  public class HandlePatchTest {
+    private DhcpSubnetService.State startState;
+
+    @BeforeMethod
+    public void beforeMethod() throws Throwable {
+      startState = createInitialState();
+      Operation result = xenonClient.post(DhcpSubnetService.FACTORY_LINK, startState);
+      assertThat(result.getStatusCode(), is(HttpStatus.SC_OK));
+      startState = result.getBody(DhcpSubnetService.State.class);
+    }
+
+    @AfterMethod
+    public void afterMethod() throws Throwable {
+      ServiceHostUtils.deleteAllDocuments(host, "test-host");
+    }
+
+    @Test
+    public void testExtractSubnet() throws Throwable {
+      DhcpSubnetService.SubnetOperationPatch subnetOperationPatch =
+          new DhcpSubnetService.SubnetOperationPatch(
+              DhcpSubnetService.SubnetOperationPatch.Kind.ExtractSubnetFromBottom,
+              16);
+      Operation patchOperation = new Operation()
+          .setAction(Service.Action.PATCH)
+          .setBody(subnetOperationPatch)
+          .setReferer("test-host")
+          .setUri(UriUtils.buildUri(host, startState.documentSelfLink));
+      host.sendRequestAndWait(patchOperation);
+
+      DhcpSubnetService.State currentState = host.getServiceState(DhcpSubnetService.State.class,
+          startState.documentSelfLink);
+
+      assertThat(currentState.lowIp, is(startState.lowIp + 16));
+      assertThat(currentState.size, is(currentState.highIp - currentState.lowIp));
+      assertThat(currentState.highIp, is(startState.highIp));
+    }
+  }
+
   private static DhcpSubnetService.State createInitialState() {
+    SubnetUtils subnetUtils = new SubnetUtils("192.168.0.0/16");
+    SubnetUtils.SubnetInfo subnetInfo = subnetUtils.getInfo();
+    Long lowIp, highIp;
+
+    InetAddress lowIpAddress = InetAddresses.forString(subnetInfo.getLowAddress());
+    lowIp = IpHelper.ipToLong((Inet4Address) lowIpAddress);
+
+    InetAddress highIpAddress = InetAddresses.forString(subnetInfo.getHighAddress());
+    highIp = IpHelper.ipToLong((Inet4Address) highIpAddress);
+
     DhcpSubnetService.State startState = new DhcpSubnetService.State();
-    startState.cidr = "192.168.0.0/16";
+    startState.lowIp = lowIp;
+    startState.highIp = highIp;
+
     return startState;
   }
 }
