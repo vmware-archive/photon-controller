31d29b83d2d45d588985c694b8747718517efe85
Touseef Liaqat <tliaqat@vmware.com>
2016-07-24 22:34:07 -0700

    Add Dockerfiles for combinations of photon-lightwave
    Also rename docker container repository from 'photon' to 'vmware'
    
    Change-Id: Ia9df13cb4c6279df90d641536e07217d158b7504

--- devbox-photon/prepare-devbox-deployment.sh
+++ devbox-photon/prepare-devbox-deployment.sh
@@ -17,8 +17,8 @@ export NO_RESTART_ALWAYS=1
 
 ./gradlew :devbox:renewPhoton
 
-vagrant ssh -c "docker tag photon/haproxy esxcloud/haproxy"
-vagrant ssh -c "docker tag photon/photon-controller-core esxcloud/photon-controller-core"
+vagrant ssh -c "docker tag vmware/haproxy esxcloud/haproxy"
+vagrant ssh -c "docker tag vmware/photon-controller-core esxcloud/photon-controller-core"
 
 mgmt_ui_container_url="https://ci.ec.eng.vmware.com/view/UI/job/ec-ui-mgmt-publish-docker-image-develop/lastSuccessfulBuild/artifact/ci/docker-image/esxcloud-management-ui.tar"
 container_tar=$(basename "$mgmt_ui_container_url")
