2834c57431e17d95cb503aba27e0580a2553bdcc
Pankaj Lakhina <plakhina@vmware.com>
2016-07-26 15:07:01 -0700

    Add helper method to calculate CIDR from an IPv4 range
    Change-Id: I351dddc9e9882db86e749c135ed9220bc383c5fa

--- java/common/src/main/java/com/vmware/photon/controller/common/IpHelper.java
+++ java/common/src/main/java/com/vmware/photon/controller/common/IpHelper.java
@@ -13,6 +13,8 @@
 
 package com.vmware.photon.controller.common;
 
+import com.google.common.base.Preconditions;
+
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -62,5 +64,41 @@ public class IpHelper {
   public static byte[] longToNetworkByteOrderArray(long addr) {
     return new byte[]{(byte) (addr >>> 24), (byte) (addr >>> 16), (byte) (addr >>> 8),
         (byte) addr};
+  }
+
+  /**
+   * Calculate the CIDR from a given ip range.
+   * Valid ranges are the ones that can be covered with a single CIDR and that
+   * is validated by making sure that the inverseSubnet+1 is a power of 2.
+   *
+   * @param ipLow
+   * @param ipHigh
+   * @return
+   */
+  public static String calculateCidrFromIpV4Range(long ipLow, long ipHigh) {
+
+    Preconditions.checkArgument(ipLow <= ipHigh,
+        String.format("ipLow should be less than or equal to ipHigh, ipLow=%s, ipHigh=%s", ipLow, ipHigh));
+
+    int inverseSubnetMask = (int) (ipHigh - ipLow);
+    int subnetMask = ~inverseSubnetMask;
+
+    Preconditions.checkState((inverseSubnetMask == 0) || ((inverseSubnetMask & (inverseSubnetMask + 1)) == 0),
+        String.format(
+            "inverseSubnetMask should be 0 or " +
+                "inverseSubnetMask + 1 should be a power of 2, inverseSubnetMask=%s, ipLow=%s, ipHigh=%s",
+            Long.toBinaryString(ipLow),
+            Long.toBinaryString(ipHigh),
+            Long.toBinaryString(inverseSubnetMask)));
+
+    Preconditions.checkState((ipLow & subnetMask) == ipLow,
+        String.format("ipLow & subnetMask should equal ipLow, ipLow = %s, ipHigh= %s, subnetMask= %s",
+            Long.toBinaryString(ipLow),
+            Long.toBinaryString(ipHigh),
+            Long.toBinaryString(subnetMask)));
+
+    int cidr = (32 - Integer.numberOfTrailingZeros(subnetMask));
+    InetAddress subnetAddress = longToIp(ipLow);
+    return subnetAddress.getHostAddress() + "/" + cidr;
   }
 }
