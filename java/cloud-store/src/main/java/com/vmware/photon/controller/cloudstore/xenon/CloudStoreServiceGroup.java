6e6531c7e489c0a3bc1bf27c4347dd7214d5cc9a
Xiao Zhou <xiaoz@vmware.com>
2016-07-27 16:49:48 -0700

    cloud-store: TaskTriggerService triggers DhcpSubnetDeleteService
    Change-Id: I7a10e1ff8ae03c27f84961be2c0afd04e86d5fb3

--- java/cloud-store/src/main/java/com/vmware/photon/controller/cloudstore/xenon/CloudStoreServiceGroup.java
+++ java/cloud-store/src/main/java/com/vmware/photon/controller/cloudstore/xenon/CloudStoreServiceGroup.java
@@ -46,6 +46,7 @@ import com.vmware.photon.controller.cloudstore.xenon.task.IpLeaseDeleteService;
 import com.vmware.photon.controller.cloudstore.xenon.task.TombstoneCleanerFactoryService;
 import com.vmware.photon.controller.cloudstore.xenon.task.trigger.AvailabilityZoneCleanerTriggerBuilder;
 import com.vmware.photon.controller.cloudstore.xenon.task.trigger.DatastoreCleanerTriggerBuilder;
+import com.vmware.photon.controller.cloudstore.xenon.task.trigger.DhcpSubnetDeleteTriggerBuilder;
 import com.vmware.photon.controller.cloudstore.xenon.task.trigger.EntityLockCleanerTriggerBuilder;
 import com.vmware.photon.controller.cloudstore.xenon.task.trigger.EntityLockDeleteTriggerBuilder;
 import com.vmware.photon.controller.cloudstore.xenon.task.trigger.TombstoneCleanerTriggerBuilder;
@@ -227,7 +228,9 @@ public class CloudStoreServiceGroup
             && photonControllerXenonHost.checkServiceAvailable(
             TaskTriggerFactoryService.SELF_LINK + DatastoreCleanerTriggerBuilder.TRIGGER_SELF_LINK)
             && photonControllerXenonHost.checkServiceAvailable(TaskSchedulerServiceFactory.SELF_LINK)
-            && photonControllerXenonHost.checkServiceAvailable(IP_LEASE_DELETE_SERVICE);
+            && photonControllerXenonHost.checkServiceAvailable(IP_LEASE_DELETE_SERVICE)
+            && photonControllerXenonHost.checkServiceAvailable(
+            TaskTriggerFactoryService.SELF_LINK + DhcpSubnetDeleteTriggerBuilder.TRIGGER_SELF_LINK);
   }
 
   @Override
