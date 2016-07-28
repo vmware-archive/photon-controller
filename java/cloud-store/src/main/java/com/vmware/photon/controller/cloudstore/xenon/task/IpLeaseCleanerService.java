d15d837a5f90980252e834f9beb72b17abe668a9
Xiao Zhou <xiaoz@vmware.com>
2016-07-28 16:01:55 -0700

    cloud-store: add place holder for IpLeaseCleanerService
    Change-Id: I7d4c9d5bcb5880dc89b5f5632e68ba8fcc7146c0

---
+++ java/cloud-store/src/main/java/com/vmware/photon/controller/cloudstore/xenon/task/IpLeaseCleanerService.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2015 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.photon.controller.cloudstore.xenon.task;
+
+import com.vmware.photon.controller.common.xenon.InitializationUtils;
+import com.vmware.photon.controller.common.xenon.PatchUtils;
+import com.vmware.photon.controller.common.xenon.ServiceUtils;
+import com.vmware.photon.controller.common.xenon.ValidationUtils;
+import com.vmware.photon.controller.common.xenon.deployment.NoMigrationDuringDeployment;
+import com.vmware.photon.controller.common.xenon.migration.NoMigrationDuringUpgrade;
+import com.vmware.photon.controller.common.xenon.validation.DefaultBoolean;
+import com.vmware.photon.controller.common.xenon.validation.DefaultTaskState;
+import com.vmware.xenon.common.FactoryService;
+import com.vmware.xenon.common.Operation;
+import com.vmware.xenon.common.ServiceDocument;
+import com.vmware.xenon.common.StatefulService;
+import com.vmware.xenon.common.TaskState;
+
+/**
+ * Class implementing a periodically triggered service to clean up IpLeaseService,
+ * in the case that vm has been deleted,
+ * but the vmId in IpLeaseService has not been cleared.
+ */
+public class IpLeaseCleanerService extends StatefulService {
+
+  public static final String FACTORY_LINK = com.vmware.photon.controller.common.xenon.ServiceUriPaths.CLOUDSTORE_ROOT
+      + "/ip-leases-cleaners";
+
+  public static FactoryService createFactory() {
+    return FactoryService.create(IpLeaseDeleteService.class, IpLeaseDeleteService.State.class);
+  }
+
+  public IpLeaseCleanerService() {
+    super(State.class);
+    super.toggleOption(ServiceOption.PERSISTENCE, true);
+    super.toggleOption(ServiceOption.REPLICATION, true);
+    super.toggleOption(ServiceOption.OWNER_SELECTION, true);
+    super.toggleOption(ServiceOption.INSTRUMENTATION, true);
+  }
+
+  @Override
+  public void handleStart(Operation start) {
+    ServiceUtils.logInfo(this, "Starting service %s", getSelfLink());
+    State state = start.getBody(State.class);
+    initializeState(state);
+    validateState(state);
+    start.setBody(state).complete();
+  }
+
+  @Override
+  public void handlePatch(Operation patch) {
+    ServiceUtils.logInfo(this, "Handling patch for service %s", getSelfLink());
+    State currentState = getState(patch);
+    State patchState = patch.getBody(State.class);
+
+    validatePatch(currentState, patchState);
+    applyPatch(currentState, patchState);
+    validateState(currentState);
+    patch.complete();
+  }
+
+  /**
+   * Initialize state with defaults.
+   *
+   * @param current
+   */
+  private void initializeState(State current) {
+    InitializationUtils.initialize(current);
+
+    if (current.documentExpirationTimeMicros <= 0) {
+      current.documentExpirationTimeMicros =
+          ServiceUtils.computeExpirationTime(ServiceUtils.DEFAULT_DOC_EXPIRATION_TIME_MICROS);
+    }
+  }
+
+  /**
+   * Validate service state coherence.
+   *
+   * @param current
+   */
+  private void validateState(State current) {
+    ValidationUtils.validateState(current);
+  }
+
+  /**
+   * This method applies a patch to a state object.
+   *
+   * @param current Supplies the start state object.
+   * @param patch   Supplies the patch state object.
+   */
+  private State applyPatch(State current, State patch) {
+    ServiceUtils.logInfo(this, "Moving to stage %s", patch.taskState.stage);
+    if (patch.nextPageLink == null) {
+      current.nextPageLink = null;
+    }
+    PatchUtils.patchState(current, patch);
+    return current;
+  }
+
+  /**
+   * This method checks a patch object for validity against a document state object.
+   *
+   * @param current Supplies the start state object.
+   * @param patch   Supplies the patch state object.
+   */
+  private void validatePatch(State current, State patch) {
+    ValidationUtils.validatePatch(current, patch);
+    ValidationUtils.validateTaskStageProgression(current.taskState, patch.taskState);
+  }
+
+  /**
+   * Durable service state data.
+   */
+  @NoMigrationDuringUpgrade
+  @NoMigrationDuringDeployment
+  public static class State extends ServiceDocument {
+
+    /**
+     * Service execution stage.
+     */
+    @DefaultTaskState(value = TaskState.TaskStage.CREATED)
+    public TaskState taskState;
+
+    /**
+     * The link to next page.
+     */
+    public String nextPageLink;
+
+    /**
+     * Flag that controls if we should self patch to make forward progress.
+     */
+    @DefaultBoolean(value = false)
+    public Boolean isSelfProgressionDisabled;
+  }
+}
