1f93acbd0f51fb7212f04d86fc9492693199924b
Umer Khan <ukhan@vmware.com>
2016-07-29 16:49:36 -0700

    api: save ipRange, floatingIpRange & floatingAddressSpaceSubnetId in cloudstore
    - This saves ipRange & floatingIpRange in cloudstore.
    - Also a place holder for floating IPs subnet.
    
    Change-Id: I7c8eb8f6e139fce88f2a9039462cf5d238926ffd

--- java/api/frontend/src/test/java/com/vmware/photon/controller/api/frontend/entities/DeploymentEntityTest.java
+++ java/api/frontend/src/test/java/com/vmware/photon/controller/api/frontend/entities/DeploymentEntityTest.java
@@ -67,6 +67,9 @@ public class DeploymentEntityTest {
       entity.setNetworkManagerPassword("networkManagerPassword");
       entity.setNetworkZoneId("networkZoneId");
       entity.setNetworkTopRouterId("networkTopRouterId");
+      entity.setIpRange("10.0.0.1/24");
+      entity.setFloatingIpRange("192.168.0.1/28");
+      entity.setFloatingAddressSpaceSubnetId("floatingAddressSpaceSubnetId");
       entity.setNtpEndpoint("http://ntp");
       entity.setImageDatastores(Collections.singleton("datastore1"));
       entity.setUseImageDatastoreForVms(true);
@@ -88,6 +91,9 @@ public class DeploymentEntityTest {
       assertThat(entity.getNetworkManagerPassword(), is("networkManagerPassword"));
       assertThat(entity.getNetworkZoneId(), is("networkZoneId"));
       assertThat(entity.getNetworkTopRouterId(), is("networkTopRouterId"));
+      assertThat(entity.getIpRange(), is("10.0.0.1/24"));
+      assertThat(entity.getFloatingIpRange(), is("192.168.0.1/28"));
+      assertThat(entity.getFloatingAddressSpaceSubnetId(), is("floatingAddressSpaceSubnetId"));
       assertThat(entity.getNtpEndpoint(), is("http://ntp"));
       Assert.assertTrue(entity.getImageDatastores().contains("datastore1"));
       assertThat(entity.getUseImageDatastoreForVms(), is(true));
@@ -120,6 +126,9 @@ public class DeploymentEntityTest {
       entity.setNetworkManagerPassword("networkManagerPassword");
       entity.setNetworkZoneId("networkZoneId");
       entity.setNetworkTopRouterId("networkTopRouterId");
+      entity.setIpRange("10.0.0.1/24");
+      entity.setFloatingIpRange("192.168.0.1/28");
+      entity.setFloatingAddressSpaceSubnetId("floatingAddressSpaceSubnetId");
       entity.setNtpEndpoint("http://ntp");
       entity.setImageDatastores(Collections.singleton("datastore1"));
       entity.setUseImageDatastoreForVms(true);
