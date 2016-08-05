ebb51af9a4c8a8b0920993bfcbed78fa8b41d858
Gokcen Iskender <giskender@vmware.com>
2016-08-04 17:29:22 -0700

    Thrift endpoints use SSL cert when auth is enabled
    Change-Id: I9ffc8f72a03fb64c4844e5ad950bec36d02c795c

--- java/common/src/main/java/com/vmware/photon/controller/common/thrift/ClientPoolUtils.java
+++ java/common/src/main/java/com/vmware/photon/controller/common/thrift/ClientPoolUtils.java
@@ -17,10 +17,13 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.thrift.async.TAsyncClient;
 import org.apache.thrift.protocol.TProtocolFactory;
 import org.apache.thrift.transport.TNonblockingSocket;
-import org.apache.thrift.transport.TNonblockingTransport;
+import org.apache.thrift.transport.TSSLTransportFactory;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.Map;
@@ -35,9 +38,21 @@ public class ClientPoolUtils {
   public static <C extends TAsyncClient> C createNewClient(
       InetSocketAddress address, TProtocolFactory protocolFactory,
       ClientPoolOptions options, ThriftFactory thriftFactory,
-      TAsyncClientFactory<C> clientFactory, Map<C, TNonblockingTransport> clientTransportMap)
-      throws IOException {
-    TNonblockingSocket socket = new TNonblockingSocket(address.getHostString(), address.getPort());
+      TAsyncClientFactory<C> clientFactory, Map<C, TTransport> clientTransportMap)
+      throws IOException, TTransportException {
+    TTransport socket = null;
+
+    if (!isKeyStoreUsed(options.getKeyStorePath())) {
+      // Auth is not enabled
+      socket = new TNonblockingSocket(address.getHostString(), address.getPort());
+    } else {
+      TSSLTransportFactory.TSSLTransportParameters params =
+          new TSSLTransportFactory.TSSLTransportParameters();
+      params.setTrustStore(options.getKeyStorePath(), options.getKeyStorePassword());
+
+      socket = TSSLTransportFactory.getClientSocket(address.getHostString(), address.getPort(), (options.getTimeoutMs
+          () == 0L) ? 10000 : (int) options.getTimeoutMs(), params);
+    }
     if (StringUtils.isNotBlank(options.getServiceName())) {
       protocolFactory = thriftFactory.create(options.getServiceName());
     }
@@ -46,5 +61,14 @@ public class ClientPoolUtils {
     clientTransportMap.put(client, socket);
     logger.debug("created new client {} for {}", client, address);
     return client;
+  }
+
+  private static boolean isKeyStoreUsed(String keyStorePath) {
+    if (keyStorePath == null) {
+      return false;
+    }
+
+    File file = new File(keyStorePath);
+    return file.exists();
   }
 }
