5ba8315e89cff9788583e4168f7304581b50c755
Long Zhou <longz@vmware.com>
2016-08-15 15:20:21 -0700

    Agent: support DVS (distributed virtual switch)
    DVS is handled in similar fashion as PortGroup, because both are
    static networks pre-created by admin. They are different from
    NSX virtual networks which are created dynamically.
    
    DVSwitches are reported to management plane in GetHostConfig response
    as a VM Network.
    When creating a VM connected to DVS, we first create a DVPort on the
    DVS, then connect VM to it. When deleting VM or if VM creation fails,
    we need to delete DVPort to avoid leaking this resource.
    
    Change-Id: I01072269d67a609cfb5c773848d4223fd092c793

--- python/src/host/host/hypervisor/network_manager.py
+++ python/src/host/host/hypervisor/network_manager.py
@@ -24,19 +24,36 @@ class NetworkManager(object):
         self.logger = logging.getLogger(__name__)
 
     def get_vm_networks(self):
-        """ Return the list of all actual networks on this ESX server. """
+        """ Return the list of VM Networks on this host. """
         return self.vim_client.get_networks()
 
-    def get_networks(self):
-        """ This method will call vim_client to get a list of VM networks and
-        translate them into thrift representation.
-        """
-        vm_networks = self.vim_client.get_networks()
+    def get_dvs(self):
+        """ Return the list of Distributed Virtual Switch on this host. """
+        return self.vim_client.get_dvs()
 
+    def get_networks(self):
+        """ Return all networks (combining VM networks and DVS)
+            Translate into thrift representation.
+        """
         networks = []
-        # Add VM networks in network list
+
+        # Add VM networks to network list
+        vm_networks = self.vim_client.get_networks()
         for network_name in vm_networks:
             network = Network(network_name, [NetworkType.VM])
             networks.append(network)
 
+        # Add DVS to network list
+        dvs = self.vim_client.get_dvs()
+        for network_name in dvs:
+            network = Network(network_name, [NetworkType.VM])
+            networks.append(network)
+
+        self.logger.debug("found %d vm_networks and %d dvs" % (len(vm_networks), len(dvs)))
         return networks
+
+    def create_dvport(self, dvs):
+        return self.vim_client.create_dvport(dvs)
+
+    def delete_dvport(self, dvport):
+        self.vim_client.delete_dvport(dvport)
