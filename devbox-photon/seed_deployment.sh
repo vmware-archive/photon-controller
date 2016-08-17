2a52af82cce325a974b06b0be17312e999026b5a
York Wei <wyork@vmware.com>
2016-08-17 16:08:54 -0700

    devbox: update seeding of the deployment entity
    Seeding the deployment will be done in a separate file
    to handle the create spec and credentials for auth enabled.
    
    Change-Id: If2466dfa0386062f3ab055863f14ed28073a1988

---
+++ devbox-photon/seed_deployment.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -e
+
+# This script generates a deployment create spec from set environment variables
+# and creates a deployment entity to CloudStore.
+# DEPLOYMENT_SEED_CURL_OPTS are needed for an auth enabled deployment to send the proper credentials.
+# This is sent directly from the container because the proper credential files for an auth enabled
+# deployment are located only in the container.
+
+if [[ $ENABLE_AUTH == 'true' ]]; then
+    DEPLOYMENT_SEED_CURL_OPTS="--cert /etc/keys/machine.crt \
+  --key /etc/keys/machine.privkey --cacert /etc/keys/cacert.crt"
+    PROTOCOL="https"
+else
+    DEPLOYMENT_SEED_CURL_OPTS=""
+    PROTOCOL="http"
+fi
+
+deployment_create_spec_json="{ \
+   \"state\" : \"READY\", \
+   \"imageDataStoreNames\" : [\"${ESX_DATASTORE}\"], \
+   \"imageDataStoreUsedForVMs\" : \"true\", \
+   \"imageId\" : \"none\", \
+   \"projectId\" : \"none\",\
+   \"virtualNetworkEnabled\" : \"false\", \
+   \"documentSelfLink\" : \"test-deployment\""
+
+   if [[ $ENABLE_AUTH == 'true' ]]; then
+      deployment_create_spec_json+=", \
+      \"oAuthEnabled\" : true, \
+      \"oAuthTenantName\" : \"${LW_DOMAIN_NAME}\", \
+      \"oAuthSecurityGroups\" : [\"${LW_DOMAIN_NAME}\\Administrators\"], \
+      \"oAuthUserName\" : \"ec-admin@${LW_DOMAIN_NAME}\", \
+      \"oAuthPassword\" : \"${LW_PASSWORD}\", \
+      \"oAuthServerPort\" : 443"
+   else
+      deployment_create_spec_json+=", \
+      \"oAuthEnabled\" : false"
+   fi
+   if [ -n $PUBLIC_LW_NETWORK_IP ]; then
+      deployment_create_spec_json+=", \
+      \"oAuthServerAddress\" : \"${PUBLIC_LW_NETWORK_IP}\""
+   else
+      private_network_lw_ip = ${PRIVATE_NETWORK_LW_IP} || "172.31.253.67"
+      deployment_create_spec_json+="', \
+      \"oAuthServerAddress\" : \"${private_network_lw_ip}\""
+   fi
+   if [[ $STATS_ENABLED == "true" ]]; then
+      deployment_create_spec_json+=", \
+      \"statsStorePort\" : \"${STATS_STORE_PORT}\", \
+      \"statsStoreEndpoint\" : \"${STATS_STORE_ENDPOINT}\", \
+      \"statsEnabled\" : \"true\""
+   else
+      deployment_create_spec_json+=", \
+      \"statsEnabled\" : \"false\""
+   fi
+deployment_create_spec_json+="}"
+
+echo "Deployment create spec ${deployment_create_spec_json}"
+
+echo "Seeding deployment state"
+docker_curl="docker exec photon-controller-core curl -sS -w \"%{http_code}\" $DEPLOYMENT_SEED_CURL_OPTS \
+            -H \"Content-type: application/json\" -d \"$deployment_create_spec_json\" \
+            $PROTOCOL://$PUBLIC_NETWORK_IP:19000/photon/cloudstore/deployments"
+vagrant ssh -c "$docker_curl"
+exit 0
