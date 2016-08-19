806d611d014a3d96cd807d8ec9f3e541266680bf
Umer Khan <ukhan@vmware.com>
2016-08-19 16:08:14 -0700

    int-test: add virtual networks URIs in authorization tests.
    - This change adds physical networks or virtual networks based on
      sdn_enabled field in the deployment.
    
    Change-Id: I4addfc6e5a4914dbdac31c1b853021a6003c36b4

--- ruby/integration_tests/spec/support/api_routes_helper.rb
+++ ruby/integration_tests/spec/support/api_routes_helper.rb
@@ -15,16 +15,29 @@ module EsxCloud
   class ApiRoutesHelper
     def self.all_routes
       [
-        *auth_routes,
+        *auth_routes
+      ].concat(all_routes_excluding_auth_routes)
+    end
+
+    def self.all_routes_excluding_auth_routes
+      seeder = EsxCloud::SystemSeeder.instance
+      if seeder.deployment.network_configuration.sdn_enabled
+        nw_routes = virtual_networks_routes
+        proj_routes = projects_routes_with_virtual_networks_routes(seeder.project!.id)
+      else
+        nw_routes = networks_routes
+        proj_routes = projects_routes(seeder.project!.id)
+      end
+      [
         *clusters_routes,
         *datastores_routes,
         *deployments_routes,
         *disks_routes,
         *flavors_routes,
         *hosts_routes,
-        *networks_routes,
+        *nw_routes,
         *images_routes,
-        *projects_routes,
+        *proj_routes,
         *resource_tickets_routes,
         *tasks_routes,
         *tenants_routes,
@@ -34,6 +47,13 @@ module EsxCloud
     end
 
     def self.all_routes_using_seeder(seeder)
+      if seeder.deployment.network_configuration.sdn_enabled
+        nw_routes = virtual_networks_routes
+        proj_routes = projects_routes_with_virtual_networks_routes(seeder.project!.id)
+      else
+        nw_routes = networks_routes
+        proj_routes = projects_routes(seeder.project!.id)
+      end
       [
           *auth_routes,
           *clusters_routes,
@@ -41,11 +61,11 @@ module EsxCloud
           *deployments_routes(seeder.deployment!.id),
           *disks_routes(seeder.persistent_disk!.id),
           *hosts_routes,
-          *networks_routes,
+          *nw_routes,
           *vms_routes(seeder.vm!.id),
           *images_routes(seeder.image!.id),
           *flavors_routes(seeder.vm_flavor!.id),
-          *projects_routes(seeder.project!.id),
+          *proj_routes,
           *resource_tickets_routes,
           *tasks_routes,
           *tenants_routes(seeder.tenant!.id),
@@ -126,7 +146,16 @@ module EsxCloud
         EsxCloud::ApiRoute.new(:post, "/subnets", 400, 400, 403, 403, 403),
         EsxCloud::ApiRoute.new(:get, "/subnets/#{id}", 404, 404, 404, 404, 404),
         EsxCloud::ApiRoute.new(:post, "/subnets/#{id}/set_portgroups", 400, 400, 403, 403, 403),
-        EsxCloud::ApiRoute.new(:delete, "/subnets/#{SecureRandom.uuid}", 404, 404, 403, 403, 403)
+        EsxCloud::ApiRoute.new(:delete, "/subnets/#{SecureRandom.uuid}", 404, 404, 403, 403, 403),
+        EsxCloud::ApiRoute.new(:post, "/subnets/#{id}/set_default", 400, 400, 403, 403, 403)
+      ]
+    end
+
+    def self.virtual_networks_routes(id = SecureRandom.uuid)
+      [
+        EsxCloud::ApiRoute.new(:get, "/subnets/#{id}", 404, 404, 404, 404, 404),
+        EsxCloud::ApiRoute.new(:delete, "/subnets/#{SecureRandom.uuid}", 404, 404, 403, 403, 403),
+        EsxCloud::ApiRoute.new(:post, "/subnets/#{id}/set_default", 400, 400, 403, 403, 403)
       ]
     end
 
@@ -153,6 +182,15 @@ module EsxCloud
         EsxCloud::ApiRoute.new(:post, "/projects/#{id}/set_security_groups", 400, 400, 400, 403, 403),
         EsxCloud::ApiRoute.new(:delete, "/projects/#{id}", 404, 201, 201, 403, 403)
       ]
+    end
+
+    def self.projects_routes_with_virtual_networks_routes(id = SecureRandom.uuid)
+      projects_routes.concat(
+        [
+          EsxCloud::ApiRoute.new(:get, "/projects/#{id}/subnets", 404, 200, 200, 200, 403),
+          EsxCloud::ApiRoute.new(:post, "/projects/#{id}/subnets", 400, 400, 400, 400, 403),
+        ]
+      )
     end
 
     def self.resource_tickets_routes(id = SecureRandom.uuid)
