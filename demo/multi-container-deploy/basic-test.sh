e435d76de536eba252d51d7d6e82b8a9a7543de6
Touseef Liaqat <tliaqat@vmware.com>
2016-08-23 14:45:10 -0700

    Add default deployment at service startup
    New deployment property added in the deployer config
    that user can pass to set default deployment.
    Renamed auth config properties to be consistent with naming convention.
    Added more auth properties in config so that default deployment can
    be created.
    Updated scripts that depend on this new default deployment.
    Tests added to verify that deployment is created in multi host environment,
    and that IDEMPOTENT behavior of deployment service will handle the
    subsequent requests of creating this deployment object.
    One thing to note here is that, by this manual deployment the
    many things being managed by by deployment workflow service would not be happening.
    We still need to figure out what we have to replicate from that workflow service here
    when we are creating deployment at startup time.
    
    Change-Id: I68614bf87aff408ec4f15a6eab397266cfb3c604

--- demo/multi-container-deploy/basic-test.sh
+++ demo/multi-container-deploy/basic-test.sh
@@ -1,7 +1,12 @@
-#!/bin/bash -xe
+#!/bin/bash -x +e
 
 PHOTON_CONTROLLER_HOST_IP=192.168.114.12
 LIGHTWAVE_HOST_IP=192.168.114.2
 
 docker cp ./basic-test-helper.sh photon-controller-0:/
 docker exec -t photon-controller-0 /basic-test-helper.sh
+if [ $? -ne 0 ]; then
+  echo "On error clean up running containers."
+  ./delete-pc-cluster.sh
+  ./delete-lw-cluster.sh
+fi
