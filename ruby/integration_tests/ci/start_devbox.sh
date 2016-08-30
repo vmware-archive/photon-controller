298922b5a34a8aa83d76a4bee139fd3fbebba16c
Long Zhou <longz@vmware.com>
2016-08-30 14:12:57 -0700

    devbox: default for ENVOY_VIB_URL
    My recent changes in agent made it requires envoy.vib, or agent
    won't launch.
    In CI, builds or full deployment, we already setup envoy by default.
    This change adds a default envoy url for devbox.
    
    Change-Id: I7f65f85e10d83bb9b1ee990dc9a8927a9c592f1d

--- ruby/integration_tests/ci/start_devbox.sh
+++ ruby/integration_tests/ci/start_devbox.sh
@@ -3,6 +3,8 @@
 export WORKSPACE=${WORKSPACE:=$(git rev-parse --show-toplevel)}
 export DEVBOX=${DEVBOX:="$WORKSPACE/devbox-photon"}
 export TESTS=${TESTS:="$WORKSPACE/ruby/integration_tests"}
+export ENVOY_VIB_URL=${ENVOY_VIB_URL:="https://s3.amazonaws.com/photon-platform/artifacts/vibs/envoy/develop/latest/vmware-envoy-latest.vib"}
+
 if [ ! -d "$DEVBOX" ]; then fail "$DEVBOX is not accessible"; fi
 
 # Only continue if all these environment variables are defined
