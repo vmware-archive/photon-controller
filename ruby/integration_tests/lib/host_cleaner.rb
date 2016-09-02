d3c14eb2cbea91e179691dfe3d334889d6e59ccd
Varnika Goyal <goyalv@vmware.com>
2016-09-02 12:55:00 -0700

    VSAN : fix for file leak in host cleaner
    This change fixes following issues:
    -when ddb.deletable=false, image delete fails with Vmacore.FileIOPermissionDeniedException
     The fix is to edit .vmdk file and remove this flag. See PR 1704935 for details.
    
    Change-Id: Id4b5836caa6860c2880b513ee84a750fdfd8a29f
    (cherry picked from commit 6a80f3864e0bf0456f7fd5ecdb6bfc312263a67c)

--- ruby/integration_tests/lib/host_cleaner.rb
+++ ruby/integration_tests/lib/host_cleaner.rb
@@ -120,7 +120,9 @@ module EsxCloud
         DATASTORE_DIRS_TO_DELETE.each do |folder|
           if datastore.start_with?('vsan')
             rm_cmd = "for dir in `/usr/lib/vmware/osfs/bin/osfs-ls #{datastore_dir} | grep -i #{folder}`; do"\
-                     " for vmdk in `find $dir -name *.vmdk`; do vmkfstools -U #{datastore_dir}$vmdk || true; done"\
+                     " for vmdk in `find #{datastore_dir}$dir/ -name *.vmdk`; do"\
+                     "  if [ #{folder} = 'image' ]; then sed -i '/ddb.deletable = \"false\"/d' $vmdk; fi"\
+                     " && vmkfstools -U $vmdk || true; done"\
                      " && rm -rf #{datastore_dir}$dir/*"\
                      " && (rm -rf #{datastore_dir}$dir/.* || true)"\
                      " && /usr/lib/vmware/osfs/bin/osfs-rmdir #{datastore_dir}$dir;"\
