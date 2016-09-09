ab8d879f9936f5953aacdbb26d2d573300627be7
Touseef Liaqat <tliaqat@vmware.com>
2016-09-09 00:41:56 -0700

    Scripts to launch multi-container Photon-Controller deployment
    These scripts can be used to launch a cluster of 3 Lightwave container nodes.
    Then launch 3 container nodes of Photon-Controller cluster that joins with Lightwave.
    And then does basic test on this cluster for sanity checking.
    
    Change-Id: Iec032703a9f898e6c2b08f2e08f4db13d18abc15

---
+++ demo/multi-container-deploy/load-images.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -xe
+
+if [ "$(find . -iname photon-controller-docker-*.tar | wc -l)" -ne "1" ]; then
+  echo "ERROR: Found more than 1 photon-controller docker tar files. Expected 1"
+  exit 1
+fi
+
+find . -iname photon-controller-docker-*.tar | xargs docker load -i
+docker load < ./vmware-lightwave-sts.tar
+docker tag vmware/photon-controller vmware/photon-controller-lightwave-client || true
