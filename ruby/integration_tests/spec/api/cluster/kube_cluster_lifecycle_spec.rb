378aaa4fa6ad1d9ace7ef3d8c8d02530da87fb49
Shan Yang <yangshan@vmware.com>
2016-09-10 19:11:55 -0700

    Ruby Test: Add Kubernetes responding check
    Did some factorization of the code for checking
    
    Change-Id: Ia2349c3116de3699330689146888718e2eb352e1

--- ruby/integration_tests/spec/api/cluster/kube_cluster_lifecycle_spec.rb
+++ ruby/integration_tests/spec/api/cluster/kube_cluster_lifecycle_spec.rb
@@ -40,17 +40,8 @@ describe "Kubernetes cluster-service lifecycle", cluster: true do
 
     puts "Starting to create a Kubernetes cluster"
     begin
-      public_key_contents = File.read("/tmp/test_rsa.pub")
       project = @seeder.project!
-      props = {
-        "dns" => ENV["MESOS_ZK_DNS"],
-        "gateway" => ENV["MESOS_ZK_GATEWAY"],
-        "netmask" => ENV["MESOS_ZK_NETMASK"],
-        "master_ip" => ENV["KUBERNETES_MASTER_IP"],
-        "container_network" => "10.2.0.0/16",
-        "etcd_ip1" => ENV["KUBERNETES_ETCD_1_IP"],
-        "ssh_key" => public_key_contents
-      }
+      props = construct_props(ENV["KUBERNETES_MASTER_IP"],ENV["KUBERNETES_ETCD_1_IP"])
       expected_etcd_count = 1
       if ENV["KUBERNETES_ETCD_2_IP"] != ""
         props["etcd_ip2"] = ENV["KUBERNETES_ETCD_2_IP"]
@@ -71,70 +62,117 @@ describe "Kubernetes cluster-service lifecycle", cluster: true do
         extended_properties: props
       )
 
-      cid = cluster.id
-      cluster = client.find_cluster_by_id(cid)
-      expect(cluster.name).to start_with("kubernetes-")
-      expect(cluster.type).to eq("KUBERNETES")
-      expect(cluster.worker_count).to eq 1
-      expect(cluster.state).to eq "READY"
-      expect(cluster.extended_properties["cluster_version"]).to eq ("v1.3.5")
-      expect(cluster.extended_properties.length).to be > 12
+      validate_cluster_responding(cluster, 1)
+      validate_kube_api_responding(ENV["KUBERNETES_MASTER_IP"], 2 )
 
-      puts "Check that host can ssh successfully"
-      # Disabling strict_host_key_checking (:paranoid => false) and setting user_known_hosts_file to null to not validate
-      # the host key as it will change with each lifecycle run.
-      Net::SSH.start(ENV["KUBERNETES_MASTER_IP"], "root",
-                     :keys => ["/tmp/test_rsa"],
-                     :paranoid => false,
-                     :user_known_hosts_file => ["/dev/null"]) do |ssh|
-        # Getting here without an exception means we can connect with ssh successfully. If SSH failed, we would get an
-        # exception like Authentication Failed or Connection Timeout and our tests will fail as we are catching the
-        # exception and failing the test below.
-        puts "SSH successful"
-      end
+      validate_ssh()
 
       N_WORKERS = (ENV["N_SLAVES"] || 2).to_i
+      resize_cluster(cluster, N_WORKERS, expected_etcd_count)
 
-      puts "Starting to resize a Kubernetes cluster"
-      client.resize_cluster(cid, N_WORKERS)
-      cluster = client.find_cluster_by_id(cid)
-      expect(cluster.worker_count).to eq N_WORKERS
+      delete_cluster(cluster)
 
-      puts "Waiting for cluster to become READY after resize"
-      EsxCloud::ClusterHelper.wait_for_cluster_state(cid, "READY", 5, 120, client)
-
-      puts "Getting Cluster VM list"
-      etcd_count = 0
-      master_count = 0
-      worker_count = 0
-      client.get_cluster_vms(cid).items.each do |i|
-        if i.name.start_with?("etcd")
-          etcd_count += 1
-        elsif i.name.start_with?("master")
-          master_count += 1
-        elsif i.name.start_with?("worker")
-          worker_count += 1
-        else
-          fail("Find an unknown vm #{i.name} in the cluster")
-        end
-      end
-      expect(etcd_count).to eq expected_etcd_count
-      expect(master_count).to eq 1
-      expect(worker_count).to eq N_WORKERS
-
-      puts "Starting to delete a Kubernetes cluster"
-      client.delete_cluster(cid)
-      begin
-        client.find_cluster_by_id(cid)
-        fail("KUBERNETES Cluster #{cid} should be deleted")
-      rescue EsxCloud::ApiError => e
-        e.response_code.should == 404
-      rescue EsxCloud::CliError => e
-        e.output.should match("not found")
-      end
     rescue EsxCloud::Error => e
       EsxCloud::ClusterHelper.show_logs(@seeder.project, client)
       fail "KUBERNETES cluster integration Test failed. Error: #{e.message}"
     end
+  end
+
+  private
+
+  # Curl to the Kubernetes api using env_master_ip to make sure that it is responding.
+  # Check that there is exactly expect_node number of nodes from the api response
+  # Node that the expected_node includes the master node for kubernetes
+  def validate_kube_api_responding(env_master_ip, expected_node)
+    puts "Check Kubernetes api is responding"
+    get_node_count_cmd = "http://" + env_master_ip + ":8080/api/v1/nodes | grep nodeInfo | wc -l"
+    total_node_count = `curl #{get_node_count_cmd} `
+    expect(total_node_count.to_i).to eq expected_node
+  end
+
+  # This function validate that our cluster manager api is working properly
+  # It also checks that the extended properties added later were put it properly
+  def validate_cluster_responding(cluster_current, expected_worker_count)
+    cid_current = cluster_current.id
+    cluster_current = client.find_cluster_by_id(cid_current)
+    expect(cluster_current.name).to start_with("kubernetes-")
+    expect(cluster_current.type).to eq("KUBERNETES")
+    expect(cluster_current.worker_count).to eq expected_worker_count
+    expect(cluster_current.state).to eq "READY"
+    expect(cluster_current.extended_properties["cluster_version"]).to eq ("v1.3.5")
+    expect(cluster_current.extended_properties.length).to be > 12
+  end
+
+  def validate_ssh()
+    puts "Check that host can ssh successfully"
+    # Disabling strict_host_key_checking (:paranoid => false) and setting user_known_hosts_file to null to not validate
+    # the host key as it will change with each lifecycle run.
+    Net::SSH.start(ENV["KUBERNETES_MASTER_IP"], "root",
+                   :keys => ["/tmp/test_rsa"],
+                   :paranoid => false,
+                   :user_known_hosts_file => ["/dev/null"]) do |ssh|
+      # Getting here without an exception means we can connect with ssh successfully. If SSH failed, we would get an
+      # exception like Authentication Failed or Connection Timeout and our tests will fail as we are catching the
+      # exception and failing the test below.
+      puts "SSH successful"
+    end
+  end
+
+  def resize_cluster(cluster, expected_worker_count, expected_etcd_count)
+    puts "Starting to resize a Kubernetes cluster"
+    cid = cluster.id
+    client.resize_cluster(cid, expected_worker_count)
+    cluster = client.find_cluster_by_id(cid)
+    expect(cluster.worker_count).to eq expected_worker_count
+
+    puts "Waiting for cluster to become READY after resize"
+    EsxCloud::ClusterHelper.wait_for_cluster_state(cid, "READY", 5, 120, client)
+
+    puts "Getting Cluster VM list"
+    etcd_count = 0
+    master_count = 0
+    worker_count = 0
+    client.get_cluster_vms(cid).items.each do |i|
+      if i.name.start_with?("etcd")
+        etcd_count += 1
+      elsif i.name.start_with?("master")
+        master_count += 1
+      elsif i.name.start_with?("worker")
+        worker_count += 1
+      else
+        fail("Find an unknown vm #{i.name} in the cluster")
+      end
+    end
+    expect(etcd_count).to eq expected_etcd_count
+    expect(master_count).to eq 1
+    expect(worker_count).to eq expected_worker_count
+  end
+
+  def delete_cluster(cluster)
+    puts "Starting to delete a Kubernetes cluster"
+    cluster_id = cluster.id
+    client.delete_cluster(cluster_id)
+    begin
+      client.find_cluster_by_id(cluster_id)
+      fail("KUBERNETES Cluster #{cluster_id} should be deleted")
+    rescue EsxCloud::ApiError => e
+      e.response_code.should == 404
+    rescue EsxCloud::CliError => e
+      e.output.should match("not found")
+    end
+  end
+
+  def construct_props(master_ip, etcd_ip)
+    public_key_contents = File.read("/tmp/test_rsa.pub")
+    props = {
+        "dns" => ENV["MESOS_ZK_DNS"],
+        "gateway" => ENV["MESOS_ZK_GATEWAY"],
+        "netmask" => ENV["MESOS_ZK_NETMASK"],
+        "master_ip" => master_ip,
+        "container_network" => "10.2.0.0/16",
+        "etcd_ip1" => etcd_ip,
+        "ssh_key" => public_key_contents
+    }
+    return props
   end
 end
