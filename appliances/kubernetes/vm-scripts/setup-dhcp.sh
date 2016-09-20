1ffde22274f2376c285f5eb24acdd63bb7bf6e91
Shan Yang <yangshan@vmware.com>
2016-09-19 17:18:02 -0700

    Add init-network files to the Stem Cell
    This is the related to a recently discovered bug where worker nodes did
    not have enough time to get an DHCP address and the IP_ADDRESS got set
    incorrectly to local host for worker nodes. This was originally a cloud-init
    problem. But we are moving the code to the Kube Stem cell for better
    portability
    
    Change-Id: Ic9b79b2aef33c78f4af91a0278058acc8346590a

---
+++ appliances/kubernetes/vm-scripts/setup-dhcp.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+eno_name=$(ip addr | grep eno | sed 's/.*\(eno.*\):.*/\1/' | head -n 1)
+cat > "/etc/systemd/network/10-dhcp-${eno_name}.network" << EOF
+[Match]
+Name=${eno_name}
+
+[Network]
+DHCP=yes
+EOF
+ip addr flush label "${eno_name}"
+systemctl restart systemd-networkd
+timeout=20
+second=0
+DEFAULT_INTERFACE=$(ip -o -4 route show to default | awk '{print $5}' | head -1)
+# wait for the interface to be not loopback
+echo "current interface: ${DEFAULT_INTERFACE}"
+ip addr
+while [[ "${DEFAULT_INTERFACE}" == "" ]] || [[ "${DEFAULT_INTERFACE}" == "lo" ]]; do
+  ((second++))
+  echo ${second}
+    if [[ ${second} -gt ${timeout} ]]; then
+      echo "ERROR: No network interface ready!"
+      break
+    fi
+    sleep 1
+    DEFAULT_INTERFACE=$(ip -o -4 route show to default | awk '{print $5}' | head -1)
+done
+echo "done: Output IP ADDR for ip addr is "
+ip addr
