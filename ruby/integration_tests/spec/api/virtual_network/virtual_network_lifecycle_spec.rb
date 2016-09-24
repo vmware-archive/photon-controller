05d1420b44fe1dc63c3f381b1b9d23ae72b7ff65
Umer Khan <ukhan@vmware.com>
2016-09-23 17:54:58 -0700

    int-test: add two virtual networks and create VM in each network
    - This verifies if ip allocated to VM is within allocated range of virtual network.
    
    Change-Id: Idc32b3ebf59dc58563128511078ffc92208c6abe

--- ruby/integration_tests/spec/api/virtual_network/virtual_network_lifecycle_spec.rb
+++ ruby/integration_tests/spec/api/virtual_network/virtual_network_lifecycle_spec.rb
@@ -44,6 +44,56 @@ describe "virtual_network_lifecyle", :virtual_network => true do
     network.delete
   end
 
+  it "Creates two virtual networks, two VMs and then delete them" do
+    network1 = create_network(spec)
+    network2 = create_network(spec)
+
+    retrieved_network_list = client.get_project_networks(@project.id)
+    expect(retrieved_network_list.items.count).to eq(2)
+    retrieved_network1 = retrieved_network_list.items[0]
+    retrieved_network2 = retrieved_network_list.items[1]
+
+    vm1 = create_vm(@project, { networks: [retrieved_network1.id] })
+    vm1.start!
+
+    vm2 = create_vm(@project, { networks: [retrieved_network2.id] })
+    vm2.start!
+
+    network1_connections = client.get_vm_networks(vm1.id).network_connections
+    network_match = false
+    network1_connections.each do |nc|
+      if nc.network == retrieved_network1.id
+        network_match = true
+        break
+      end
+    end
+    expect(network_match).to be_true
+
+    network1_connection_ips = network1_connections.map { |nc| nc.ip_address }
+    retrieved_network1_range = retrieved_network1.low_ip_dynamic + "-" + retrieved_network1.high_ip_dynamic
+    expect((network1_connection_ips-IPRange.parse(retrieved_network1_range)).empty?).to be_true
+
+    network2_connections = client.get_vm_networks(vm2.id).network_connections
+    network_match = false
+    network2_connections.each do |nc|
+      if nc.network == retrieved_network2.id
+        network_match = true
+        break
+      end
+    end
+    expect(network_match).to be_true
+
+    network2_connection_ips = network2_connections.map { |nc| nc.ip_address }
+    retrieved_network2_range = retrieved_network2.low_ip_dynamic + "-" + retrieved_network2.high_ip_dynamic
+    expect((network2_connection_ips-IPRange.parse(retrieved_network2_range)).empty?).to be_true
+
+    vm1.stop!
+    vm1.delete
+
+    vm2.stop!
+    vm2.delete
+  end
+
   private
 
   def create_network(spec)
