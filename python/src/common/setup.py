c5def7b86b1a834812baa15bd7b8af1c353f6b9e
Long Zhou <longz@vmware.com>
2016-09-28 15:11:16 -0700

    Check-in thrift 0.9.3 python bindings
    To enable thrift over SSL, we need to apply a fix on thrift 0.9.3
    python bindings. The fix is already committed to thrift main, but
    has not yet released. So before next thrift release, we need to
    carry a copy of the code.
    
    Thrift python bindings are checked in under python/src/thrift.
    This change also updates photon agent to use this copy.
    
    Change-Id: Ia99991b88f3199fd6c72131aed9044ee658ccaa0

--- python/src/common/setup.py
+++ python/src/common/setup.py
@@ -27,7 +27,7 @@ setup(name='photon.controller.common',
           'enum34==0.9.19',
           'photon.controller.gen.thrift',
           'setuptools==0.6c11',
-          'thrift==0.9.3',
+          'thrift==0.9.3.1',
       ],
       extras_require={
           'test': [
