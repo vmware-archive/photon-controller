8a31db457cedf1b02ef8b3bf18975fd8d4df2f1b
Long Zhou <longz@vmware.com>
2016-10-01 05:53:18 -0700

    Use CertificateManager to apply new certificate
    Previously we overwrite rui.crt/.key directly and restart hostd to
    apply certificate. This approach is heavy-handed and causes deployment
    failures.
    
    With this change, we use hostd's CertificateManager to import
    pem and notify affected services.
    
    Change-Id: I7ba21e5e7d565e6177496027c7cfcf0ecc2a75a9

---
+++ python/vib/agent/payloads/agent/opt/vmware/photon/controller/sh/apply_cert.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -------------------------------------------------------------
+# Copyright (C) 2016 VMware, Inc. All rights reserved.
+# -------------------------------------------------------------
+import httplib
+import sys
+import pyVmomi
+from pyVmomi import vim
+import ssl
+
+
+if len(sys.argv) < 3:
+    print "Enter HostIP RootPassword PemPath"
+    print "Incorrect set of arguments passed."
+    sys.exit()
+host_ip = sys.argv[1]
+root_password = sys.argv[2]
+pem_path = sys.argv[3]
+
+print "Apply Certificate: %s, %s, %s" % (host_ip, root_password, pem_path)
+root_password='ca$hc0w'
+
+print "Connecting to hostd CertificateManager"
+ssl._create_default_https_context = ssl._create_unverified_context
+stub = pyVmomi.SoapStubAdapter(host=host_ip, version="vim.version.version10", path="/sdk")
+si = vim.ServiceInstance("ServiceInstance", stub)
+content = si.RetrieveServiceContent()
+content.sessionManager.Login("root", root_password)
+cert_manager = vim.host.CertificateManager('ha-certificate-manager', stub)
+
+print "Loading certificate from file"
+cert = open(pem_path, 'r').read()
+print "Installing certificate"
+dName = '/CN=%s' % host_ip
+csr = cert_manager.GenerateCertificateSigningRequestByDn(dName)
+cert_manager.InstallServerCertificate(cert=cert)
+
+print "Notifying affected services"
+try:
+    cert_manager.NotifyAffectedServices()
+except httplib.BadStatusLine:
+  pass
+
+print "Success"
