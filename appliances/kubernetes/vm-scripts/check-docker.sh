20970450a30a84fd18ac5598d51852ef3336ae84
York Wei <wyork@vmware.com>
2016-10-13 16:23:33 -0700

    Fix docker unable to start due to bridge network IP already in use
    This addresses the problem of docker is unable to start because
    it is unable to get the bridge network IP by removing the
    docker files before restarting the docker service.
    
    Change-Id: I9451138d8721a5ad59d9b52cc63085ec431db6a9

--- appliances/kubernetes/vm-scripts/check-docker.sh
+++ appliances/kubernetes/vm-scripts/check-docker.sh
@@ -17,6 +17,10 @@ do
     retry=`expr $retry + 1`
   done
     echo "Docker unreachable after $retry_max retries, restarting docker ($restart_retry/$restart_retry_max)"
+    # This addresses the issue where docker fails to start due to the bridge address is in use.
+    # We remove the docker network files that contain the network settings
+    # of the docker because docker can exit without properly closing the bridge address.
+    rm -rf /var/lib/docker/network/files
     systemctl restart docker
     sleep 1
     restart_retry=`expr $restart_retry + 1`
