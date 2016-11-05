ce493bb19c3ec23a08cb9e149eb210eca23404b0
Umer Khan <ukhan@vmware.com>
2016-11-05 12:08:19 -0700

    Use tdnf to install dnsmasq instead of getting from artifactory.
    Change-Id: I5654115001bf14f32289ecbf83b44780bddff954

--- appliances/dhcp-ova/build.sh
+++ appliances/dhcp-ova/build.sh
@@ -25,13 +25,9 @@ DHCP_AGENT_TAR_PATH=(../../java/dhcp-agent/build/distributions/dhcp-agent-*.tar)
 
 SSHD_ENABLE_ROOT_LOGIN=${SSHD_ENABLE_ROOT_LOGIN:-"false"}
 PHOTON_OVA_URL=${PHOTON_OVA_URL:="../photon-ova/build/photon-ova-virtualbox.ova"}
-DNSMASQ_URL=${DNSMASQ_URL:="http://artifactory.ec.eng.vmware.com/artifactory/esxcloud-archives/external/dnsmasq-v2-75/dnsmasq"}
-DNSMASQ_DHCP_RELEASE_URL=${DNSMASQ_DHCP_RELEASE_URL:="http://artifactory.ec.eng.vmware.com/artifactory/esxcloud-archives/external/dnsmasq-v2-75/dhcp_release"}
 
 packer build -force \
 	-var "photon_ova_url=$PHOTON_OVA_URL" \
-	-var "dnsmasq_url=$DNSMASQ_URL" \
-	-var "dnsmasq_dhcp_release_url=$DNSMASQ_DHCP_RELEASE_URL" \
 	-var "sshd_enable_root_login=$SSHD_ENABLE_ROOT_LOGIN" \
 	-var "dhcpAgentTarPath=$DHCP_AGENT_TAR_PATH" \
         dhcp-ova.json
