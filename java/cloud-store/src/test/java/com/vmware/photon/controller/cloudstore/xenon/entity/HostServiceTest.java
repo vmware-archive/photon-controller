ca34dd85b34eb73b3d9d965f5b7fe82397b5144c
Long Zhou <longz@vmware.com>
2016-11-11 07:12:03 -0800

    Support adding image datastores - HostService changes
    Modify HostService's periodic maintenance handler to
    check whether DeploymentService.State.imageDataStoreNames
    has changed; If changed, call agent to update its
    configuration.
    
    Change-Id: Ice22dd3c91eaa9284b6d63c6966c432944086de7

--- java/cloud-store/src/test/java/com/vmware/photon/controller/cloudstore/xenon/entity/HostServiceTest.java
+++ java/cloud-store/src/test/java/com/vmware/photon/controller/cloudstore/xenon/entity/HostServiceTest.java
@@ -13,12 +13,18 @@
 
 package com.vmware.photon.controller.cloudstore.xenon.entity;
 
+import com.vmware.photon.controller.agent.gen.AgentControl;
+import com.vmware.photon.controller.agent.gen.UpdateConfigResponse;
+import com.vmware.photon.controller.agent.gen.UpdateConfigResultCode;
 import com.vmware.photon.controller.api.model.AgentState;
+import com.vmware.photon.controller.api.model.DeploymentState;
 import com.vmware.photon.controller.api.model.HostState;
 import com.vmware.photon.controller.api.model.UsageTag;
 import com.vmware.photon.controller.cloudstore.xenon.helpers.TestEnvironment;
 import com.vmware.photon.controller.cloudstore.xenon.helpers.TestHelper;
 import com.vmware.photon.controller.cloudstore.xenon.task.DatastoreDeleteFactoryService;
+import com.vmware.photon.controller.common.clients.AgentControlClient;
+import com.vmware.photon.controller.common.clients.AgentControlClientFactory;
 import com.vmware.photon.controller.common.clients.HostClient;
 import com.vmware.photon.controller.common.clients.HostClientFactory;
 import com.vmware.photon.controller.common.thrift.StaticServerSet;
@@ -44,6 +50,7 @@ import com.vmware.xenon.services.common.QueryTask;
 
 import com.google.common.collect.ImmutableSet;
 import org.apache.thrift.async.AsyncMethodCallback;
+import org.hamcrest.CoreMatchers;
 import org.hamcrest.Matchers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -52,6 +59,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static org.hamcrest.CoreMatchers.containsString;
@@ -78,6 +86,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -541,6 +550,11 @@ public class HostServiceTest {
     private List<Datastore> datastoreList;
     private Set<String> imageDatastoreIds;
 
+    @BeforeTest
+    public void setUp() {
+      datastoreList = buildDatastoreList(10);
+    }
+
     @AfterTest
     public void tearDown() throws Throwable {
       if (testEnvironment != null) {
@@ -557,7 +571,7 @@ public class HostServiceTest {
       doReturn(hostClient).when(hostClientFactory).create();
 
       // Return OK response with host config which has datastores.
-      GetConfigResponse response = getConfigResponse(true, getHostConfig(true));
+      GetConfigResponse response = getConfigResponse(true, getHostConfig(true, null));
       Host.AsyncSSLClient.get_host_config_call call = mock(Host.AsyncSSLClient.get_host_config_call.class);
       doReturn(response).when(call).getResult();
 
@@ -625,7 +639,7 @@ public class HostServiceTest {
       doReturn(hostClient).when(hostClientFactory).create();
 
       // Return OK response with host config which has datastores.
-      GetConfigResponse response = getConfigResponse(true, getHostConfig(true));
+      GetConfigResponse response = getConfigResponse(true, getHostConfig(true, null));
       Host.AsyncSSLClient.get_host_config_call call = mock(Host.AsyncSSLClient.get_host_config_call.class);
       doReturn(response).when(call).getResult();
 
@@ -677,7 +691,7 @@ public class HostServiceTest {
       assertThat(savedState.state, is(HostState.MAINTENANCE));
 
       // Return OK response with host config which does not have datastores.
-      response = getConfigResponse(true, getHostConfig(false));
+      response = getConfigResponse(true, getHostConfig(false, null));
       doReturn(response).when(call).getResult();
 
       // Put host in READY state. This will trigger the deletion of datastores which were previously reported, but
@@ -740,6 +754,69 @@ public class HostServiceTest {
       assertThat(savedState.agentState, is(AgentState.MISSING));
     }
 
+    @Test
+    public void updateImageDatastores() throws Throwable {
+      HostClientFactory hostClientFactory = mock(HostClientFactory.class);
+      HostClient hostClient = mock(HostClient.class);
+      doReturn(hostClient).when(hostClientFactory).create();
+      AgentControlClientFactory agentControlClientFactory = mock(AgentControlClientFactory.class);
+      AgentControlClient agentControlClient = mock(AgentControlClient.class);
+      doReturn(agentControlClient).when(agentControlClientFactory).create();
+
+      testEnvironment = new TestEnvironment.Builder()
+          .hostClientFactory(hostClientFactory)
+          .agentControlClientFactory(agentControlClientFactory)
+          .hostCount(1)
+          .build();
+      DeploymentService.State startState = new DeploymentService.State();
+      startState.imageDataStoreNames = Collections.singleton(datastoreList.get(9).getName());
+      startState.imageDataStoreUsedForVMs = true;
+      startState.state = DeploymentState.READY;
+      DeploymentService.State deploymentState = testEnvironment.callServiceSynchronously(
+          DeploymentServiceFactory.SELF_LINK,
+          startState,
+          DeploymentService.State.class);
+
+      final CountDownLatch latch = new CountDownLatch(2);
+
+      // Return OK response with host config which has datastores.
+      GetConfigResponse response = getConfigResponse(true, getHostConfig(true, ServiceUtils.getIDFromDocumentSelfLink
+          (deploymentState.documentSelfLink)));
+      Host.AsyncSSLClient.get_host_config_call call = mock(Host.AsyncSSLClient.get_host_config_call.class);
+      doReturn(response).when(call).getResult();
+      doAnswer(invocation -> {
+        ((AsyncMethodCallback<Host.AsyncSSLClient.get_host_config_call>) invocation.getArguments()[0]).onComplete(call);
+        latch.countDown();
+        return null;
+      }).when(hostClient).getHostConfig(any(AsyncMethodCallback.class));
+
+      // Return OK response with host config which has datastores.
+      UpdateConfigResponse updateConfigResponse = new UpdateConfigResponse();
+      updateConfigResponse.setResult(UpdateConfigResultCode.OK);
+      AgentControl.AsyncSSLClient.update_config_call updateConfigCall =
+          mock(AgentControl.AsyncSSLClient.update_config_call.class);
+      doReturn(updateConfigResponse).when(updateConfigCall).getResult();
+      doAnswer(invocation -> {
+        ((AsyncMethodCallback<AgentControl.AsyncSSLClient.update_config_call>) invocation.getArguments()[2])
+            .onComplete(updateConfigCall);
+        latch.countDown();
+        return null;
+      }).when(agentControlClient).updateConfig(any(Set.class), any(Boolean.class), any(AsyncMethodCallback.class));
+
+      // Start host service with default state.
+      Operation result = testEnvironment.sendPostAndWait(HostServiceFactory.SELF_LINK,
+          TestHelper.getHostServiceStartState());
+      assertThat(result.getStatusCode(), is(Operation.STATUS_CODE_OK));
+      HostService.State createdState = result.getBody(HostService.State.class);
+
+      // Patch it to READY state and test that getHostConfig() got initiated.
+      HostService.State patchState = new HostService.State();
+      patchState.state = HostState.READY;
+      testEnvironment.sendPatchAndWait(createdState.documentSelfLink, patchState);
+
+      assertThat(latch.await(5, TimeUnit.SECONDS), CoreMatchers.is(true));
+    }
+
     private GetConfigResponse getConfigResponse(boolean success, HostConfig hostConfig) throws Throwable {
       GetConfigResponse response;
       if (success) {
@@ -753,8 +830,7 @@ public class HostServiceTest {
       return response;
     }
 
-    private HostConfig getHostConfig(boolean shouldReportDatastores) {
-      datastoreList = buildDatastoreList(10);
+    private HostConfig getHostConfig(boolean shouldReportDatastores, String deploymentId) {
       imageDatastoreIds = datastoreList.stream()
           .limit(3)
           .map((datastore) -> datastore.getId())
@@ -768,6 +844,9 @@ public class HostServiceTest {
       hostConfig.setCpu_count(hostCpuCount);
       hostConfig.setMemory_mb(hostMemoryMb);
       hostConfig.setEsx_version(esxVersion);
+      if (deploymentId != null) {
+        hostConfig.setDeployment_id(deploymentId);
+      }
       return hostConfig;
     }
 
