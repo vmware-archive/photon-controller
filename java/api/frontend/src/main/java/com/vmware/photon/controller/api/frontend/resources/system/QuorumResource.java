d6e4a1bb04ab2d108727c44d6a3e1cf5e0db4b03
Amol US1072108 <parikha@vmware.com>
2016-11-15 15:52:45 -0800

    API : Created an api for setting and creating a data quorum. Subject to change
    Get request get the membership quorum of the node.
    Post request sets the membership Quorum.
    
    Change-Id: I5185562d3fb02db90e33fb688c9c2d67c3ee3f35

---
+++ java/api/frontend/src/main/java/com/vmware/photon/controller/api/frontend/resources/system/QuorumResource.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2015 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.photon.controller.api.frontend.resources.system;
+
+import com.google.inject.Inject;
+import com.vmware.photon.controller.api.frontend.backends.clients.ApiFeXenonRestClient;
+import com.vmware.photon.controller.api.frontend.resources.routes.SystemRoutes;
+import com.vmware.photon.controller.api.model.System;
+import com.vmware.photon.controller.common.xenon.XenonRestClient;
+import com.vmware.photon.controller.common.xenon.exceptions.DocumentNotFoundException;
+import com.vmware.photon.controller.common.xenon.exceptions.BadRequestException;
+import com.vmware.xenon.common.Operation;
+import com.vmware.xenon.services.common.NodeGroupService;
+import com.vmware.xenon.services.common.ServiceUriPaths;
+
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
+import net.minidev.json.JSONObject;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+import static com.vmware.photon.controller.api.frontend.Responses.generateCustomResponse;
+
+/**
+ * This resource is for System related API.
+ */
+@Api(value = SystemRoutes.API)
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Path(SystemRoutes.GET_QUORUM)
+public class QuorumResource {
+
+    private final XenonRestClient xenonClient;
+    private final static String NODE_GROUPS = "/core/node-groups/default";
+    @Inject
+    public QuorumResource(ApiFeXenonRestClient xenonClient){
+        this.xenonClient = xenonClient;
+    }
+
+    @GET
+    @ApiOperation(value = "Information about the Photon Controller Data Quorum",
+            notes = "This API provides read-only information about the Photon Controller Data Quorum.",
+            response = System.class)
+    @ApiResponses(value = {@ApiResponse(code = 200 , message = "Returns the Quorum" )})
+    public Response get(@Context Request request) throws InterruptedException, BadRequestException, TimeoutException, DocumentNotFoundException
+    {
+        JSONObject jsonObject = new JSONObject();
+        Operation operation = xenonClient.get(NODE_GROUPS);
+        NodeGroupService.NodeGroupState body = operation.getBody(NodeGroupService.NodeGroupState.class);
+        for(Map.Entry<String, ?> entry: body.nodes.entrySet()) {
+            jsonObject.put("Quorum", body.nodes.get((entry.getKey())).membershipQuorum);
+        }
+        return generateCustomResponse(Response.Status.OK, jsonObject);
+    }
+
+    @POST
+    @ApiOperation(value = "Create a Photon Controller Quorum",
+            notes = "This API helps create the Photon Controller Quorum.",
+            response = System.class)
+    @ApiResponses(value = {@ApiResponse(code = 200 , message = "Creates the Quorum" )})
+    @Path(SystemRoutes.CREATE_QUORUM)
+    public void post(@Context Request request, @PathParam("size") int size) throws InterruptedException, BadRequestException, TimeoutException, DocumentNotFoundException {
+        updateQuorum(size);
+    }
+
+    private void updateQuorum(int quorumSize) {
+        NodeGroupService.UpdateQuorumRequest patch = new NodeGroupService.UpdateQuorumRequest();
+        patch.kind = NodeGroupService.UpdateQuorumRequest.KIND;
+        patch.membershipQuorum = quorumSize;
+        patch.isGroupUpdate = true;
+        try {
+            this.xenonClient.patch(ServiceUriPaths.DEFAULT_NODE_GROUP, patch);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } catch (BadRequestException e) {
+            e.printStackTrace();
+        } catch (DocumentNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
