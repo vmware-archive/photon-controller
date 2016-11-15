3c3343ec056c345a5e48f760f82796d2890d58ef
Amol US1072108 <parikha@vmware.com>
2016-11-15 15:52:45 -0800

    API : Created an api for setting and creating a data quorum. Get request get the membership quorum of the node. Post request sets the membership Quorum.
    Change-Id: I5185562d3fb02db90e33fb688c9c2d67c3ee3f35

---
+++ java/api/model/src/main/java/com/vmware/photon/controller/api/model/System.java
@@ -0,0 +1,24 @@
+package com.vmware.photon.controller.api.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.wordnik.swagger.annotations.ApiModel;
+import com.wordnik.swagger.annotations.ApiModelProperty;
+
+/**
+ * System API representation
+ */
+@ApiModel(value = "Information about the Data Quorum.")
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class System {
+
+    @JsonProperty
+    @ApiModelProperty(value = "The Data Quorum for the Photon Controller Deployment", required = true)
+    private String dataQuorum;
+
+    public String getDataQuorum() {
+        return dataQuorum;
+    }
+
+
+}
