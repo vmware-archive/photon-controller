ec9bd942b1bdf1c8966ee0fadfcaf0fa2a68081b
Amol US1072108 <parikha@vmware.com>
2016-11-15 15:52:45 -0800

    API : Created an api for setting and creating a data quorum. Get request get the membership quorum of the node. Post request sets the membership Quorum.
    Change-Id: I5185562d3fb02db90e33fb688c9c2d67c3ee3f35

---
+++ java/api/frontend/src/main/java/com/vmware/photon/controller/api/frontend/resources/routes/SystemRoutes.java
@@ -0,0 +1,11 @@
+package com.vmware.photon.controller.api.frontend.resources.routes;
+
+/**
+ * Created by parikha on 11/14/16.
+ */
+public class SystemRoutes {
+    public static final String API = "/system";
+
+    public static final String DATA_QUORUM_PATH = API+ "/data-quorum";
+
+}
