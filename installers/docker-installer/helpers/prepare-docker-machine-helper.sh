93a5dd0218ac08f063e57f09df4f64bdd2bf8158
York Wei <wyork@vmware.com>
2016-12-12 17:29:07 -0800

    Docker-installer: Create directories for systemd in Lightwave containers
    - Directories systemd in Lightwave containers were not being created due to
    permissions.
    - Disables -i option for docker run commands for CI.
    - Updates the Photon Controller version to local if a Photon
    Controller image is loaded.
    
    Change-Id: Ib1c9bc321512dc39389bb825451c80a9ff2ca56d

--- installers/docker-installer/helpers/prepare-docker-machine-helper.sh
+++ installers/docker-installer/helpers/prepare-docker-machine-helper.sh
@@ -6,8 +6,16 @@ fi
 
 ROOT=${1:-\/}
 
-# boot2docker VMs do not have this directory present. Create it for systemd in lightwave container.
-mkdir -p ${ROOT}sys/fs/cgroup/systemd
-mount -t cgroup -o none,name=systemd cgroup ${ROOT}sys/fs/cgroup/systemd || true
-mkdir -p ${ROOT}sys/fs/cgroup/systemd/user
-echo $$ | tee -a ${ROOT}sys/fs/cgroup/systemd/user/cgroup.procs
+if [ -n "$GERRIT_REFSPEC" ]; then
+  # This a CI run creating systemd directories for the Lightwave container needs to be run as root.
+  sudo mkdir -p ${ROOT}sys/fs/cgroup/systemd
+  sudo mount -t cgroup -o none,name=systemd cgroup ${ROOT}sys/fs/cgroup/systemd || true
+  sudo mkdir -p ${ROOT}sys/fs/cgroup/systemd/user
+  echo $$ | sudo tee -a ${ROOT}sys/fs/cgroup/systemd/user/cgroup.procs
+else
+  # boot2docker VMs do not have this directory present. Create it for systemd in Lightwave container.
+  mkdir -p ${ROOT}sys/fs/cgroup/systemd
+  mount -t cgroup -o none,name=systemd cgroup ${ROOT}sys/fs/cgroup/systemd || true
+  mkdir -p ${ROOT}sys/fs/cgroup/systemd/user
+  echo $$ | tee -a ${ROOT}sys/fs/cgroup/systemd/user/cgroup.procs
+fi
