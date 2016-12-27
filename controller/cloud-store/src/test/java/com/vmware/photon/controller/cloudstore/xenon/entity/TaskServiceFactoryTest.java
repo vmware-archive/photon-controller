2f695a910ffff3f06167dcf5026d768aaa6ac7d5
Alain Roy <alainr@vmware.com>
2016-12-27 09:05:12 -0800

    Switch task service to be on-demand load
    On-demand load means that idle services are completely unloaded by
    Xenon. Without on-demand load, the current state may be unloaded when
    idle, but with on-demand load, two more things happen when a service
    is idle:
    
    * The service is removed from the URI map (saves about 500 bytes per
      service)
    * The service is not synchronized on node group changes, which means
      that node group changes quiesce more quickly.
    
    If the service is needed (GET, POST, etc...) it is reloaded and node
    ownership is updated (if needed).
    
    This is particularly desirable for our task services because we have
    many of them and they are usually only accessed when they're actively
    being updated, but they live for the duration of the object (e.g. VM)
    they are associated with.
    
    This is a completely transparent change for users (internal and
    external).
    
    I plan to make other services on-demand load as well, but this should
    be our biggest bang for our buck.
    
    Change-Id: I5e603ea08b255505f38305f58c0718d244075658

--- controller/cloud-store/src/test/java/com/vmware/photon/controller/cloudstore/xenon/entity/TaskServiceFactoryTest.java
+++ controller/cloud-store/src/test/java/com/vmware/photon/controller/cloudstore/xenon/entity/TaskServiceFactoryTest.java
@@ -46,7 +46,8 @@ public class TaskServiceFactoryTest {
         Service.ServiceOption.REPLICATION,
         Service.ServiceOption.FACTORY,
         Service.ServiceOption.CONCURRENT_GET_HANDLING,
-        Service.ServiceOption.CONCURRENT_UPDATE_HANDLING);
+        Service.ServiceOption.CONCURRENT_UPDATE_HANDLING,
+        Service.ServiceOption.ON_DEMAND_LOAD);
     assertThat(factory.getOptions(), is(expected));
     assertThat(factory.getPeerNodeSelectorPath(), is(equalTo(ServiceUriPaths.DEFAULT_CLOUD_STORE_NODE_SELECTOR)));
   }
