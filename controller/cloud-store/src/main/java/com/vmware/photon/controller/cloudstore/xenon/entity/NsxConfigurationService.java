e9935492106426835d12ff854b9b9ee7a2fb1a78
Yu Sheng <ysheng@vmware.com>
2017-01-04 17:02:12 -0800

    Add a NsxConfiguration entity in cloud-store
    For the new installer model, we separate the NSX configuration
    from the deployment entity. All global NSX related configuration
    data should be stored in this entity, which can be shared
    among tenants/projects.
    
    Change-Id: Idd2dc2f98c894c8a47b90647116c62c296ac5028

---
+++ controller/cloud-store/src/main/java/com/vmware/photon/controller/cloudstore/xenon/entity/NsxConfigurationService.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2017 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.photon.controller.cloudstore.xenon.entity;
+
+import com.vmware.photon.controller.common.Constants;
+import com.vmware.photon.controller.common.xenon.InitializationUtils;
+import com.vmware.photon.controller.common.xenon.ServiceUriPaths;
+import com.vmware.photon.controller.common.xenon.ServiceUtils;
+import com.vmware.photon.controller.common.xenon.ValidationUtils;
+import com.vmware.photon.controller.common.xenon.deployment.MigrateDuringDeployment;
+import com.vmware.photon.controller.common.xenon.migration.MigrateDuringUpgrade;
+import com.vmware.photon.controller.common.xenon.migration.MigrationUtils;
+import com.vmware.photon.controller.common.xenon.validation.Immutable;
+import com.vmware.photon.controller.common.xenon.validation.NotBlank;
+import com.vmware.photon.controller.common.xenon.validation.WriteOnce;
+import com.vmware.xenon.common.FactoryService;
+import com.vmware.xenon.common.Operation;
+import com.vmware.xenon.common.ServiceDocument;
+import com.vmware.xenon.common.StatefulService;
+
+/**
+ * This class represents the NSX configuration entity.
+ */
+public class NsxConfigurationService extends StatefulService {
+
+  public static final String FACTORY_LINK = ServiceUriPaths.CLOUDSTORE_ROOT + "/nsx-configuration";
+
+  public NsxConfigurationService() {
+    super(State.class);
+    super.toggleOption(ServiceOption.PERSISTENCE, true);
+    super.toggleOption(ServiceOption.REPLICATION, true);
+    super.toggleOption(ServiceOption.OWNER_SELECTION, true);
+    super.toggleOption(ServiceOption.INSTRUMENTATION, true);
+  }
+
+  public static FactoryService createFactory() {
+    return FactoryService.create(
+        NsxConfigurationService.class,
+        NsxConfigurationService.State.class,
+        ServiceOption.IDEMPOTENT_POST);
+  }
+
+  @Override
+  public void handleStart(Operation startOperation) {
+    ServiceUtils.logInfo(this, "Starting service %s", getSelfLink());
+    try {
+      State startState = startOperation.getBody(State.class);
+      InitializationUtils.initialize(startState);
+      ValidationUtils.validateState(startState);
+      startOperation.complete();
+    } catch (IllegalStateException t) {
+      ServiceUtils.failOperationAsBadRequest(this, startOperation, t);
+    } catch (Throwable t) {
+      ServiceUtils.logSevere(this, t);
+      startOperation.fail(t);
+    }
+  }
+
+  /**
+   * This class represents the NSX configuration data.
+   */
+  @MigrateDuringUpgrade(transformationServicePath = MigrationUtils.REFLECTION_TRANSFORMATION_SERVICE_LINK,
+      sourceFactoryServicePath = FACTORY_LINK,
+      destinationFactoryServicePath = FACTORY_LINK,
+      serviceName = Constants.CLOUDSTORE_SERVICE_NAME)
+  @MigrateDuringDeployment(
+      factoryServicePath = FACTORY_LINK,
+      serviceName = Constants.CLOUDSTORE_SERVICE_NAME)
+  public static class State extends ServiceDocument {
+
+    @NotBlank
+    @Immutable
+    public String nsxAddress;
+
+    @NotBlank
+    @Immutable
+    public String nsxUsername;
+
+    @NotBlank
+    @Immutable
+    public String nsxPassword;
+
+    @WriteOnce
+    public String overlayTransportZoneId;
+
+    @WriteOnce
+    public String vlanTransportZoneId;
+  }
+}
